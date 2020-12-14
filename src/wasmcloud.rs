eec6082764fd02524f2afd814826b6b6e925c606
Brooks Townsend <brooksmtownsend@gmail.com>
2020-12-14 16:10:24 -0500

    integrated structopt into wasmcloud main

--- src/wasmcloud.rs
+++ src/wasmcloud.rs
@@ -1,10 +1,34 @@
+use structopt::{clap::AppSettings, StructOpt};
 use wasmcloud_host::{HostBuilder, Result};
 
 #[macro_use]
 extern crate log;
 
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(
+     global_settings(&[AppSettings::ColoredHelp, AppSettings::VersionlessSubcommands]),
+     name = "wasmcloud")]
+struct Cli {
+    /// Host for RPC connections
+    #[structopt(long = "host", default_value = "0.0.0.0", env = "RPC_HOST")]
+    rpc_host: String,
+
+    /// Port for RPC connections
+    #[structopt(long = "port", default_value = "4222", env = "RPC_PORT")]
+    rpc_port: String,
+
+    /// Allows live updating of actors
+    #[structopt(long = "allow-live-updates")]
+    allow_live_updates: bool,
+
+    /// Allows the use of "latest" artifact tag
+    #[structopt(long = "allow-oci-latest")]
+    allow_oci_latest: bool,
+}
+
 #[actix_rt::main]
 async fn main() -> Result<()> {
+    let cli = Cli::from_args();
     let _ = env_logger::Builder::from_env(env_logger::Env::default().filter_or(
         env_logger::DEFAULT_FILTER_ENV,
         "wasmcloud=info,wasmcloud_host=info",
@@ -12,15 +36,22 @@ async fn main() -> Result<()> {
     .format_module_path(false)
     .try_init();
 
-    // TODO get this information from env vars/structopt
-    let nc_rpc = nats::asynk::connect("0.0.0.0:4222").await?;
-    let nc_control = nats::asynk::connect("0.0.0.0:4222").await?;
+    let nats_url = &format!("{}:{}", cli.rpc_host, cli.rpc_port);
+    let nc_rpc = nats::asynk::connect(nats_url).await?;
+    let nc_control = nats::asynk::connect(nats_url).await?;
 
-    let host = HostBuilder::new()
+    let mut host_builder = HostBuilder::new()
         .with_rpc_client(nc_rpc)
-        .with_control_client(nc_control)
-        .enable_live_updates()
-        .build();
+        .with_control_client(nc_control);
+
+    if cli.allow_live_updates {
+        host_builder = host_builder.enable_live_updates();
+    }
+    if cli.allow_oci_latest {
+        host_builder = host_builder.oci_allow_latest();
+    }
+
+    let host = host_builder.build();
     match host.start().await {
         Ok(_) => {
             actix_rt::signal::ctrl_c().await.unwrap();
