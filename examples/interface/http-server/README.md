b8fbfcb0a40fec2c892f03e14e4d255fcb8c7324
steve <dev@somecool.net>
2021-07-06 09:47:00 -0700

    http-server interface, add built source to crate

--- examples/interface/http-server/README.md
+++ examples/interface/http-server/README.md
@@ -5,11 +5,6 @@ This folder contains
 - generated documentation (in html)
 - generated rust library (in rust)
 
-For a crate to be published on crates.io, both codegen.toml and the
-`.smithy` model file need to be in the published source tree,
-i.e., in the `rust` source directory, so that it can be built correctly.
-
-
 Any rust actor or capability provider using wasmcloud:httpserver imports this library. A capability provider implements the trait HttpServerReceiver.
 
 
--- examples/interface/http-server/rust/src/httpserver.rs
+++ examples/interface/http-server/rust/src/httpserver.rs
@@ -1,7 +1,120 @@
-#![allow(unused_imports)]
+// This file is generated automatically using wasmcloud-weld and smithy model definitions
+//
+#[allow(unused_imports)]
+use async_trait::async_trait;
+#[allow(unused_imports)]
+use serde::{Deserialize, Serialize};
+#[allow(unused_imports)]
+use std::borrow::Cow;
+#[allow(unused_imports)]
+use wasmbus_rpc::{
+    client, context, deserialize, serialize, Message, MessageDispatch, RpcError, Transport,
+};
 
-// this module is auto-generated from the smithy model
-//   file: wasmcloud-core.smithy
-//   namespace: org.wasmcloud.core
+pub const SMITHY_VERSION: &str = "1.0";
 
-include!(concat!(env!("OUT_DIR"), "/src/httpserver.rs"));
+/// Headers is a list of http headers
+pub type Headers = std::collections::HashMap<String, String>;
+
+/// HttpRequest contains data sent to actor about the http request
+#[derive(Default, Clone, Debug, Serialize, Deserialize)]
+pub struct HttpRequest {
+    pub path: String,
+    pub header: Headers,
+    #[serde(rename = "queryString")]
+    pub query_string: String,
+    #[serde(with = "serde_bytes")]
+    pub body: Vec<u8>,
+    pub method: String,
+}
+
+/// HttpResponse contains the actor's response to return to the http client
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct HttpResponse {
+    #[serde(with = "serde_bytes")]
+    pub body: Vec<u8>,
+    /// statusCode should be 200 if the request was correctly handled
+    #[serde(rename = "statusCode")]
+    pub status_code: u32,
+    pub status: String,
+    pub header: Headers,
+}
+
+/// HttpServer is the contract to be implemented by actor
+/// @direction(actorReceiver)
+#[async_trait]
+pub trait HttpServer {
+    async fn handle_request(
+        &self,
+        ctx: &context::Context<'_>,
+        arg: &HttpRequest,
+    ) -> Result<HttpResponse, RpcError>;
+}
+
+/// HttpServerReceiver receives messages defined in the HttpServer service trait
+/// HttpServer is the contract to be implemented by actor
+/// @direction(actorReceiver)
+#[async_trait]
+pub trait HttpServerReceiver: MessageDispatch + HttpServer {
+    async fn dispatch(
+        &self,
+        ctx: &context::Context<'_>,
+        message: &Message<'_>,
+    ) -> Result<Message<'static>, RpcError> {
+        match message.method {
+            "HandleRequest" => {
+                let value: HttpRequest = deserialize(message.arg.as_ref())?;
+                let resp = HttpServer::handle_request(self, ctx, &value).await?;
+                let buf = Cow::Owned(serialize(&resp)?);
+                Ok(Message {
+                    method: "HttpServer.HandleRequest",
+                    arg: buf,
+                })
+            }
+            _ => Err(RpcError::MethodNotHandled(format!(
+                "HttpServer::{}",
+                message.method
+            ))),
+        }
+    }
+}
+
+/// HttpServerSender sends messages to a HttpServer service
+/// HttpServer is the contract to be implemented by actor
+/// @direction(actorReceiver)
+#[derive(Debug)]
+pub struct HttpServerSender<T> {
+    transport: T,
+    config: client::SendConfig,
+}
+
+impl<T: Transport> HttpServerSender<T> {
+    pub fn new(config: client::SendConfig, transport: T) -> Self {
+        HttpServerSender { transport, config }
+    }
+}
+
+#[async_trait]
+impl<T: Transport + std::marker::Sync + std::marker::Send> HttpServer for HttpServerSender<T> {
+    #[allow(unused)]
+    async fn handle_request(
+        &self,
+        ctx: &context::Context<'_>,
+        arg: &HttpRequest,
+    ) -> Result<HttpResponse, RpcError> {
+        let arg = serialize(arg)?;
+        let resp = self
+            .transport
+            .send(
+                ctx,
+                &self.config,
+                Message {
+                    method: "HandleRequest",
+                    arg: Cow::Borrowed(&arg),
+                },
+            )
+            .await?;
+        let value = deserialize(resp.arg.as_ref())?;
+        Ok(value)
+    }
+}
