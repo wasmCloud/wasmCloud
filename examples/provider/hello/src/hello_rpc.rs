338ec24991248e9af724057ba0a6714790327749
steve <dev@somecool.net>
2021-07-14 14:16:28 -0700

    replaced the loop in main with the new wait_for_shutdown function in wasmbus-rpc lib; move some rust functions between source files to make compiler happy

--- examples/provider/hello/src/hello_rpc.rs
+++ examples/provider/hello/src/hello_rpc.rs
@@ -1,18 +1,56 @@
-use async_trait::async_trait;
-use wasmbus_rpc::{context::Context, provider::prelude::*, MessageDispatch, RpcError};
+use crate::HelloProvider;
+use wasmbus_rpc::core::{HealthCheckRequest, HealthCheckResponse};
+use wasmbus_rpc::provider::prelude::*;
 use wasmcloud_example_hello::{Hello, HelloReceiver};
-
-/// Hello provider implementation
-#[derive(Clone, Debug, Default)]
-pub struct HelloProvider {}
 
 /// Implementation of hello provider
 #[async_trait]
 impl Hello for HelloProvider {
     /// for any input string SSS,  return "Hello SSS"
-    async fn say_hello(&self, _ctx: &'_ Context<'_>, arg: &String) -> Result<String, RpcError> {
+    async fn say_hello(
+        &self,
+        _ctx: &'_ context::Context<'_>,
+        arg: &String,
+    ) -> Result<String, RpcError> {
         Ok(format!("Hello {}", arg))
     }
+}
+
+/// Your provider can handle any of these methods
+/// to receive notification of new actor links, deleted links,
+/// and for handling health check.
+/// The default handlers are implemented in the trait ProviderHandler.
+impl ProviderHandler for HelloProvider {
+    /// Perform health check. Called at regular intervals by host
+    fn health_request(&self, _arg: &HealthCheckRequest) -> Result<HealthCheckResponse, RpcError> {
+        Ok(HealthCheckResponse {
+            healthy: true,
+            message: None,
+        })
+    }
+
+    /*
+    /// Provider should perform any operations needed for a new link,
+    /// including setting up per-actor resources, and checking authorization.
+    /// If the link is allowed, return true, otherwise return false to deny the link.
+    /// This message is idempotent - provider must be able to handle
+    /// duplicates
+    fn put_link(&self, ld: &LinkDefinition) -> Result<bool, RpcError> {
+        Ok(true)
+    }
+     */
+
+    /*
+    /// Notify the provider that the link is dropped
+    fn delete_link(&self, actor_id: &str) {}
+     */
+
+    /*
+    /// Handle system shutdown message
+    fn shutdown(&self) -> Result<(), Infallible> {
+        Ok(())
+    }
+     */
 }
 
 // TODO: derive macro should generate most of the following
--- examples/provider/hello/src/main.rs
+++ examples/provider/hello/src/main.rs
@@ -4,55 +4,15 @@
 use anyhow::anyhow;
 use log::info;
 use once_cell::sync::OnceCell;
-use std::time::Duration;
-use tokio::sync::oneshot::error::TryRecvError;
-use wasmbus_rpc::provider::{HostBridge, ProviderHandler};
-use wasmbus_rpc::{
-    core::{HealthCheckRequest, HealthCheckResponse},
-    RpcError,
-};
+use wasmbus_rpc::provider::{wait_for_shutdown, HostBridge};
 
 mod hello_rpc;
-use crate::hello_rpc::HelloProvider;
+
+/// Hello provider implementation
+#[derive(Clone, Debug, Default)]
+pub struct HelloProvider {}
 
 static BRIDGE: OnceCell<HostBridge> = OnceCell::new();
-
-/// Your provider can handle any of these methods
-/// to receive notification of new actor links, deleted links,
-/// and for handling health check.
-/// The default handlers are implemented in the trait ProviderHandler.
-impl ProviderHandler for HelloProvider {
-    /// Perform health check. Called at regular intervals by host
-    fn health_request(&self, _arg: &HealthCheckRequest) -> Result<HealthCheckResponse, RpcError> {
-        Ok(HealthCheckResponse {
-            healthy: true,
-            message: None,
-        })
-    }
-
-    /*
-    /// Provider should perform any operations needed for a new link,
-    /// including setting up per-actor resources, and checking authorization.
-    /// If the link is allowed, return true, otherwise return false to deny the link.
-    /// This message is idempotent - provider must be able to handle
-    /// duplicates
-    fn put_link(&self, ld: &LinkDefinition) -> Result<bool, RpcError> {
-        Ok(true)
-    }
-     */
-
-    /*
-    /// Notify the provider that the link is dropped
-    fn delete_link(&self, actor_id: &str) {}
-     */
-
-    /*
-    /// Handle system shutdown message
-    fn shutdown(&self) -> Result<(), Infallible> {
-        Ok(())
-    }
-     */
-}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -61,10 +21,8 @@ async fn main() -> Result<(), anyhow::Error> {
     // The channel bound is 0 so that a thread will block until it's been logged.
     let (log_tx, log_rx) = crossbeam::channel::bounded(0);
 
-    info!("Initializing hello provider");
-
     let host_data = wasmbus_rpc::provider::load_host_data()?;
-    let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel();
+    let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel();
     info!(
         "Starting Hello Capability Provider {}\n",
         &host_data.provider_key
@@ -74,8 +32,8 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let provider = HelloProvider::default();
 
+    // create bridge to subscribe to host events, issuing callbacks to provider for processing
     let _ = BRIDGE.set(HostBridge::new(nc.clone(), &host_data, log_tx));
-
     BRIDGE
         .get()
         .unwrap()
@@ -87,53 +45,14 @@ async fn main() -> Result<(), anyhow::Error> {
         )
         .map_err(|e| anyhow!("fatal error setting up subscriptions: {}", e))?;
 
-    info!("Hello provider is ready for requests (8)");
+    info!("Hello provider is ready for requests");
 
-    // every 50ms, check for new log messages.
-    let timeout = Duration::from_millis(50);
-    loop {
-        match log_rx.recv_timeout(timeout) {
-            // if we have received a log message, continue
-            // so that we check immediately - a group of messages that
-            // arrive together will be printed with no delay between them.
-            Ok((level, s)) => {
-                log::logger().log(
-                    &log::Record::builder()
-                        .args(format_args!("{}", s))
-                        .level(level)
-                        .build(),
-                );
-                continue;
-            }
-            Err(crossbeam::channel::RecvTimeoutError::Timeout) => {}
-            Err(crossbeam::channel::RecvTimeoutError::Disconnected) => {
-                eprintln!("** Logger exited - quitting **");
-                break;
-            }
-        }
+    // process subscription events and log messages, waiting for shutdown signal
+    wait_for_shutdown(log_rx, shutdown_rx);
 
-        // on each iteration of the loop, after all pending logs
-        // have been written out, check for shutdown signal
-        match shutdown_rx.try_recv() {
-            Ok(_) => {
-                // got the shutdown signal
-                break;
-            }
-            Err(TryRecvError::Empty) => {
-                // no signal yet, keep waiting
-            }
-            Err(TryRecvError::Closed) => {
-                // sender exited
-                break;
-            }
-        }
-    }
-    //let _wait_for_shutdown = shutdown_rx.await;
-    eprintln!("********** Hello server exiting");
-
-    // Flush outgoing buffers and unsubscribe all subscriptions.
-    // Most of the subscriptions have alrady been drained.
+    // Flush outgoing buffers and unsubscribe from all remaining subscriptions
     let _ = nc.drain();
+    info!("Hello provider exiting");
 
     Ok(())
 }
