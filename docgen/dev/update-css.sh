3d2e59a4199652cc623eb55ab80eef73d592d70f
steve <dev@somecool.net>
2021-07-13 20:56:17 -0700

    move html templates into codegen folder to allow publishing to crates.io

--- docgen/dev/update-css.sh
+++ docgen/dev/update-css.sh
@@ -6,7 +6,7 @@ chmod 755 $OUT
 chmod 777 $OUT/css
 
 docker run --rm \
-    -v $PWD/../templates:/templates:ro \
+    -v $PWD/../../codegen/templates/html:/templates:ro \
     -v $PWD/conf:/project/conf:ro \
     -v $PWD/src:/project/src:ro \
     -v $OUT:/public \
