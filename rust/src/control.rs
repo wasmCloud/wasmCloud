fbbf7d080dc0f2933b89e51747c48ca5d8ca72ed
Brooks Townsend <brooks@cosmonic.com>
2021-09-01 14:11:29 -0400

    use common crates release action
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- rust/src/control.rs
+++ rust/src/control.rs
@@ -10,8 +10,8 @@ use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, string::ToString};
 #[allow(unused_imports)]
 use wasmbus_rpc::{
-    deserialize, serialize, Context, Message, MessageDispatch, RpcError, RpcResult, SendOpts,
-    Transport,
+    context::Context, deserialize, serialize, Message, MessageDispatch, RpcError, RpcResult,
+    SendOpts, Transport,
 };
 
 pub const SMITHY_VERSION: &str = "1.0";
