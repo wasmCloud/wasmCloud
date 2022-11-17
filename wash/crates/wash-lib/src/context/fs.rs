7e144f229fc32fc2d9f145b244165240050a22ac
Taylor Thomas <taylor@cosmonic.com>
2022-11-17 14:12:59 -0700

    feat(lib): Adds new context tests
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- wash/crates/wash-lib/src/context/fs.rs
+++ wash/crates/wash-lib/src/context/fs.rs
@@ -155,3 +155,180 @@ pub fn load_context(path: impl AsRef<Path>) -> Result<WashContext> {
 fn context_path_from_name(dir: impl AsRef<Path>, name: &str) -> PathBuf {
     dir.as_ref().join(format!("{}.json", name))
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn round_trip_happy_path() {
+        let tempdir = tempfile::tempdir().expect("Unable to create tempdir");
+        let contexts_path = tempdir.path().join("contexts");
+        let ctx_dir =
+            ContextDir::new(&contexts_path).expect("Should be able to create context dir");
+
+        assert!(
+            contexts_path.exists() && contexts_path.is_dir(),
+            "Non-existent directory should have been created"
+        );
+
+        let mut orig_ctx = WashContext {
+            name: "happy_path".to_string(),
+            rpc_lattice_prefix: "foobar".to_string(),
+            ..Default::default()
+        };
+
+        ctx_dir
+            .save_context(&orig_ctx)
+            .expect("Should be able to save a context to disk");
+
+        let mut readdir = contexts_path.read_dir().unwrap();
+        let ctx_path = readdir
+            .next()
+            .expect("Should be at least 1 entry in directory")
+            .unwrap()
+            .path();
+
+        assert_eq!(
+            ctx_path,
+            contexts_path.join("happy_path.json"),
+            "Newly created context should exist"
+        );
+
+        assert!(
+            readdir.next().is_none(),
+            "Only one path should exist in the directory"
+        );
+
+        // Now load the context from disk and compare
+        let loaded = ctx_dir
+            .load_context("happy_path")
+            .expect("Should be able to load context from disk");
+        assert!(
+            orig_ctx.name == loaded.name
+                && orig_ctx.rpc_lattice_prefix == loaded.rpc_lattice_prefix,
+            "Should have loaded the correct context from disk"
+        );
+
+        // Save one more context
+        orig_ctx.name = "happy_gilmore".to_string();
+        orig_ctx.rpc_lattice_prefix = "baz".to_string();
+        ctx_dir
+            .save_context(&orig_ctx)
+            .expect("Should be able to save second context");
+
+        assert_eq!(
+            contexts_path.read_dir().unwrap().count(),
+            2,
+            "Directory should have only 2 entries"
+        );
+
+        ctx_dir
+            .set_default_context("happy_gilmore")
+            .expect("Should be able to set default context");
+        assert_eq!(
+            ctx_dir
+                .default_context()
+                .expect("Should be able to load default context"),
+            "happy_gilmore",
+            "Default context should be correct"
+        );
+
+        // Load the default context
+        let loaded = ctx_dir
+            .load_default_context()
+            .expect("Should be able to load default context from disk");
+        assert!(
+            orig_ctx.name == loaded.name
+                && orig_ctx.rpc_lattice_prefix == loaded.rpc_lattice_prefix,
+            "Should have loaded the correct context from disk"
+        );
+
+        assert_eq!(
+            contexts_path.read_dir().unwrap().count(),
+            3,
+            "Directory should have a new entry from the default context"
+        );
+
+        // List the contexts
+        let list = ctx_dir
+            .list_contexts()
+            .expect("Should be able to list contexts");
+        assert_eq!(list.len(), 2, "Should only list 2 contexts");
+        for ctx in list.into_iter() {
+            assert!(
+                ctx == "happy_path" || ctx == "happy_gilmore",
+                "Should have found only the contexts we created"
+            );
+        }
+
+        // Delete a context
+        ctx_dir
+            .delete_context("happy_path")
+            .expect("Should be able to delete context");
+
+        assert!(
+            !contexts_path
+                .read_dir()
+                .unwrap()
+                .into_iter()
+                .any(|p| p.unwrap().path() == ctx_path),
+            "Context should have been removed from directory"
+        );
+    }
+
+    #[test]
+    fn load_non_existent_contexts() {
+        let tempdir = tempfile::tempdir().expect("Unable to create tempdir");
+        let ctx_dir = ContextDir::new(&tempdir).expect("Should be able to create context dir");
+
+        ctx_dir
+            .load_default_context()
+            .expect_err("Loading a non-existent default context should error");
+
+        ctx_dir
+            .load_context("idontexist")
+            .expect_err("Loading a non-existent context should error");
+    }
+
+    #[test]
+    fn default_context_with_no_settings() {
+        let tempdir = tempfile::tempdir().expect("Unable to create tempdir");
+        let ctx_dir = ContextDir::new(&tempdir).expect("Should be able to create context dir");
+
+        assert_eq!(
+            ctx_dir
+                .default_context()
+                .expect("Should be able to get a default context with nothing set"),
+            HOST_CONFIG_NAME,
+            "Default context name should be {}",
+            HOST_CONFIG_NAME
+        );
+
+        ctx_dir
+            .set_default_context("idontexist")
+            .expect_err("Should not be able to set a default context that doesn't exist");
+    }
+
+    const PRE_REFACTOR_CONTEXT: &str = r#"{"name":"host_config","cluster_seed":"SCAJ3HQZCDA562YW3VUHHIAUJ2SUCYUNGDCP5DBKQOTEZ6ZZGBKT5NI3DQ","ctl_host":"127.0.0.1","ctl_port":5893,"ctl_jwt":"","ctl_seed":"","ctl_credsfile":null,"ctl_timeout":2000,"ctl_lattice_prefix":"default","rpc_host":"127.0.0.1","rpc_port":5893,"rpc_jwt":"","rpc_seed":"","rpc_credsfile":null,"rpc_timeout":2000,"rpc_lattice_prefix":"default"}"#;
+
+    #[test]
+    fn works_with_existing() {
+        let tempdir = tempfile::tempdir().expect("Unable to create tempdir");
+        std::fs::write(
+            tempdir.path().join("host_config.json"),
+            PRE_REFACTOR_CONTEXT,
+        )
+        .expect("Unable to write test data to disk");
+        let ctx_dir = ContextDir::new(&tempdir).expect("Should be able to create context dir");
+
+        let ctx = ctx_dir
+            .load_context("host_config")
+            .expect("Should not be able to load a pre-existing context");
+
+        assert!(
+            ctx.name == "host_config" && ctx.ctl_port == 5893,
+            "Should read the correct data from disk"
+        );
+    }
+}
