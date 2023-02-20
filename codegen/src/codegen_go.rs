cd7b4a608f968ee9711fca15142c0f7f088f32ca
Taction <zchao9100@gmail.com>
2023-02-21 02:42:10 +0800

    fix: go code gen array alloc error (#132)
    * fix: go code gen array alloc error
    
    Signed-off-by: zhangchao <zchao9100@gmail.com>
    
    * fix go slice make
    
    Signed-off-by: zhangchao <zchao9100@gmail.com>
    
    ---------
    
    Signed-off-by: zhangchao <zchao9100@gmail.com>

--- codegen/src/codegen_go.rs
+++ codegen/src/codegen_go.rs
@@ -1623,7 +1623,7 @@ impl<'model> GoCodeGen<'model> {
                         }}
                        	{}
                         if err != nil {{ return make([]{}, 0 ), err }}
-                        val := make([]{}, size)
+                        val := make([]{}, 0, size)
                         for i := uint32(0); i < size; i++ {{
                            item,err := {}
                            if err != nil {{ return val, err }}
@@ -1654,7 +1654,7 @@ impl<'model> GoCodeGen<'model> {
                         }}
                        	{}
                         if err != nil {{ return make([]{},0),err }}
-                        val := make([]{}, size)
+                        val := make([]{}, 0, size)
                         for i := uint32(0); i < size; i++ {{
                            item,err := {}
                            if err != nil {{ return val, err }}
