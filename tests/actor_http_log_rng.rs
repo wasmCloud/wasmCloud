d6906efc2838823077ba7da0e4f09ba3d3eae593
Roman Volosatovs <rvolosatovs@riseup.net>
2023-03-16 10:46:41 +0100

    test: add a component test using `wasm-tools` as a binary
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- tests/actor_http_log_rng.rs
+++ tests/actor_http_log_rng.rs
@@ -41,8 +41,24 @@ fn new_runtime() -> Runtime {
     Runtime::from_host_handler(HandlerFunc::from(host_call)).expect("failed to construct runtime")
 }
 
-fn assert_response(response: Option<impl AsRef<[u8]>>) -> anyhow::Result<()> {
-    let response = response.context("response missing")?;
+async fn run(wasm: impl AsRef<[u8]>) -> anyhow::Result<()> {
+    let (wasm, key) = sign(
+        wasm,
+        "http_log_rng",
+        [caps::HTTP_SERVER, caps::LOGGING, caps::NUMBERGEN],
+    )
+    .context("failed to sign Wasm")?;
+
+    let rt = new_runtime();
+    let actor = Actor::new(&rt, wasm).expect("failed to construct actor");
+    assert_eq!(actor.claims().subject, key.public_key());
+
+    let response = actor
+        .call("HttpServer.HandleRequest", Some(REQUEST.as_slice()))
+        .await
+        .context("failed to call `HttpServer.HandleRequest`")?
+        .expect("`HttpServer.HandleRequest` must not fail")
+        .context("response missing")?;
 
     #[derive(Deserialize)]
     struct Response {
@@ -82,47 +98,24 @@ async fn actor_http_log_rng_module() -> anyhow::Result<()> {
     let wasm = fs::read(WASM)
         .await
         .unwrap_or_else(|_| panic!("failed to read `{WASM}`"));
-    let (wasm, key) = sign(
-        wasm,
-        "http_log_rng",
-        [caps::HTTP_SERVER, caps::LOGGING, caps::NUMBERGEN],
-    )
-    .context("failed to sign module")?;
-
-    let rt = new_runtime();
-    let actor = Actor::new(&rt, wasm).expect("failed to construct actor");
-    assert_eq!(actor.claims().subject, key.public_key());
-
-    let response = actor
-        .call("HttpServer.HandleRequest", Some(REQUEST.as_slice()))
-        .await
-        .context("failed to call `HttpServer.HandleRequest`")?
-        .expect("`HttpServer.HandleRequest` must not fail");
-    assert_response(response)
+    run(wasm).await
 }
 
 #[tokio::test]
-async fn actor_http_log_rng_component() -> anyhow::Result<()> {
+async fn actor_http_log_rng_component_lib() -> anyhow::Result<()> {
     init();
 
     let wat = wat::parse_file(env!("CARGO_CDYLIB_FILE_ACTOR_HTTP_LOG_RNG_COMPONENT"))
         .context("failed to parse binary")?;
-    let wasm = encode_component(&wat, true)?;
-    let (wasm, key) = sign(
-        wasm,
-        "http_log_rng",
-        [caps::HTTP_SERVER, caps::LOGGING, caps::NUMBERGEN],
-    )
-    .context("failed to sign component")?;
+    let wasm = encode_component_lib(&wat, true)?;
+    run(wasm).await
+}
 
-    let rt = new_runtime();
-    let actor = Actor::new(&rt, wasm).expect("failed to construct actor");
-    assert_eq!(actor.claims().subject, key.public_key());
+#[tokio::test]
+async fn actor_http_log_rng_component_bin() -> anyhow::Result<()> {
+    init();
 
-    let response = actor
-        .call("HttpServer.HandleRequest", Some(REQUEST.as_slice()))
-        .await
-        .context("failed to call `HttpServer.HandleRequest`")?
-        .expect("`HttpServer.HandleRequest` must not fail");
-    assert_response(response)
+    let wasm =
+        encode_component_bin(env!("CARGO_CDYLIB_FILE_ACTOR_HTTP_LOG_RNG_COMPONENT"), true).await?;
+    run(wasm).await
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,8 +1,14 @@
+use tokio::fs;
 pub use wascap::{caps, jwt};
 pub use wasmbus_rpc::common::{deserialize, serialize};
 
+use std::path::Path;
+use std::process::{Output, Stdio};
+
 use anyhow::Context;
 use once_cell::sync::Lazy;
+use tempfile::NamedTempFile;
+use tokio::process::Command;
 use tracing_subscriber::prelude::*;
 use wascap::prelude::{ClaimsBuilder, KeyPair};
 use wascap::wasm::embed_claims;
@@ -47,7 +53,8 @@ pub fn sign(
     Ok((wasm, module))
 }
 
-pub fn encode_component(module: &[u8], wasi: bool) -> anyhow::Result<Vec<u8>> {
+/// Encode a component using `wasm-tools` as a library
+pub fn encode_component_lib(module: &[u8], wasi: bool) -> anyhow::Result<Vec<u8>> {
     let encoder = ComponentEncoder::default()
         .validate(true)
         .module(module)
@@ -63,4 +70,44 @@ pub fn encode_component(module: &[u8], wasi: bool) -> anyhow::Result<Vec<u8>> {
         encoder
     };
     encoder.encode().context("failed to encode a component")
+}
+
+/// Encode a component using `wasm-tools` as a binary
+#[allow(dead_code)] // by some reason Rust fails to find usage of this function in parent crate
+pub async fn encode_component_bin(path: impl AsRef<Path>, wasi: bool) -> anyhow::Result<Vec<u8>> {
+    let wasm = NamedTempFile::new()
+        .expect("failed to create temporary file")
+        .into_temp_path();
+
+    let mut cmd = Command::new(env!("CARGO_BIN_FILE_WASM_TOOLS"));
+    let cmd = cmd
+        .stdin(Stdio::piped())
+        .stderr(Stdio::piped())
+        .args(["component", "new"])
+        .arg(path.as_ref())
+        .arg("-o")
+        .arg(wasm.as_os_str());
+    if wasi {
+        cmd.arg("--adapt").arg(format!(
+            "wasi_snapshot_preview1={}",
+            env!("CARGO_CDYLIB_FILE_WASI_SNAPSHOT_PREVIEW1")
+        ));
+    };
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = cmd
+        .spawn()
+        .expect("failed to spawn `wasm-tools component`")
+        .wait_with_output()
+        .await
+        .expect("failed to run `wasm-tools component`");
+    eprintln!("stderr: {}", String::from_utf8(stderr).unwrap());
+    assert!(
+        status.success(),
+        "stdout: {}",
+        String::from_utf8(stdout).unwrap()
+    );
+    fs::read(wasm).await.context("failed to read Wasm")
 }
