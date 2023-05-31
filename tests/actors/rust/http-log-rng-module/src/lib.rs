b8fe093e4e392c7d0c39ac46b77a852a75bcaf1f
Roman Volosatovs <rvolosatovs@riseup.net>
2023-05-31 11:04:21 +0200

    test: align module and component tests
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- tests/actors/rust/http-log-rng-module/src/lib.rs
+++ tests/actors/rust/http-log-rng-module/src/lib.rs
@@ -1,7 +1,8 @@
 use serde::Deserialize;
 use serde_json::json;
 use wasmcloud_actor::{
-    debug, error, export_actor, info, warn, HostRng, HttpHandler, HttpRequest, HttpResponse,
+    debug, error, export_actor, info, logging, random, trace, warn, HostRng, HttpHandler,
+    HttpRequest, HttpResponse,
 };
 
 #[derive(Default)]
@@ -12,6 +13,19 @@ impl HttpHandler for HttpLogRng {
         &self,
         HttpRequest { body, .. }: HttpRequest,
     ) -> Result<HttpResponse, String> {
+        logging::log(logging::Level::Trace, "trace-context", "trace");
+        logging::log(logging::Level::Debug, "debug-context", "debug");
+        logging::log(logging::Level::Info, "info-context", "info");
+        logging::log(logging::Level::Warn, "warn-context", "warn");
+        logging::log(logging::Level::Error, "error-context", "error");
+
+        trace!(context: "trace-context", "trace");
+        debug!(context: "debug-context", "debug");
+        info!(context: "info-context", "info");
+        warn!(context: "warn-context", "warn");
+        error!(context: "error-context", "error");
+
+        trace!("trace");
         debug!("debug");
         info!("info");
         warn!("warn");
@@ -25,12 +39,15 @@ impl HttpHandler for HttpLogRng {
         let Request { min, max } =
             serde_json::from_slice(&body).expect("failed to decode request body");
 
-        let body = serde_json::to_vec(&json!({
+        let res = json!({
+            "get_random_bytes": random::get_random_bytes(8),
+            "get_random_u64": random::get_random_u64(),
             "guid": HostRng::generate_guid(),
-            "random_in_range": HostRng::random_in_range(min, max),
             "random_32": HostRng::random32(),
-        }))
-        .expect("failed to encode response to JSON");
+            "random_in_range": HostRng::random_in_range(min, max),
+        });
+        eprintln!("response: `{res:?}`");
+        let body = serde_json::to_vec(&res).expect("failed to encode response to JSON");
         Ok(HttpResponse {
             body,
             ..Default::default()
