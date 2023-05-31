3e4ed14467e396c23bf2afd629c29a8f71719b12
Roman Volosatovs <rvolosatovs@riseup.net>
2023-05-31 12:52:35 +0200

    build(nix): specify caches in flake
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- wash/flake.nix
+++ wash/flake.nix
@@ -1,4 +1,15 @@
 {
+  nixConfig.extra-substituters = [
+    "https://wasmcloud.cachix.org"
+    "https://nix-community.cachix.org"
+    "https://cache.garnix.io"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "wasmcloud.cachix.org-1:9gRBzsKh+x2HbVVspreFg/6iFRiD4aOcUQfXVDl3hiM="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+  ];
+
   description = "wash - wasmCloud Shell";
 
   inputs.nixify.url = github:rvolosatovs/nixify;
