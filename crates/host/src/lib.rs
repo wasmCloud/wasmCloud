32cead5ec7c1559ad0c161568712140b7d89d196
Roman Volosatovs <rvolosatovs@riseup.net>
2023-07-13 19:52:18 +0200

    feat(wasmbus): implement actor operations
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/host/src/lib.rs
+++ crates/host/src/lib.rs
@@ -10,12 +10,23 @@ pub mod local;
 /// wasmbus lattice
 pub mod wasmbus;
 
+/// bindle artifact fetching
+pub mod bindle;
+
+/// OCI artifact fetching
+pub mod oci;
+
 pub use local::{Lattice as LocalLattice, LatticeConfig as LocalLatticeConfig};
 pub use wasmbus::{Lattice as WasmbusLattice, LatticeConfig as WasmbusLatticeConfig};
 
 pub use url;
 
-use anyhow::Context as _;
+use std::path::PathBuf;
+
+use anyhow::{anyhow, bail, Context as _};
+use tokio::fs;
+use tracing::instrument;
+use url::Url;
 
 #[cfg(unix)]
 fn socket_pair() -> anyhow::Result<(tokio::net::UnixStream, tokio::net::UnixStream)> {
@@ -25,4 +36,52 @@ fn socket_pair() -> anyhow::Result<(tokio::net::UnixStream, tokio::net::UnixStre
 #[cfg(windows)]
 fn socket_pair() -> anyhow::Result<(tokio::io::DuplexStream, tokio::io::DuplexStream)> {
     Ok(tokio::io::duplex(8196))
+}
+
+enum ResourceRef<'a> {
+    File(PathBuf),
+    Bindle(&'a str),
+    Oci(&'a str),
+}
+
+impl<'a> TryFrom<&'a str> for ResourceRef<'a> {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        match Url::parse(s) {
+            Ok(url) => {
+                match url.scheme() {
+                    "file" => url
+                        .to_file_path()
+                        .map(Self::File)
+                        .map_err(|_| anyhow!("failed to convert `{url}` to a file path")),
+                    "bindle" => s
+                        .strip_prefix("bindle://")
+                        .map(Self::Bindle)
+                        .context("invalid Bindle reference"),
+                    // TODO: Support other schemes
+                    scheme => bail!("unsupported scheme `{scheme}`"),
+                }
+            }
+            Err(url::ParseError::RelativeUrlWithoutBase) => Ok(Self::Oci(s)), // TODO: Validate
+            Err(e) => {
+                bail!(anyhow!(e).context(format!("failed to parse reference `{s}`")))
+            }
+        }
+    }
+}
+
+/// Fetch an actor from a reference.
+#[instrument(skip(actor_ref))]
+pub async fn fetch_actor(actor_ref: impl AsRef<str>) -> anyhow::Result<Vec<u8>> {
+    match ResourceRef::try_from(actor_ref.as_ref())? {
+        ResourceRef::File(actor_ref) => fs::read(actor_ref).await.context("failed to read actor"),
+        ResourceRef::Bindle(actor_ref) => crate::bindle::fetch_actor(None, &actor_ref)
+            .await
+            .with_context(|| format!("failed to fetch actor under Bindle reference `{actor_ref}`")),
+        // TODO: Set config
+        ResourceRef::Oci(actor_ref) => crate::oci::fetch_actor(None, &actor_ref, true, vec![])
+            .await
+            .with_context(|| format!("failed to fetch actor under OCI reference `{actor_ref}`")),
+    }
 }
---
+++ crates/host/src/wasmbus/event.rs
@@ -0,0 +1,93 @@
+use core::num::NonZeroUsize;
+
+use std::collections::BTreeMap;
+
+use serde_json::json;
+use uuid::Uuid;
+use wascap::jwt;
+
+fn format_claims(claims: &jwt::Claims<jwt::Actor>) -> serde_json::Value {
+    let issuer = &claims.issuer;
+    let not_before_human = "TODO";
+    let expires_human = "TODO";
+    if let Some(actor) = &claims.metadata {
+        json!({
+            "call_alias": actor.call_alias,
+            "caps": actor.caps,
+            "issuer": issuer,
+            "tags": actor.tags,
+            "name": actor.name,
+            "version": actor.ver,
+            "revision": actor.rev,
+            "not_before_human": not_before_human,
+            "expires_human": expires_human,
+        })
+    } else {
+        json!({
+            "issuer": issuer,
+            "not_before_human": not_before_human,
+            "expires_human": expires_human,
+        })
+    }
+}
+
+pub fn actor_started(
+    claims: &jwt::Claims<jwt::Actor>,
+    annotations: &BTreeMap<String, String>,
+    instance_id: Uuid,
+    image_ref: impl AsRef<str>,
+) -> serde_json::Value {
+    json!({
+        "public_key": claims.subject,
+        "image_ref": image_ref.as_ref(),
+        "api_version": "n/a",
+        "instance_id": instance_id,
+        "annotations": annotations,
+        "claims": format_claims(claims),
+    })
+}
+
+pub fn actor_stopped(
+    claims: &jwt::Claims<jwt::Actor>,
+    annotations: &BTreeMap<String, String>,
+    instance_id: Uuid,
+) -> serde_json::Value {
+    json!({
+        "public_key": claims.subject,
+        "instance_id": instance_id,
+        "annotations": annotations,
+    })
+}
+
+pub fn actors_started(
+    claims: &jwt::Claims<jwt::Actor>,
+    annotations: &BTreeMap<String, String>,
+    host_id: impl AsRef<str>,
+    count: impl Into<usize>,
+    image_ref: impl AsRef<str>,
+) -> serde_json::Value {
+    json!({
+        "public_key": claims.subject,
+        "image_ref": image_ref.as_ref(),
+        "annotations": annotations,
+        "host_id": host_id.as_ref(),
+        "claims": claims,
+        "count": count.into(),
+    })
+}
+
+pub fn actors_stopped(
+    claims: &jwt::Claims<jwt::Actor>,
+    annotations: &BTreeMap<String, String>,
+    host_id: impl AsRef<str>,
+    count: NonZeroUsize,
+    remaining: usize,
+) -> serde_json::Value {
+    json!({
+        "host_id": host_id.as_ref(),
+        "public_key": claims.subject,
+        "count": count,
+        "remaining": remaining,
+        "annotations": annotations,
+    })
+}
