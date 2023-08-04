9d3ceb337405493781311ba11a730a10f9e7aca9
Connor Smith <connor.smith.256@gmail.com>
2023-08-03 20:02:43 -0600

    feat: add support for custom lattice prefix
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>

--- crates/host/src/wasmbus/config.rs
+++ crates/host/src/wasmbus/config.rs
@@ -4,18 +4,22 @@ use url::Url;
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 /// wasmCloud Host configuration
 pub struct Host {
-    /// URL to connect to
-    pub url: Url,
-    /// Optional host seed
+    /// NATS URL to connect to for control interface connection
+    pub ctl_nats_url: Url,
+    /// The lattice the host belongs to
+    pub lattice_prefix: String,
+    /// The seed key (a printable 256-bit Ed25519 private key) used by this host to generate its public key
     pub host_seed: Option<String>,
-    /// Optional cluster seed
+    /// The seed key (a printable 256-bit Ed25519 private key) used by this host to sign all invocations
     pub cluster_seed: Option<String>,
 }
 
 impl Default for Host {
     fn default() -> Self {
         Self {
-            url: Url::parse("nats://localhost:4222").expect("failed to parse URL"),
+            ctl_nats_url: Url::parse("nats://localhost:4222")
+                .expect("failed to parse control NATS URL"),
+            lattice_prefix: "default".to_string(),
             host_seed: None,
             cluster_seed: None,
         }
