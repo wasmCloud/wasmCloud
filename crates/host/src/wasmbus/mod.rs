1089ca1f5c35a9c75c2e397738a1c2a871f4cc2e
Brooks Townsend <brooksmtownsend@gmail.com>
2023-08-22 09:10:43 -0400

    Revert "feat: delete claims when actors or providers are stopped"
    This reverts commit 31b76fd2754e1962df36340275ad5179576c8d07.
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- crates/host/src/wasmbus/mod.rs
+++ crates/host/src/wasmbus/mod.rs
@@ -1474,10 +1474,6 @@ impl Host {
     ) -> anyhow::Result<()> {
         let actor = entry.remove();
         let claims = actor.pool.claims().context("claims missing")?;
-        self.delete_claims(claims.clone())
-            .await
-            .context("failed to delete claims")?;
-
         let mut instances = actor.instances.write().await;
         let remaining: usize = instances.values().map(Vec::len).sum();
         stream::iter(instances.drain())
@@ -1683,9 +1679,6 @@ impl Host {
                     }
                     if remaining == 0 {
                         drop(instances);
-                        self.delete_claims(claims.clone())
-                            .await
-                            .context("failed to delete claims")?;
                         entry.remove();
                     }
                 }
@@ -1844,9 +1837,6 @@ impl Host {
                 }
                 if instances.len() == 0 {
                     drop(instances);
-                    self.delete_claims(claims.clone())
-                        .await
-                        .context("failed to delete claims")?;
                     entry.remove();
                 }
             }
@@ -2152,9 +2142,6 @@ impl Host {
             }
         }
         if instances.is_empty() {
-            self.delete_claims(provider.claims.clone())
-                .await
-                .context("failed to delete claims")?;
             entry.remove();
         }
         Ok(SUCCESS.into())
@@ -2518,17 +2505,6 @@ impl Host {
             .put(key, bytes)
             .await
             .context("failed to put claims")?;
-        Ok(())
-    }
-
-    async fn delete_claims<T>(&self, claims: T) -> anyhow::Result<()>
-    where
-        T: TryInto<StoredClaims, Error = anyhow::Error>,
-    {
-        let stored_claims: StoredClaims = claims.try_into()?;
-        let key = format!("CLAIMS_{}", stored_claims.subject);
-
-        self.data.delete(key).await?;
         Ok(())
     }
 
