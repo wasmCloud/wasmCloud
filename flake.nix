df7c71d6435cd18b5ff7e9c4076c843db2e958c0
Roman Volosatovs <rvolosatovs@riseup.net>
2023-11-15 16:04:05 +0100

    build(nix): add crane cache
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   nixConfig.extra-substituters = [
     "https://wasmcloud.cachix.org"
+    "https://crane.cachix.org"
     "https://bytecodealliance.cachix.org"
     "https://nix-community.cachix.org"
     "https://cache.garnix.io"
   ];
   nixConfig.extra-trusted-public-keys = [
     "wasmcloud.cachix.org-1:9gRBzsKh+x2HbVVspreFg/6iFRiD4aOcUQfXVDl3hiM="
+    "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
     "bytecodealliance.cachix.org-1:0SBgh//n2n0heh0sDFhTm+ZKBRy2sInakzFGfzN531Y="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
