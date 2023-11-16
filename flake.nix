2e2c2f1b9c9c9f3623b4a76b6db1f1b4052102c6
Roman Volosatovs <rvolosatovs@riseup.net>
2023-11-16 18:19:37 +0100

    build: add `nixify` cache
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   nixConfig.extra-substituters = [
     "https://wasmcloud.cachix.org"
+    "https://nixify.cachix.org"
     "https://crane.cachix.org"
     "https://bytecodealliance.cachix.org"
     "https://nix-community.cachix.org"
@@ -8,6 +9,7 @@
   ];
   nixConfig.extra-trusted-public-keys = [
     "wasmcloud.cachix.org-1:9gRBzsKh+x2HbVVspreFg/6iFRiD4aOcUQfXVDl3hiM="
+    "nixify.cachix.org-1:95SiUQuf8Ij0hwDweALJsLtnMyv/otZamWNRp1Q1pXw="
     "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
     "bytecodealliance.cachix.org-1:0SBgh//n2n0heh0sDFhTm+ZKBRy2sInakzFGfzN531Y="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
