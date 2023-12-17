106043989350cc79bc8166ad102781a514b79deb
Yordis Prieto <yordis.prieto@gmail.com>
2023-12-17 13:04:59 -0500

    chore: add playwright setup
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

---
+++ washboard-ui/playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const PORT = 5173;
+
+export default defineConfig({
+  testDir: './tests/playwright',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: `http://localhost:${PORT}`,
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  webServer: {
+    command: `npm run dev -- --port=${PORT}`,
+    timeout: 10 * 1000,
+    port: PORT,
+    reuseExistingServer: !process.env.CI,
+  },
+});
---
+++ washboard-ui/tests/playwright/home.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test';
+
+test('home page has the the proper title', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle('wasmCloud UI | Washboard 🏄');
+});
