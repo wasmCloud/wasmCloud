713e7746a9aa61ceff373cf62cf85e68617fa788
Roman Volosatovs <rvolosatovs@riseup.net>
2024-02-20 20:58:14 +0100

    feat: add (most of) custom interface plumbing
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/runtime/src/actor/component/bus.rs
+++ crates/runtime/src/actor/component/bus.rs
@@ -1,21 +1,14 @@
 use super::{Ctx, Instance, TableResult};
 
-use crate::capability::bus::{guest_config, host, lattice};
+use crate::capability::bus::{guest_config, lattice};
 use crate::capability::{Bus, TargetInterface};
-
-use core::future::Future;
-use core::pin::Pin;
 
 use std::sync::Arc;
 
-use anyhow::{anyhow, bail, Context as _};
+use anyhow::{anyhow, Context as _};
 use async_trait::async_trait;
-use futures::future::Shared;
-use futures::FutureExt;
 use tracing::instrument;
-use wasmtime::component::{Resource, ResourceTable};
-use wasmtime_wasi::preview2::pipe::{AsyncReadStream, AsyncWriteStream};
-use wasmtime_wasi::preview2::{HostOutputStream, InputStream, OutputStream, Pollable};
+use wasmtime::component::Resource;
 
 impl Instance {
     /// Set [`Bus`] handler for this [Instance].
@@ -25,117 +18,11 @@ impl Instance {
     }
 }
 
-type FutureResult = Pin<Box<dyn Future<Output = Result<(), String>> + Send>>;
-
-trait TableHostExt {
-    fn push_future_result(&mut self, res: FutureResult) -> TableResult<u32>;
-    fn get_future_result(&self, res: u32) -> TableResult<Box<Shared<FutureResult>>>;
-    fn delete_future_result(&mut self, res: u32) -> TableResult<Box<Shared<FutureResult>>>;
-}
-
-impl TableHostExt for ResourceTable {
-    fn push_future_result(&mut self, res: FutureResult) -> TableResult<u32> {
-        let res = self.push(Box::new(res.shared()))?;
-        Ok(res.rep())
-    }
-    fn get_future_result(&self, res: u32) -> TableResult<Box<Shared<FutureResult>>> {
-        self.get(&Resource::new_borrow(res)).cloned()
-    }
-    fn delete_future_result(&mut self, res: u32) -> TableResult<Box<Shared<FutureResult>>> {
-        self.delete(Resource::new_own(res))
-    }
-}
-
-#[async_trait]
-impl host::Host for Ctx {
-    #[instrument]
-    async fn call(
-        &mut self,
-        target: Option<host::TargetEntity>,
-        operation: String,
-    ) -> anyhow::Result<
-        Result<
-            (
-                host::FutureResult,
-                Resource<InputStream>,
-                Resource<OutputStream>,
-            ),
-            String,
-        >,
-    > {
-        let target = target
-            .map(TryInto::try_into)
-            .transpose()
-            .context("failed to parse target")?;
-        match self.handler.call(target, operation).await {
-            Ok((result, stdin, stdout)) => {
-                let result = self
-                    .table
-                    .push_future_result(result)
-                    .context("failed to push result to table")?;
-                let stdin: Box<dyn HostOutputStream> =
-                    Box::new(AsyncWriteStream::new(1 << 16, stdin));
-                let stdin = self
-                    .table
-                    .push(stdin)
-                    .context("failed to push stdin stream")?;
-                let stdout = self
-                    .table
-                    .push(InputStream::Host(Box::new(AsyncReadStream::new(stdout))))
-                    .context("failed to push stdout stream")?;
-                Ok(Ok((result, stdout, stdin)))
-            }
-            Err(err) => Ok(Err(format!("{err:#}"))),
-        }
-    }
-
-    #[instrument(skip(self, payload))]
-    async fn call_sync(
-        &mut self,
-        target: Option<host::TargetEntity>,
-        operation: String,
-        payload: Vec<u8>,
-    ) -> anyhow::Result<Result<Vec<u8>, String>> {
-        let target = target
-            .map(TryInto::try_into)
-            .transpose()
-            .context("failed to parse target")?;
-        match self.handler.call_sync(target, operation, payload).await {
-            Ok(res) => Ok(Ok(res)),
-            Err(err) => Ok(Err(format!("{err:#}"))),
-        }
-    }
-
-    #[instrument]
-    async fn listen_to_future_result(&mut self, _res: u32) -> anyhow::Result<Resource<Pollable>> {
-        bail!("not supported") // TODO: Support
-    }
-
-    #[instrument]
-    async fn future_result_get(&mut self, res: u32) -> anyhow::Result<Option<Result<(), String>>> {
-        let fut = self.table.get_future_result(res)?;
-        if let Some(result) = fut.clone().now_or_never() {
-            let fut = self.table.delete_future_result(res)?;
-            drop(fut);
-            Ok(Some(result))
-        } else {
-            Ok(None)
-        }
-    }
-
-    #[instrument]
-    async fn drop_future_result(&mut self, res: u32) -> anyhow::Result<()> {
-        let fut = self.table.delete_future_result(res)?;
-        drop(fut);
-        Ok(())
-    }
-}
-
 #[async_trait]
 impl lattice::Host for Ctx {
     async fn set_target(
         &mut self,
-        target: Option<host::TargetEntity>,
+        target: Option<lattice::TargetEntity>,
         interfaces: Vec<Resource<TargetInterface>>,
     ) -> anyhow::Result<()> {
         let interfaces = interfaces
