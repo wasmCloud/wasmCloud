4bc6840143aac172d82e244a81ad1da17215046e
Brooks Townsend <brooksmtownsend@gmail.com>
2024-02-23 12:39:52 -0500

    feat(host)!: flatten instances on actor/providers
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    polish wasmbus
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    require unique provider ID
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    address PR comments for host
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/host/src/wasmbus/event.rs
+++ crates/host/src/wasmbus/event.rs
@@ -1,6 +1,6 @@
 use core::num::NonZeroUsize;
 
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 
 use anyhow::Context;
 use cloudevents::{EventBuilder, EventBuilderV10};
@@ -126,57 +126,53 @@ pub fn actor_scale_failed(
 }
 
 pub fn linkdef_set(
-    id: impl AsRef<str>,
-    actor_id: impl AsRef<str>,
-    provider_id: impl AsRef<str>,
-    link_name: impl AsRef<str>,
-    contract_id: impl AsRef<str>,
-    values: &HashMap<String, String>,
+    source_id: impl AsRef<str>,
+    target: impl AsRef<str>,
+    name: impl AsRef<str>,
+    wit_namespace: impl AsRef<str>,
+    wit_package: impl AsRef<str>,
+    interfaces: &Vec<String>,
+    source_config: &Vec<String>,
+    target_config: &Vec<String>,
 ) -> serde_json::Value {
     json!({
-        "id": id.as_ref(),
-        "actor_id": actor_id.as_ref(),
-        "provider_id": provider_id.as_ref(),
-        "link_name": link_name.as_ref(),
-        "contract_id": contract_id.as_ref(),
-        "values": values,
+        "source_id": source_id.as_ref(),
+        "target": target.as_ref(),
+        "name": name.as_ref(),
+        "wit_namespace": wit_namespace.as_ref(),
+        "wit_package": wit_package.as_ref(),
+        "interfaces": interfaces,
+        "source_config": source_config,
+        "target_config": target_config,
     })
 }
 
 pub fn linkdef_deleted(
-    id: impl AsRef<str>,
-    actor_id: impl AsRef<str>,
-    provider_id: impl AsRef<str>,
-    link_name: impl AsRef<str>,
-    contract_id: impl AsRef<str>,
-    values: &HashMap<String, String>,
+    source_id: impl AsRef<str>,
+    name: impl AsRef<str>,
+    wit_namespace: impl AsRef<str>,
+    wit_package: impl AsRef<str>,
 ) -> serde_json::Value {
     json!({
-        "id": id.as_ref(),
-        "actor_id": actor_id.as_ref(),
-        "provider_id": provider_id.as_ref(),
-        "link_name": link_name.as_ref(),
-        "contract_id": contract_id.as_ref(),
-        "values": values,
+        "source_id": source_id.as_ref(),
+        "name": name.as_ref(),
+        "wit_namespace": wit_namespace.as_ref(),
+        "wit_package": wit_package.as_ref(),
     })
 }
 
 pub fn provider_started(
     claims: &jwt::Claims<jwt::CapabilityProvider>,
     annotations: &BTreeMap<String, String>,
-    instance_id: Uuid,
     host_id: impl AsRef<str>,
     image_ref: impl AsRef<str>,
-    link_name: impl AsRef<str>,
+    provider_id: impl AsRef<str>,
 ) -> serde_json::Value {
     let metadata = claims.metadata.as_ref();
     json!({
         "host_id": host_id.as_ref(),
-        "public_key": claims.subject,
         "image_ref": image_ref.as_ref(),
-        "link_name": link_name.as_ref(),
-        "contract_id": metadata.map(|jwt::CapabilityProvider { capid, .. }| capid),
-        "instance_id": instance_id,
+        "provider_id": provider_id.as_ref(),
         "annotations": annotations,
         "claims": {
             "issuer": &claims.issuer,
@@ -186,38 +182,46 @@ pub fn provider_started(
             "not_before_human": "TODO",
             "expires_human": "TODO",
         },
+        // TODO(#1548): remove these fields when we don't depend on them
+        "instance_id": provider_id.as_ref(),
+        "public_key": provider_id.as_ref(),
+        "link_name": "default",
+        "contract_id": metadata.map(|jwt::CapabilityProvider { capid, .. }| capid),
     })
 }
 
 pub fn provider_start_failed(
     provider_ref: impl AsRef<str>,
-    link_name: impl AsRef<str>,
+    provider_id: impl AsRef<str>,
     error: &anyhow::Error,
 ) -> serde_json::Value {
     json!({
         "provider_ref": provider_ref.as_ref(),
-        "link_name": link_name.as_ref(),
+        "provider_id": provider_id.as_ref(),
         "error": format!("{error:#}"),
+        // TODO(#1548): remove this field when we don't depend on it
+        "link_name": "default",
     })
 }
 
 pub fn provider_stopped(
     claims: &jwt::Claims<jwt::CapabilityProvider>,
     annotations: &BTreeMap<String, String>,
-    instance_id: Uuid,
     host_id: impl AsRef<str>,
-    link_name: impl AsRef<str>,
+    provider_id: impl AsRef<str>,
     reason: impl AsRef<str>,
 ) -> serde_json::Value {
     let metadata = claims.metadata.as_ref();
     json!({
         "host_id": host_id.as_ref(),
-        "public_key": claims.subject,
-        "link_name": link_name.as_ref(),
-        "contract_id": metadata.map(|jwt::CapabilityProvider { capid, .. }| capid),
-        "instance_id": instance_id,
+        "provider_id": provider_id.as_ref(),
         "annotations": annotations,
         "reason": reason.as_ref(),
+        // TODO(#1548): remove these fields when we don't depend on them
+        "instance_id": provider_id.as_ref(),
+        "public_key": provider_id.as_ref(),
+        "link_name": "default",
+        "contract_id": metadata.map(|jwt::CapabilityProvider { capid, .. }| capid),
     })
 }
 
