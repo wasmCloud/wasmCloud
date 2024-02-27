713a90ef1aa30f918357b5fcd711c2063fc17199
Brooks Townsend <brooksmtownsend@gmail.com>
2024-02-27 13:32:33 -0500

    chore(lattice-controller)!: comment out outdated ctl functionality
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/providers/lattice-controller/src/bin/lattice-controller.rs
+++ crates/providers/lattice-controller/src/bin/lattice-controller.rs
@@ -1,12 +1,13 @@
-use wasmcloud_provider_wit_bindgen::deps::wasmcloud_provider_sdk;
+// TODO(brooksmtownsend): bring the lattice control capability provider up-to-date with the control interface
+// use wasmcloud_provider_wit_bindgen::deps::wasmcloud_provider_sdk;
 
-use wasmcloud_provider_lattice_controller::LatticeControllerProvider;
+// use wasmcloud_provider_lattice_controller::LatticeControllerProvider;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    wasmcloud_provider_sdk::start_provider(
-        LatticeControllerProvider::with_cache_timeout_minutes(600),
-        Some("lattice-control-provider".to_string()),
-    )?;
+    // wasmcloud_provider_sdk::start_provider(
+    //     LatticeControllerProvider::with_cache_timeout_minutes(600),
+    //     Some("lattice-control-provider".to_string()),
+    // )?;
 
     eprintln!("Lattice Controller capability provider exiting");
     Ok(())
