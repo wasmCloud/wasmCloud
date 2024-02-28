4319df1ed33ea9ff2ec3a13788523c07a425f040
Brooks Townsend <brooksmtownsend@gmail.com>
2024-02-28 11:31:28 -0500

    test(wrpc): update component test with robust types
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- tests/actors/rust/wrpc-pinger-component/src/lib.rs
+++ tests/actors/rust/wrpc-pinger-component/src/lib.rs
@@ -1,17 +1,31 @@
 wit_bindgen::generate!({
-    world: "test",
+    world: "actor",
     exports: {
-        "wrpc:testing/invoke": Component,
+        "wasmcloud:testing/invoke": Actor,
     },
 });
 
-use exports::wrpc::testing::invoke::Guest;
+use crate::wasmcloud::testing::*;
+use exports::wasmcloud::testing::invoke::Guest;
 
-struct Component;
+struct Actor;
 
-impl Guest for Component {
+impl Guest for Actor {
     fn call() -> String {
-        let pong = crate::wrpc::testing::pingpong::ping();
-        format!("Ping {pong}")
+        // No args, return string
+        let pong = pingpong::ping();
+        // Number arg, return number
+        let meaning_of_universe = busybox::increment_number(41);
+        // Multiple args, return vector of strings
+        let other: Vec<String> = busybox::string_split("hi,there,friend", ',');
+        // Variant / Enum argument, return bool
+        let is_same = busybox::string_assert(busybox::Easyasonetwothree::A, "a");
+        let doggo = busybox::Dog {
+            name: "Archie".to_string(),
+            age: 3,
+        };
+        // Record / struct argument
+        let is_good_boy = busybox::is_good_boy(&doggo);
+        format!("Ping {pong}, meaning of universe is: {meaning_of_universe}, split: {other:?}, is_same: {is_same}, archie good boy: {is_good_boy}")
     }
 }
