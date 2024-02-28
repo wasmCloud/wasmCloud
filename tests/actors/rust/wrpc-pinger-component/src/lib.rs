edbe8fa883d3ab031a5f65addd56f8cb6cea18ad
Roman Volosatovs <rvolosatovs@riseup.net>
2024-02-28 22:02:54 +0100

    feat: implement `wrpc:http/outgoing-handler.handle`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- tests/actors/rust/wrpc-pinger-component/src/lib.rs
+++ tests/actors/rust/wrpc-pinger-component/src/lib.rs
@@ -3,15 +3,54 @@ wit_bindgen::generate!({
     exports: {
         "wasmcloud:testing/invoke": Actor,
     },
+    with: {
+        "wasi:io/streams@0.2.0": wasmcloud_actor::wasi::io::streams,
+    }
 });
 
-use crate::wasmcloud::testing::*;
-use exports::wasmcloud::testing::invoke::Guest;
+use std::io::{Read, Write};
+
+use wasi::http;
+use wasi::io::poll::poll;
+use wasmcloud::testing::*;
+use wasmcloud_actor::{InputStreamReader, OutputStreamWriter};
 
 struct Actor;
 
-impl Guest for Actor {
+impl exports::wasmcloud::testing::invoke::Guest for Actor {
     fn call() -> String {
+        let http_request = http::types::OutgoingRequest::new(http::types::Fields::new());
+        http_request
+            .set_method(&http::types::Method::Put)
+            .expect("failed to set request method");
+        http_request
+            .set_path_with_query(Some("/test"))
+            .expect("failed to set request path with query");
+        http_request
+            .set_scheme(Some(&http::types::Scheme::Https))
+            .expect("failed to set request scheme");
+        http_request
+            .set_authority(Some("localhost:4242"))
+            .expect("failed to set request authority");
+        let http_request_body = http_request
+            .body()
+            .expect("failed to get outgoing request body");
+        {
+            let mut stream = http_request_body
+                .write()
+                .expect("failed to get outgoing request stream");
+            let mut w = OutputStreamWriter::from(&mut stream);
+            w.write_all(b"test")
+                .expect("failed to write `test` to outgoing request stream");
+            w.flush().expect("failed to flush outgoing request stream");
+        }
+        http::types::OutgoingBody::finish(http_request_body, None)
+            .expect("failed to finish sending request body");
+
+        let http_response = http::outgoing_handler::handle(http_request, None)
+            .expect("failed to handle HTTP request");
+        let http_response_sub = http_response.subscribe();
+
         // No args, return string
         let pong = pingpong::ping();
         // Number arg, return number
@@ -26,6 +65,33 @@ impl Guest for Actor {
         };
         // Record / struct argument
         let is_good_boy = busybox::is_good_boy(&doggo);
+
+        assert_eq!(poll(&[&http_response_sub]), [0]);
+        let http_response = http_response
+            .get()
+            .expect("HTTP request response missing")
+            .expect("HTTP request response requested more than once")
+            .expect("HTTP request failed");
+        assert_eq!(http_response.status(), 200);
+
+        // TODO: Assert headers
+        _ = http_response.headers();
+        let http_response_body = http_response
+            .consume()
+            .expect("failed to get incoming request body");
+        {
+            let mut buf = vec![];
+            let mut stream = http_response_body
+                .stream()
+                .expect("failed to get HTTP request response stream");
+            InputStreamReader::from(&mut stream)
+                .read_to_end(&mut buf)
+                .expect("failed to read value from HTTP request response stream");
+            assert_eq!(buf, b"test");
+        };
+        // TODO: Assert trailers
+        let _trailers = http::types::IncomingBody::finish(http_response_body);
+
         format!("Ping {pong}, meaning of universe is: {meaning_of_universe}, split: {other:?}, is_same: {is_same}, archie good boy: {is_good_boy}")
     }
 }
--- tests/wrpc.rs
+++ tests/wrpc.rs
@@ -1,9 +1,16 @@
+use core::time::Duration;
+
 use anyhow::Context;
+use futures::stream;
+use futures::TryStreamExt as _;
 use test_actors::{RUST_WRPC_PINGER_COMPONENT, RUST_WRPC_PONGER_COMPONENT};
+use tokio::try_join;
+use tracing::info;
+use tracing_subscriber::prelude::*;
 use wasmcloud_test_util::{
     actor::assert_scale_actor, host::WasmCloudTestHost, lattice::link::assert_advertise_link,
 };
-use wrpc_transport::Client;
+use wrpc_transport::{AcceptedInvocation, Client as _, Transmitter as _};
 
 pub mod common;
 use common::nats::start_nats;
@@ -14,6 +21,15 @@ const PONGER_COMPONENT_ID: &str = "wrpc_ponger_component";
 
 #[tokio::test]
 async fn wrpc() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().pretty().without_time())
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                tracing_subscriber::EnvFilter::new("info,cranelift_codegen=warn,wasmcloud=trace")
+            }),
+        )
+        .init();
+
     // Start NATS server
     let (nats_server, nats_url, nats_client) =
         start_nats().await.expect("should be able to start NATS");
@@ -21,12 +37,19 @@ async fn wrpc() -> anyhow::Result<()> {
     let ctl_client = wasmcloud_control_interface::ClientBuilder::new(nats_client.clone())
         .lattice(LATTICE.to_string())
         .build();
-    let wrpc_client =
-        wrpc_transport_nats::Client::new(nats_client, format!("{LATTICE}.{PINGER_COMPONENT_ID}"));
+    let wrpc_client = wrpc_transport_nats::Client::new(
+        nats_client.clone(),
+        format!("{LATTICE}.{PINGER_COMPONENT_ID}"),
+    );
     // Build the host
     let host = WasmCloudTestHost::start(&nats_url, LATTICE, None, None)
         .await
         .context("failed to start test host")?;
+
+    let mut outgoing_http_invocations =
+        wrpc_interface_http::OutgoingHandler::serve_handle(&wrpc_client)
+            .await
+            .context("failed to serve `wrpc:http/outgoing-handler` invocations")?;
 
     // Scale pinger
     assert_scale_actor(
@@ -67,25 +90,122 @@ async fn wrpc() -> anyhow::Result<()> {
     .await
     .expect("should advertise link");
 
-    let result = wrpc_client
-        .invoke_dynamic(
-            "wasmcloud:testing/invoke",
-            "call",
-            (),
-            &[wrpc_types::Type::String],
-        )
-        .await;
-    match result {
-        Ok((values, _tx)) => {
-            if let Some(wrpc_transport::Value::String(result)) = values.first() {
-                assert_eq!(result, "Ping pong, meaning of universe is: 42, split: [\"hi\", \"there\", \"friend\"], is_same: true, archie good boy: true");
-            } else {
-                panic!("Got something other than a string from the component")
-            }
-        }
-        _ => panic!("Error"),
-    }
+    // Link pinger --wasi:http/outgoing-handler--> pinger
+    assert_advertise_link(
+        &ctl_client,
+        PINGER_COMPONENT_ID,
+        PINGER_COMPONENT_ID,
+        "default",
+        "wasi",
+        "http",
+        vec!["outgoing-handler".to_string()],
+        vec![],
+        vec![],
+    )
+    .await
+    .expect("should advertise link");
 
+    try_join!(
+        async {
+            let (results, tx) = wrpc_client
+                .invoke_static::<String>("wasmcloud:testing/invoke", "call", ())
+                .await
+                .context("invocation failed")?;
+            tx.await.context("failed to transmit parameters")?;
+            assert_eq!(
+                results,
+                r#"Ping pong, meaning of universe is: 42, split: ["hi", "there", "friend"], is_same: true, archie good boy: true"#
+            );
+            anyhow::Ok(())
+        },
+        async {
+            let AcceptedInvocation {
+                params:
+                    (
+                        wrpc_interface_http::Request {
+                            mut body,
+                            trailers,
+                            method,
+                            path_with_query,
+                            scheme,
+                            authority,
+                            headers,
+                        },
+                        opts,
+                    ),
+                result_subject,
+                transmitter,
+                ..
+            } = outgoing_http_invocations
+                .try_next()
+                .await
+                .context("failed to accept `wrpc:http/outgoing-handler.handle` invocation")?
+                .context("invocation stream unexpectedly finished")?;
+            assert_eq!(method, wrpc_interface_http::Method::Put);
+            assert_eq!(path_with_query.as_deref(), Some("/test"));
+            assert_eq!(scheme, Some(wrpc_interface_http::Scheme::HTTPS));
+            assert_eq!(authority.as_deref(), Some("localhost:4242"));
+            assert_eq!(
+                headers,
+                vec![("host".into(), vec!["localhost:4242".into()])],
+            );
+            // wasmtime defaults
+            assert_eq!(
+                opts,
+                Some(wrpc_interface_http::RequestOptions {
+                    connect_timeout: Some(Duration::from_secs(600)),
+                    first_byte_timeout: Some(Duration::from_secs(600)),
+                    between_bytes_timeout: Some(Duration::from_secs(600)),
+                })
+            );
+            try_join!(
+                async {
+                    info!("transmit response");
+                    transmitter
+                        .transmit_static(
+                            result_subject,
+                            Ok::<_, wrpc_interface_http::ErrorCode>(
+                                wrpc_interface_http::Response {
+                                    body: stream::iter([("test".into())]),
+                                    trailers: async { None },
+                                    status: 200,
+                                    headers: Vec::default(),
+                                },
+                            ),
+                        )
+                        .await
+                        .context("failed to transmit response")?;
+                    info!("response transmitted");
+                    anyhow::Ok(())
+                },
+                async {
+                    info!("await request body element");
+                    let item = body
+                        .try_next()
+                        .await
+                        .context("failed to receive body item")?
+                        .context("unexpected end of body stream")?;
+                    assert_eq!(String::from_utf8(item).unwrap(), "test");
+                    info!("await request body end");
+                    let item = body
+                        .try_next()
+                        .await
+                        .context("failed to receive end item")?;
+                    assert_eq!(item, None);
+                    info!("request body verified");
+                    Ok(())
+                },
+                async {
+                    info!("await request trailers");
+                    let trailers = trailers.await.context("failed to receive trailers")?;
+                    assert_eq!(trailers, None);
+                    info!("request trailers verified");
+                    Ok(())
+                }
+            )?;
+            Ok(())
+        }
+    )?;
     nats_server
         .stop()
         .await
