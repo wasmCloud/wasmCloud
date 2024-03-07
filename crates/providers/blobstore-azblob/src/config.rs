8e2e8a55544250adf30ca2f47c6a33cb187219d3
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2024-03-07 15:54:49 -0800

    fix(providers): remove unused comments in azblob::conifg
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- crates/providers/blobstore-azblob/src/config.rs
+++ crates/providers/blobstore-azblob/src/config.rs
@@ -13,44 +13,6 @@ use base64::Engine;
 use azure_storage::StorageCredentials;
 
 /// Configuration for connecting to Azblob.
-///
-/// 
-/// Example from the Azblob sdk
-/// ```
-/// #[tokio::main]
-/// async fn main() -> azure_core::Result<()> {
-///    let file_name = "azure_sdk_for_rust_stream_test.txt";
-///
-///    // First we retrieve the account name and access key from environment variables.
-///    let account = std::env::var("STORAGE_ACCOUNT").expect("missing STORAGE_ACCOUNT");
-///    let access_key = std::env::var("STORAGE_ACCESS_KEY").expect("missing STORAGE_ACCOUNT_KEY");
-///    let container = std::env::var("STORAGE_CONTAINER").expect("missing STORAGE_CONTAINER");
-///    let blob_name = std::env::var("STORAGE_BLOB_NAME").expect("missing STORAGE_BLOB_NAME");
-///
-///    let storage_credentials = StorageCredentials::access_key(account.clone(), access_key);
-///    let blob_client = ClientBuilder::new(account, storage_credentials).blob_client(&container, blob_name);
-///
-///    blob_client.put_block_blob("hello world").content_type("text/plain").await?;
-///
-///    let mut result: Vec<u8> = vec![];
-///
-///    // The stream is composed of individual calls to the get blob endpoint
-///    let mut stream = blob_client.get().into_stream();
-///    while let Some(value) = stream.next().await {
-///        let mut body = value?.data;
-///        // For each response, we stream the body instead of collecting it all
-///        // into one large allocation.
-///        while let Some(value) = body.next().await {
-///            let value = value?;
-///            result.extend(&value);
-///        }
-///    }
-///
-///    println!("result: {:?}", result);
-///
-///    Ok(())
-/// }
-/// ```
 #[derive(Clone, Default, Deserialize)]
 #[serde(crate = "wasmcloud_provider_wit_bindgen::deps::serde")]
 pub struct StorageConfig {
