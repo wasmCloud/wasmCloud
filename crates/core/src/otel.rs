baa1e80b6e39210b43b0dbbadf4a03674f3dd1e0
Joonas Bergius <joonas@cosmonic.com>
2024-03-11 18:58:35 -0500

    chore: Use traces instead of tracing user-facing language to align with OTEL signal names
    Signed-off-by: Joonas Bergius <joonas@cosmonic.com>

--- crates/core/src/otel.rs
+++ crates/core/src/otel.rs
@@ -4,21 +4,21 @@ use serde::{Deserialize, Serialize};
 
 use crate::wit::WitMap;
 
-/// Configuration values for Open Telemetry
+/// Configuration values for OpenTelemetry
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct OtelConfig {
     /// Determine whether observability should be enabled.
     pub enable_observability: bool,
-    /// Determine whether tracing should be enabled.
-    pub enable_tracing: Option<bool>,
+    /// Determine whether traces should be enabled.
+    pub enable_traces: Option<bool>,
     /// Determine whether metrics should be enabled.
     pub enable_metrics: Option<bool>,
     /// Determine whether logs should be enabled.
     pub enable_logs: Option<bool>,
     /// Overrides the OpenTelemetry endpoint for all signals.
     pub observability_endpoint: Option<String>,
-    /// Overrides the OpenTelemetry endpoint for tracing.
-    pub tracing_endpoint: Option<String>,
+    /// Overrides the OpenTelemetry endpoint for traces.
+    pub traces_endpoint: Option<String>,
     /// Overrides the OpenTelemetry endpoint for metrics.
     pub metrics_endpoint: Option<String>,
     /// Overrides the OpenTelemetry endpoint for logs.
