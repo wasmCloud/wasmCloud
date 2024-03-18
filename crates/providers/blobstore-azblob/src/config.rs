8d9a17f9b399413f24c865647a021d9898ae4fed
Brooks Townsend <brooksmtownsend@gmail.com>
2024-03-18 12:47:10 -0400

    feat(blobstore-azure): implement new provider sdk, wrpc:blobstore
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/providers/blobstore-azblob/src/config.rs
+++ crates/providers/blobstore-azblob/src/config.rs
@@ -4,17 +4,14 @@
 //! and EC2 IAM authorizations.
 //!
 use std::collections::HashMap;
-use std::env;
 
-use anyhow::{Context, Result};
-use wasmcloud_provider_wit_bindgen::deps::{serde::Deserialize, serde_json};
-use base64::Engine;
+use anyhow::Result;
+use serde::Deserialize;
 
 use azure_storage::StorageCredentials;
 
 /// Configuration for connecting to Azblob.
 #[derive(Clone, Default, Deserialize)]
-#[serde(crate = "wasmcloud_provider_wit_bindgen::deps::serde")]
 pub struct StorageConfig {
     /// STORAGE_ACCOUNT, can be specified from environment
     pub storage_account: String,
@@ -23,35 +20,20 @@ pub struct StorageConfig {
     pub storage_access_key: String,
 }
 
-
 impl StorageConfig {
     pub fn from_values(values: &HashMap<String, String>) -> Result<StorageConfig> {
-        let mut config = if let Some(config_b64) = values.get("config_b64") {
-            let bytes = base64::engine::general_purpose::STANDARD
-                .decode(config_b64.as_bytes())
-                .context("invalid base64 encoding")?;
-            serde_json::from_slice::<StorageConfig>(&bytes).context("corrupt config_b64")?
-        } else if let Some(config) = values.get("config_json") {
-            serde_json::from_str::<StorageConfig>(config).context("corrupt config_json")?
-        } else {
-            StorageConfig::default()
-        };
-
-        if let Some(env_file) = values.get("env") {
-            let data = std::fs::read_to_string(env_file)
-                .with_context(|| format!("reading env file '{env_file}'"))?;
-            simple_env_load::parse_and_set(&data, |k, v| std::env::set_var(k, v));
+        match (
+            values.get("STORAGE_ACCOUNT"),
+            values.get("STORAGE_ACCESS_KEY"),
+        ) {
+            (Some(account), Some(access_key)) => Ok(StorageConfig {
+                storage_account: account.to_string(),
+                storage_access_key: access_key.to_string(),
+            }),
+            _ => Err(anyhow::anyhow!(
+                "STORAGE_ACCOUNT and STORAGE_ACCESS_KEY must be set"
+            )),
         }
-        
-        if let Ok(account) = env::var("STORAGE_ACCOUNT") {
-            config.storage_account = account;
-        }
-
-        if let Ok(access_key) = env::var("STORAGE_ACCESS_KEY") {
-            config.storage_access_key = access_key;
-        }
-
-        Ok(config)
     }
 
     pub fn configure_az(self) -> StorageCredentials {
