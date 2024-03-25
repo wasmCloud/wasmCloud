946befa58655065f1919dd39acb1c8bab195782b
Victor Adossi <vadossi@cosmonic.com>
2024-03-26 03:42:41 +0900

    refactor(vendor): remove --capid from old makefiles
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- vendor/capability-providers/sqldb-dynamodb/provider.mk
+++ vendor/capability-providers/sqldb-dynamodb/provider.mk
@@ -84,7 +84,6 @@ $(dest_par): $(bin_target0) Makefile Cargo.toml
 	$(WASH) par create \
 		--arch $(ARCH_LOOKUP_$(par_target0)) \
 		--binary $(bin_target0) \
-		--capid $(CAPABILITY_ID) \
 		--name $(NAME) \
 		--vendor $(VENDOR) \
 		--version $(VERSION) \
