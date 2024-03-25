77f0700abe8ddc57055d460eb77a7b759097748d
Victor Adossi <vadossi@cosmonic.com>
2024-03-26 03:18:53 +0900

    refactor(runtime): wascap::jwt::Actor -> wascap::jwt::Component
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/runtime/src/actor/mod.rs
+++ crates/runtime/src/actor/mod.rs
@@ -6,23 +6,27 @@ pub use component::{
 
 use core::fmt::Debug;
 
-use anyhow::{ensure, Context};
+use anyhow::{ensure, Context, Result};
 use wascap::jwt;
 use wascap::wasm::extract_claims;
 
-/// Actor instance configuration
+/// Component instance configuration
 #[derive(Clone, Debug, Default)]
 pub struct Config {
     /// Whether actors are required to be signed to be executed
     pub require_signature: bool,
 }
 
-/// Extracts and validates claims contained within `WebAssembly` binary, if such are found
-fn claims(wasm: impl AsRef<[u8]>) -> anyhow::Result<Option<jwt::Claims<jwt::Actor>>> {
+/// Extracts and validates claims contained within a WebAssembly binary, if present
+///
+/// # Arguments
+///
+/// * `wasm` - Bytes that consitute a valid WebAssembly binary
+fn claims(wasm: impl AsRef<[u8]>) -> Result<Option<jwt::Claims<jwt::Component>>> {
     let Some(claims) = extract_claims(wasm).context("failed to extract module claims")? else {
         return Ok(None);
     };
-    let v = jwt::validate_token::<jwt::Actor>(&claims.jwt)
+    let v = jwt::validate_token::<jwt::Component>(&claims.jwt)
         .context("failed to validate module token")?;
     ensure!(!v.expired, "token expired at `{}`", v.expires_human);
     ensure!(
