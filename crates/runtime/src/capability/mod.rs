90171f6f2b93fa789a7e23aa2fb319ef23800117
Roman Volosatovs <rvolosatovs@riseup.net>
2024-03-25 13:15:23 +0100

    feat: update to wasmtime 19
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/runtime/src/capability/mod.rs
+++ crates/runtime/src/capability/mod.rs
@@ -15,8 +15,6 @@ pub use wasmcloud_core::CallTargetInterface;
 #[allow(clippy::doc_markdown)]
 #[allow(missing_docs)]
 mod bindgen {
-    use wasmtime_wasi::preview2;
-
     mod keyvalue {
         pub type Bucket = std::sync::Arc<String>;
         pub type IncomingValue = wrpc_transport::IncomingInputStream;
@@ -39,27 +37,27 @@ mod bindgen {
            "wasi:blobstore/container/stream-object-names": blobstore::StreamObjectNames,
            "wasi:blobstore/types/incoming-value": blobstore::IncomingValue,
            "wasi:blobstore/types/outgoing-value": blobstore::OutgoingValue,
-           "wasi:cli/environment": preview2::bindings::cli::environment,
-           "wasi:cli/exit": preview2::bindings::cli::exit,
-           "wasi:cli/preopens": preview2::bindings::cli::preopens,
-           "wasi:cli/stderr": preview2::bindings::cli::stderr,
-           "wasi:cli/stdin": preview2::bindings::cli::stdin,
-           "wasi:cli/stdout": preview2::bindings::cli::stdout,
-           "wasi:clocks/monotonic-clock": preview2::bindings::clocks::monotonic_clock,
-           "wasi:clocks/timezone": preview2::bindings::clocks::timezone,
-           "wasi:clocks/wall_clock": preview2::bindings::clocks::wall_clock,
-           "wasi:filesystem/filesystem": preview2::bindings::filesystem::filesystem,
+           "wasi:cli/environment": wasmtime_wasi::bindings::cli::environment,
+           "wasi:cli/exit": wasmtime_wasi::bindings::cli::exit,
+           "wasi:cli/preopens": wasmtime_wasi::bindings::cli::preopens,
+           "wasi:cli/stderr": wasmtime_wasi::bindings::cli::stderr,
+           "wasi:cli/stdin": wasmtime_wasi::bindings::cli::stdin,
+           "wasi:cli/stdout": wasmtime_wasi::bindings::cli::stdout,
+           "wasi:clocks/monotonic-clock": wasmtime_wasi::bindings::clocks::monotonic_clock,
+           "wasi:clocks/timezone": wasmtime_wasi::bindings::clocks::timezone,
+           "wasi:clocks/wall_clock": wasmtime_wasi::bindings::clocks::wall_clock,
+           "wasi:filesystem/filesystem": wasmtime_wasi::bindings::filesystem::filesystem,
            "wasi:http/incoming-handler": wasmtime_wasi_http::bindings::http::incoming_handler,
            "wasi:http/outgoing-handler": wasmtime_wasi_http::bindings::http::incoming_handler,
            "wasi:http/types": wasmtime_wasi_http::bindings::http::types,
-           "wasi:io/error": preview2::bindings::io::error,
-           "wasi:io/poll": preview2::bindings::io::poll,
-           "wasi:io/streams": preview2::bindings::io::streams,
+           "wasi:io/error": wasmtime_wasi::bindings::io::error,
+           "wasi:io/poll": wasmtime_wasi::bindings::io::poll,
+           "wasi:io/streams": wasmtime_wasi::bindings::io::streams,
            "wasi:keyvalue/types/bucket": keyvalue::Bucket,
            "wasi:keyvalue/types/incoming-value": keyvalue::IncomingValue,
            "wasi:keyvalue/types/outgoing-value": keyvalue::OutgoingValue,
            "wasi:keyvalue/wasi-keyvalue-error/error": keyvalue::Error,
-           "wasi:random/random": preview2::bindings::random::random,
+           "wasi:random/random": wasmtime_wasi::bindings::random::random,
            "wasmcloud:bus/lattice/call-target-interface": super::CallTargetInterface,
         },
     });
