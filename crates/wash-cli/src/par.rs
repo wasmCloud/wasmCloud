9f75d9c15ae006213f50c0fe485ad2132fe2724e
Victor Adossi <vadossi@cosmonic.com>
2024-03-26 03:43:29 +0900

    fix(wash-lib)!: remove usage of capability signing
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/src/par.rs
+++ crates/wash-cli/src/par.rs
@@ -30,10 +30,6 @@ pub enum ParCliCommand {
 
 #[derive(Parser, Debug, Clone)]
 pub struct CreateCommand {
-    /// Capability contract ID (e.g. wasmcloud:messaging or wasmcloud:keyvalue).
-    #[clap(short = 'c', long = "capid")]
-    capid: String,
-
     /// Vendor string to help identify the publisher of the provider (e.g. Redis, Cassandra, wasmcloud, etc). Not unique.
     #[clap(short = 'v', long = "vendor")]
     vendor: String,
@@ -213,7 +209,6 @@ impl From<InspectCommand> for inspect::InspectCliCommand {
 impl From<CreateCommand> for par::ParCreateArgs {
     fn from(cmd: CreateCommand) -> Self {
         par::ParCreateArgs {
-            capid: cmd.capid,
             vendor: cmd.vendor,
             revision: cmd.revision,
             version: cmd.version,
@@ -374,8 +369,6 @@ mod test {
             "x86_64-testrunner",
             "--binary",
             "./testrunner.so",
-            "--capid",
-            "wasmcloud:test",
             "--name",
             "CreateTest",
             "--vendor",
@@ -398,7 +391,6 @@ mod test {
         .unwrap();
         match create_long.par {
             ParCliCommand::Create(CreateCommand {
-                capid,
                 vendor,
                 revision,
                 version,
@@ -413,7 +405,6 @@ mod test {
                 compress,
                 disable_keygen,
             }) => {
-                assert_eq!(capid, "wasmcloud:test");
                 assert_eq!(arch, "x86_64-testrunner");
                 assert_eq!(binary, "./testrunner.so");
                 assert_eq!(directory.unwrap(), PathBuf::from("./tests/fixtures"));
@@ -459,7 +450,6 @@ mod test {
         .unwrap();
         match create_short.par {
             ParCliCommand::Create(CreateCommand {
-                capid,
                 vendor,
                 revision,
                 version,
@@ -474,7 +464,6 @@ mod test {
                 compress,
                 disable_keygen,
             }) => {
-                assert_eq!(capid, "wasmcloud:test");
                 assert_eq!(arch, "x86_64-testrunner");
                 assert_eq!(binary, "./testrunner.so");
                 assert_eq!(directory.unwrap(), PathBuf::from("./tests/fixtures"));
