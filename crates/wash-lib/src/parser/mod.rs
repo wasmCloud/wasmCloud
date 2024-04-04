cf8c8de7c6a9314aee8070cb060b0b7ff780d69b
Siddharth Khonde <sidconstructs@gmail.com>
2024-04-04 12:03:24 -0400

    fix(wash-lib): changed the variable name for a cleaner code
    Signed-off-by: Siddharth Khonde <sidconstructs@gmail.com>

--- crates/wash-lib/src/parser/mod.rs
+++ crates/wash-lib/src/parser/mod.rs
@@ -110,7 +110,7 @@ impl TryFrom<RawActorConfig> for ActorConfig {
     type Error = anyhow::Error;
 
     fn try_from(raw_config: RawActorConfig) -> Result<Self, Self::Error> {
-        let key_dir = if let Some(key_directory) = raw_config.key_directory {
+        let key_directory = if let Some(key_directory) = raw_config.key_directory {
             key_directory
         } else {
             let home_dir = dirs::home_dir()
@@ -120,7 +120,7 @@ impl TryFrom<RawActorConfig> for ActorConfig {
         Ok(Self {
             claims: raw_config.claims.unwrap_or_default(),
             push_insecure: raw_config.push_insecure.unwrap_or(false),
-            key_directory: key_dir,
+            key_directory,
             wasm_target: raw_config
                 .wasm_target
                 .map(WasmTarget::from)
@@ -176,7 +176,7 @@ impl TryFrom<RawProviderConfig> for ProviderConfig {
     type Error = anyhow::Error;
 
     fn try_from(raw_config: RawProviderConfig) -> Result<Self, Self::Error> {
-        let key_dir = if let Some(key_directory) = raw_config.key_directory {
+        let key_directory = if let Some(key_directory) = raw_config.key_directory {
             key_directory
         } else {
             let home_dir = dirs::home_dir()
@@ -194,7 +194,7 @@ impl TryFrom<RawProviderConfig> for ProviderConfig {
             rust_target: raw_config.rust_target,
             bin_name: raw_config.bin_name,
             wit_world: raw_config.wit_world,
-            key_directory: key_dir,
+            key_directory,
         })
     }
 }
