aec33a80c4060c5bf1c945526982cbbbc6c56cc3
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-04-09 18:16:18 -0400

    feat(lattice-client-core): update client to work with wasmcloud 1.0 and wadm 0.11
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- washboard-ui/packages/washboard-ui/src/settings/use-settings.ts
+++
@@ -1,6 +0,0 @@
-import {useContext} from 'react';
-import {SettingsContext, SettingsContextValue} from './settings-context';
-
-export function useSettings(): SettingsContextValue {
-  return useContext(SettingsContext);
-}
--- washboard-ui/packages/washboard-ui/src/ui/toggle/index.ts
+++
@@ -1,2 +0,0 @@
-export {Toggle} from './toggle';
-export {toggleVariants} from './variants';
