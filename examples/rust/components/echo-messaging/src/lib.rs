de5f8d9d8c6bb5c367c9117306d7fa7755c1f731
Brooks Townsend <brooksmtownsend@gmail.com>
2024-04-11 17:26:14 -0400

    feat: add echo messaging example
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

---
+++ examples/rust/components/echo-messaging/src/lib.rs
@@ -0,0 +1,30 @@
+wit_bindgen::generate!({
+    world: "hello",
+});
+
+use exports::wasmcloud::messaging::handler::Guest;
+use wasi::logging::logging::*;
+use wasmcloud::messaging::*;
+
+struct Echo;
+
+impl Guest for Echo {
+    fn handle_message(msg: types::BrokerMessage) -> Result<(), String> {
+        if let Some(reply_to) = msg.reply_to {
+            consumer::publish(&types::BrokerMessage {
+                subject: reply_to,
+                reply_to: None,
+                body: msg.body,
+            })
+        } else {
+            log(
+                Level::Warn,
+                "",
+                "No reply_to field in message, ignoring message",
+            );
+            Ok(())
+        }
+    }
+}
+
+export!(Echo);
