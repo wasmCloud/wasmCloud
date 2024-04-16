c9da57e2b765000904e4d423dc156bf952d85fac
Brooks Townsend <brooksmtownsend@gmail.com>
2024-04-16 13:06:49 -0400

    fix(example): typescript hello world hang
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/typescript/components/http-hello-world/http-hello-world.ts
+++ examples/typescript/components/http-hello-world/http-hello-world.ts
@@ -1,6 +1,7 @@
 import {
   IncomingRequest,
   ResponseOutparam,
+  OutgoingBody,
   OutgoingResponse,
   Fields,
 } from "wasi:http/types@0.2.0";
@@ -14,16 +15,21 @@ function handle(req: IncomingRequest, resp: ResponseOutparam) {
 
   // Access the outgoing response body
   let outgoingBody = outgoingResponse.body();
-  // Create a stream for the response body
-  let outputStream = outgoingBody.write();
-  // // Write hello world to the response stream
-  outputStream.blockingWriteAndFlush(
-    new Uint8Array(new TextEncoder().encode("Hello from Typescript!\n"))
-  );
+  {
+    // Create a stream for the response body
+    let outputStream = outgoingBody.write();
+    // Write hello world to the response stream
+    outputStream.blockingWriteAndFlush(
+      new Uint8Array(new TextEncoder().encode("Hello from Typescript!\n"))
+    );
+    // @ts-ignore: This is required in order to dispose the stream before we return
+    outputStream[Symbol.dispose]();
+  }
 
   // Set the status code for the response
   outgoingResponse.setStatusCode(200);
-
+  // Finish the response body
+  OutgoingBody.finish(outgoingBody, undefined);
   // Set the created response
   ResponseOutparam.set(resp, { tag: "ok", val: outgoingResponse });
 }
