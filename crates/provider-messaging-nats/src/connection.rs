5fa6db2ba2601977932468ce9ca431cf230ed675
Victor Adossi <vadossi@cosmonic.com>
2024-04-30 19:52:31 +0200

    feat(providers): custom inbox prefix for NATS messaging provider
    This commit enables a custom inbox prefix for the NATS messaging
    provider.
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/provider-messaging-nats/src/connection.rs
+++ crates/provider-messaging-nats/src/connection.rs
@@ -10,6 +10,7 @@ const CONFIG_NATS_URI: &str = "cluster_uris";
 const CONFIG_NATS_CLIENT_JWT: &str = "client_jwt";
 const CONFIG_NATS_CLIENT_SEED: &str = "client_seed";
 const CONFIG_NATS_TLS_CA: &str = "tls_ca";
+const CONFIG_NATS_CUSTOM_INBOX_PREFIX: &str = "custom_inbox_prefix";
 
 /// Configuration for connecting a nats client.
 /// More options are available if you use the json than variables in the values string map.
@@ -31,15 +32,21 @@ pub struct ConnectionConfig {
     #[serde(default)]
     pub auth_seed: Option<String>,
 
+    /// TLS Certificate Authority, encoded as a string
     #[serde(default)]
     pub tls_ca: Option<String>,
 
+    /// TLS Certifiate Authority, as a path on disk
     #[serde(default)]
     pub tls_ca_file: Option<String>,
 
-    /// ping interval in seconds
+    /// Ping interval in seconds
     #[serde(default)]
     pub ping_interval_sec: Option<u16>,
+
+    /// Inbox prefix to use (by default
+    #[serde(default)]
+    pub custom_inbox_prefix: Option<String>,
 }
 
 impl ConnectionConfig {
@@ -71,6 +78,9 @@ impl ConnectionConfig {
         if extra.ping_interval_sec.is_some() {
             out.ping_interval_sec = extra.ping_interval_sec;
         }
+        if extra.custom_inbox_prefix.is_some() {
+            out.custom_inbox_prefix = extra.custom_inbox_prefix.clone();
+        }
         out
     }
 }
@@ -79,12 +89,13 @@ impl Default for ConnectionConfig {
     fn default() -> ConnectionConfig {
         ConnectionConfig {
             subscriptions: vec![],
-            cluster_uris: vec![DEFAULT_NATS_URI.to_string()],
+            cluster_uris: vec![DEFAULT_NATS_URI.into()],
             auth_jwt: None,
             auth_seed: None,
             tls_ca: None,
             tls_ca_file: None,
             ping_interval_sec: None,
+            custom_inbox_prefix: None,
         }
     }
 }
@@ -101,6 +112,9 @@ impl ConnectionConfig {
         }
         if let Some(url) = values.get(CONFIG_NATS_URI) {
             config.cluster_uris = url.split(',').map(String::from).collect();
+        }
+        if let Some(custom_inbox_prefix) = values.get(CONFIG_NATS_CUSTOM_INBOX_PREFIX) {
+            config.custom_inbox_prefix = Some(custom_inbox_prefix.clone());
         }
         if let Some(jwt) = values.get(CONFIG_NATS_CLIENT_JWT) {
             config.auth_jwt = Some(jwt.clone());
