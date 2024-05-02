c1b3b27f233c46b5045208b349bdf3d6578ea0ee
Victor Adossi <vadossi@cosmonic.com>
2024-05-02 08:59:20 -0500

    feat(washboard): reflect new options in LatticeSettings component
    This commit updates the `LatticeSettings` component to reflect the new
    options that are now configurable.
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- washboard-ui/packages/washboard-ui/src/app/components/lattice-settings.tsx
+++ washboard-ui/packages/washboard-ui/src/app/components/lattice-settings.tsx
@@ -20,6 +20,9 @@ const formSchema = z.object({
       },
       {message: 'Could not connect to Lattice'},
     ),
+  latticeId: z.string(),
+  ctlTopicPrefix: z.string(),
+  retryCount: z.number().min(0),
 });
 
 type LatticeFormInput = z.input<typeof formSchema>;
@@ -27,29 +30,38 @@ type LatticeFormInput = z.input<typeof formSchema>;
 type LatticeFormOutput = z.output<typeof formSchema>;
 
 export function LatticeSettings(): ReactElement {
-  const [{latticeUrl}, setConfig] = useLatticeConfig();
+  const [{latticeUrl, latticeId, ctlTopicPrefix, retryCount}, setConfig] = useLatticeConfig();
   const form = useForm<LatticeFormInput, object, LatticeFormOutput>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      latticeUrl: latticeUrl,
+      latticeUrl,
+      latticeId,
+      ctlTopicPrefix,
+      retryCount,
     },
   });
 
-  function onSubmit(data: LatticeFormOutput): void {
+  function onSubmit({latticeUrl, latticeId, ctlTopicPrefix, retryCount}: LatticeFormOutput): void {
     setConfig({
-      latticeUrl: data.latticeUrl,
+      latticeUrl,
+      latticeId,
+      ctlTopicPrefix,
+      retryCount,
     });
   }
 
   useEffect(() => {
     form.setValue('latticeUrl', latticeUrl);
-  }, [form, latticeUrl]);
+    form.setValue('latticeId', latticeId);
+    form.setValue('ctlTopicPrefix', ctlTopicPrefix);
+    form.setValue('retryCount', retryCount);
+  }, [form, latticeUrl, ctlTopicPrefix, latticeId, retryCount]);
 
   const hasErrors = Object.values(form.formState.errors).length > 0;
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
         <FormField
           control={form.control}
           name="latticeUrl"
@@ -58,6 +70,45 @@ export function LatticeSettings(): ReactElement {
               <FormLabel htmlFor="latticeUrl">Server URL</FormLabel>
               <FormControl>
                 <Input type="text" placeholder="ws://server:port" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="latticeId"
+          render={({field}) => (
+            <FormItem className="grid w-full max-w-sm items-center gap-1.5">
+              <FormLabel htmlFor="latticeId">Lattice ID</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="default" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="retryCount"
+          render={({field}) => (
+            <FormItem className="grid w-full max-w-sm items-center gap-1.5">
+              <FormLabel htmlFor="retryCount">Retry Count</FormLabel>
+              <FormControl>
+                <Input type="number" min="0" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="ctlTopicPrefix"
+          render={({field}) => (
+            <FormItem className="grid w-full max-w-sm items-center gap-1.5">
+              <FormLabel htmlFor="ctlTopicPrefix">Control Topic Prefix</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="wasmbus.ctl" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
