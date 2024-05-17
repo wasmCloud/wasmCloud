12d9a4fb9487b9058d756986a2b738c0539babce
Brooks Townsend <brooksmtownsend@gmail.com>
2024-05-17 10:38:49 -0400

    feat(example): show how reqwest simplifies http
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/rust/components/dog-fetcher/src/lib.rs
+++ examples/rust/components/dog-fetcher/src/lib.rs
@@ -1,8 +1,6 @@
 #![allow(clippy::missing_safety_doc)]
 wit_bindgen::generate!();
 
-use std::io::Read;
-
 use exports::wasi::http::incoming_handler::Guest;
 use wasi::http::types::*;
 
@@ -15,39 +13,16 @@ struct DogFetcher;
 
 impl Guest for DogFetcher {
     fn handle(_request: IncomingRequest, response_out: ResponseOutparam) {
-        let req = wasi::http::outgoing_handler::OutgoingRequest::new(Fields::new());
-        req.set_scheme(Some(&Scheme::Https)).unwrap();
-        req.set_authority(Some("dog.ceo")).unwrap();
-        req.set_path_with_query(Some("/api/breeds/image/random"))
-            .unwrap();
-        let dog_picture_url = match wasi::http::outgoing_handler::handle(req, None) {
+        let dog_picture_url = match futures::executor::block_on(
+            reqwest::Client::new()
+                .get("https://dog.ceo/api/breeds/image/random")
+                .send(),
+        ) {
             Ok(resp) => {
-                resp.subscribe().block();
-                let response = resp
-                    .get()
-                    .expect("HTTP request response missing")
-                    .expect("HTTP request response requested more than once")
-                    .expect("HTTP request failed");
-                if response.status() == 200 {
-                    let response_body = response
-                        .consume()
-                        .expect("failed to get incoming request body");
-                    let body = {
-                        let mut buf = vec![];
-                        let mut stream = response_body
-                            .stream()
-                            .expect("failed to get HTTP request response stream");
-                        InputStreamReader::from(&mut stream)
-                            .read_to_end(&mut buf)
-                            .expect("failed to read value from HTTP request response stream");
-                        buf
-                    };
-                    let _trailers = wasi::http::types::IncomingBody::finish(response_body);
-                    let dog_response: DogResponse = serde_json::from_slice(&body).unwrap();
-                    dog_response.message
-                } else {
-                    format!("HTTP request failed with status code {}", response.status())
-                }
+                let body =
+                    futures::executor::block_on(resp.bytes()).expect("should have response bytes");
+                let dog_response: DogResponse = serde_json::from_slice(&body).unwrap();
+                dog_response.message
             }
             Err(e) => {
                 format!("Got error when trying to fetch dog: {}", e)
@@ -66,45 +41,6 @@ impl Guest for DogFetcher {
             .blocking_write_and_flush(dog_picture_url.as_bytes())
             .unwrap();
         OutgoingBody::finish(response_body, None).expect("failed to finish response body");
-    }
-}
-
-pub struct InputStreamReader<'a> {
-    stream: &'a mut crate::wasi::io::streams::InputStream,
-}
-
-impl<'a> From<&'a mut crate::wasi::io::streams::InputStream> for InputStreamReader<'a> {
-    fn from(stream: &'a mut crate::wasi::io::streams::InputStream) -> Self {
-        Self { stream }
-    }
-}
-
-impl std::io::Read for InputStreamReader<'_> {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        use crate::wasi::io::streams::StreamError;
-        use std::io;
-
-        let n = buf
-            .len()
-            .try_into()
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        match self.stream.blocking_read(n) {
-            Ok(chunk) => {
-                let n = chunk.len();
-                if n > buf.len() {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "more bytes read than requested",
-                    ));
-                }
-                buf[..n].copy_from_slice(&chunk);
-                Ok(n)
-            }
-            Err(StreamError::Closed) => Ok(0),
-            Err(StreamError::LastOperationFailed(e)) => {
-                Err(io::Error::new(io::ErrorKind::Other, e.to_debug_string()))
-            }
-        }
     }
 }
 
