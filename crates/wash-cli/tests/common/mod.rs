b2072e5317cccdbea5dc3d4c9f5b49db5cb21993
Victor Adossi <vadossi@cosmonic.com>
2024-05-28 20:19:19 +0900

    chore(wash-cli): fix lint
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/tests/common/mod.rs
+++ crates/wash-cli/tests/common/mod.rs
@@ -548,7 +548,7 @@ impl TestWashInstance {
                 "--timeout-ms",
                 DEFAULT_WASH_INVOCATION_TIMEOUT_MS_ARG,
                 "--ctl-port",
-                &self.nats_port.to_string().as_ref(),
+                &self.nats_port.to_string(),
             ])
             .kill_on_drop(true)
             .output()
