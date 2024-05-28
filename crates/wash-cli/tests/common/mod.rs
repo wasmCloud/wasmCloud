fd026f017413bea299c4ab8484235c2a6120b7d5
Victor Adossi <vadossi@cosmonic.com>
2024-05-28 18:37:11 +0900

    fix(wash-cli): fix reference to http-jsonify-rust component
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/tests/common/mod.rs
+++ crates/wash-cli/tests/common/mod.rs
@@ -34,7 +34,7 @@ pub const LOCAL_REGISTRY: &str = "localhost:5001";
 pub const HELLO_OCI_REF: &str = "ghcr.io/brooksmtownsend/http-hello-world-rust:0.1.1";
 
 #[allow(unused)]
-pub const HTTP_JSONIFY_OCI_REF: &str = "ghcr.io/wasmcloud/component-http-jsonify:0.1.1";
+pub const HTTP_JSONIFY_OCI_REF: &str = "ghcr.io/wasmcloud/components/http-jsonify-rust:0.1.1";
 
 #[allow(unused)]
 pub const PROVIDER_HTTPSERVER_OCI_REF: &str = "ghcr.io/wasmcloud/http-server:0.20.0";
