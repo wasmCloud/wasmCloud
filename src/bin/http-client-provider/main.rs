f60ddcaff61234a800efdac36ca8de7970b174b4
Brooks Townsend <brooksmtownsend@gmail.com>
2024-05-30 16:26:08 -0400

    feat(http-client): provide configuration options for http-client
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- src/bin/http-client-provider/main.rs
+++ src/bin/http-client-provider/main.rs
@@ -1,12 +1,16 @@
 use anyhow::Context as _;
 use wasmcloud_provider_http_client::HttpClientProvider;
-use wasmcloud_provider_sdk::interfaces::http::run_outgoing_handler;
+use wasmcloud_provider_sdk::{interfaces::http::run_outgoing_handler, load_host_data};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    run_outgoing_handler(HttpClientProvider::default(), "http-client-provider")
-        .await
-        .context("failed to run provider")?;
+    let host_data = load_host_data()?;
+    run_outgoing_handler(
+        HttpClientProvider::new(&host_data.config).await?,
+        "http-client-provider",
+    )
+    .await
+    .context("failed to run provider")?;
     eprintln!("HttpClient provider exiting");
     Ok(())
 }
