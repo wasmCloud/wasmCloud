7f0ac03e86a010cbeef3dc4410a40ad58e0c5d88
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-06-07 14:57:01 -0400

    chore(wash): update washboard version
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- crates/wash-cli/src/ui/mod.rs
+++ crates/wash-cli/src/ui/mod.rs
@@ -14,7 +14,7 @@ use wash_lib::{
 };
 use wasmcloud_core::tls;
 
-const DEFAULT_WASHBOARD_VERSION: &str = "v0.2.1";
+const DEFAULT_WASHBOARD_VERSION: &str = "v0.3.0";
 
 #[derive(Parser, Debug, Clone)]
 pub struct UiCommand {
