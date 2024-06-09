f1683a288383ad49b0305be37ad6554e45a7b017
ossfellow <masoudbahar@gmail.com>
2024-06-09 16:45:10 -0400

    refactor: Addressed PR feedback, and reimplemented the increment function as lock free; changed how Kv stores are managed and used. Signed-off-by: ossfellow <masoudbahar@gmail.com>

--- crates/provider-keyvalue-nats/README.md
+++ crates/provider-keyvalue-nats/README.md
@@ -2,17 +2,15 @@
 
 This capability provider is an implementation of the following interfaces of `wasi:keyvalue` proposal:
 
-*   wasi:keyvalue/store
+*   wasi:keyvalue/store\*
 
-*   wasi:keyvalue/atomics\*
+*   wasi:keyvalue/atomics
 
 *   wasi:keyvalue/batch
 
-> ⚠️ NATS server doesn't provide atomic operation guarantees. So, this provider uses the combination of the ***NATS connection URL***, ***Jetstream Domain***,  ***NATS Bucket name***, and the ***Key name*** to guarantee the atomicity of the `increment` function calls, across its consuming components.
->
-> However, this best efforts approach cannot guarantee atomicity, if multiple instances of this provider are used, targeting the same *URL + domain + bucket + key* combination for the aforementioned function.
+> The NATS Kv store doesn't support a cursor, when using the `list_keys` function; therefore, all keys will be returned, irrespective of if a cursor value was provided by the user or not.
 
-This provider is multi-threaded and can handle concurrent requests from multiple consumer components. Furthermore, consumer components can share a host supplied default configuration, or provide their required provider configuration, using wasmCloud's link definitions. Each link definition declared for this provider will result in a single NATS cluster connection managed on behalf of the linked component. Connections are maintained within the provider process, so multiple instances of this provider running in the same lattice will not share connections.
+This provider is multi-threaded and can handle concurrent requests from multiple consumer components. Furthermore, consumer components can share a host supplied default configuration, or provide their bespoke provider configuration, using wasmCloud's link definitions. Each link definition declared for this provider will result in a single NATS cluster connection managed on behalf of the linked component. Connections are maintained within the provider process, so multiple instances of this provider running in the same lattice will not share connections.
 
 ## Link Definition Configuration Settings
 
@@ -20,9 +18,10 @@ To configure this provider, use the following settings in link definitions:
 
 | **Property**  | **Description**                                                                                                                |
 | :------------ | :----------------------------------------------------------------------------------------------------------------------------- |
-| `CLUSTER_URI` | NATS cluster connection URI. If not specified, the default is `0.0.0.0:4222`                                                   |
-| `JS_DOMAIN`   | The NATS Jetstream domain to connect to. If not provided it defaults to `core`.                                                |
+| `CLUSTER_URI` | NATS cluster connection URI. If not specified, the default is `nats://0.0.0.0:4222`                                            |
+| `JS_DOMAIN`   | Optional NATS Jetstream domain to connect to.                                                                                  |
 | `CLIENT_JWT`  | Optional JWT auth token. For JWT authentication, both `CLIENT_JWT` and `CLIENT_SEED` must be provided.                         |
 | `CLIENT_SEED` | Private seed for JWT authentication.                                                                                           |
 | `TLS_CA`      | To secure communications with the NATS server, the public key of its CA could be provided as an encoded string.                |
 | `TLS_CA_FILE` | Alternatively, the path qualified name of the CA public key could be provided. If both are provided, the TLS\_CA will be used. |
+
--- examples/rust/components/keyvalue-messaging/src/lib.rs
+++ examples/rust/components/keyvalue-messaging/src/lib.rs
@@ -2,9 +2,9 @@
 wit_bindgen::generate!();
 
 use crate::exports::wasmcloud::messaging::handler::Guest as NatsKvDemoGuest;
-use crate::wasi::keyvalue::{store, atomics};
-use crate::wasmcloud::messaging::{consumer, types};
+use crate::wasi::keyvalue::{atomics, store};
 use crate::wasi::logging::logging::{log, Level};
+use crate::wasmcloud::messaging::{consumer, types};
 
 struct NatsKvDemo;
 
@@ -12,18 +12,21 @@ const DEFAULT_BUCKET: &str = "WASMCLOUD";
 const DEFAULT_COUNT: u64 = 1;
 const DEFAULT_PUB_SUBJECT: &str = "nats.atomic";
 
-
 impl NatsKvDemoGuest for NatsKvDemo {
     fn handle_message(msg: types::BrokerMessage) -> Result<(), String> {
-        // Get the bucket name as a configuration value
+        // 1.1) Get the bucket name as a configuration value
         let bucket_name = match crate::wasi::config::runtime::get("bucket") {
             Ok(Some(value)) => value,
             Ok(None) => DEFAULT_BUCKET.to_string(),
             Err(_) => return Err("Failed to get bucket name".to_string()),
         };
-        log(Level::Info, "kv-demo", format!("Bucket name: {}", bucket_name).as_str());
+        log(
+            Level::Info,
+            "kv-demo",
+            format!("Bucket name: {}", bucket_name).as_str(),
+        );
 
-        // Get the repitition count as a configuration value
+        // 1.2) Get the repitition count as a configuration value
         let count = match crate::wasi::config::runtime::get("count") {
             Ok(Some(value)) => value.parse::<u64>().unwrap_or(DEFAULT_COUNT),
             Ok(None) => DEFAULT_COUNT,
@@ -31,33 +34,63 @@ impl NatsKvDemoGuest for NatsKvDemo {
         };
         log(Level::Info, "kv-demo", format!("Count: {}", count).as_str());
 
-        // Get the subject to publish to as a configuration value
+        // 1.3 Get the subject to publish to as a configuration value
         let pub_subject = match crate::wasi::config::runtime::get("pub_subject") {
             Ok(Some(value)) => value,
             Ok(None) => DEFAULT_PUB_SUBJECT.to_string(),
             Err(_) => return Err("Failed to get publish subject".to_string()),
         };
-        log(Level::Info, "kv-demo", format!("Publish subject: {}", pub_subject).as_str());
+        log(
+            Level::Info,
+            "kv-demo",
+            format!("Publish subject: {}", pub_subject).as_str(),
+        );
 
-        // Get the key from the message
+        // 2) Get the key from the incoming message
         let key = match String::from_utf8(msg.body) {
             Ok(value) => value,
             Err(_) => return Err("Failed to convert message body to string".to_string()),
         };
         log(Level::Info, "kv-demo", format!("Key: {}", key).as_str());
 
-        // Open the bucket
+        // 3) Open the bucket
         let bucket: store::Bucket = store::open(&bucket_name).expect("failed to open bucket");
+        log(
+            Level::Info,
+            "kv-demo",
+            format!("Component opened the bucket {}", bucket_name).as_str(),
+        );
 
-        // Increment the key, and repeat the increment count times
+        // 4) Initialize the key with a random neumeric value
+        use std::time::{SystemTime, UNIX_EPOCH};
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let random_number = now.as_secs() % 11; // Modulo 11 to get a range of 0-10
+
+        match bucket.set(&key, &(random_number.clone().to_string()).into_bytes()) {
+            Ok(_) => log(
+                Level::Info,
+                "kv-demo",
+                format!("Initialized the key {} with value {}", key, random_number).as_str(),
+            ),
+            Err(_) => return Err("Failed to initialize key".to_string()),
+        }
+
+        // 5) Increment the key, and repeat the increment count times
         for _ in 1..=count {
             let counter = atomics::increment(&bucket, &key, 1);
-            if let Ok(count) = counter {
-                log(Level::Info, "kv-demo", format!("Incremented key {} to {}", key, count).as_str());
+            if let Ok(_) = counter.clone() {
+                log(
+                    Level::Info,
+                    "kv-demo",
+                    format!("Incremented key {} to {}", key, counter.unwrap()).as_str(),
+                );
             }
         }
 
-        // Read the value of the key, and publish it to the pub_subject
+        // 6) Read the value of the key, and publish it to the pub_subject
         match bucket.get(&key) {
             Ok(Some(value)) => {
                 if let Err(_) = consumer::publish(&types::BrokerMessage {
@@ -69,16 +102,41 @@ impl NatsKvDemoGuest for NatsKvDemo {
                 }
                 match String::from_utf8(value) {
                     Ok(value_string) => {
-                        log(Level::Info, "kv-demo", format!("published key {} with value {} to NATS {} subject", key.clone(), value_string, pub_subject).as_str());
+                        log(
+                            Level::Info,
+                            "kv-demo",
+                            format!(
+                                "published key {} with value {} to NATS {} subject",
+                                key.clone(),
+                                value_string,
+                                pub_subject
+                            )
+                            .as_str(),
+                        );
                     }
                     Err(_) => log(Level::Error, "kv-demo", "Failed to convert value to string"),
                 }
             }
-            Ok(None) => log(Level::Info, "kv-demo", format!("No value found for key {}", key.clone()).as_str()),
+            Ok(None) => log(
+                Level::Info,
+                "kv-demo",
+                format!("No value found for key {}", key.clone()).as_str(),
+            ),
             Err(_) => return Err("Failed to get key value".to_string()),
         };
 
-        // List all keys in the bucket, and publish them to the pub_subject
+        // // 7) Set mutiple (3) keys-vlaue pairs
+        // let keys = vec!["key1", "key2", "key3"];
+        // let values = vec!["this", "is", "awesome"];
+
+        // let key_value_pairs: Vec<(String, Vec<u8>)> = keys.into_iter().zip(values.into_iter())
+        //   .map(|(key, value)| (key.to_string(), value.to_string().into_bytes()))
+        //   .collect();
+
+        // let _ = batch::set_many(&bucket, &key_value_pairs);
+        // log(Level::Info, "kv-demo", "Set multiple keys");
+
+        // 8) List all keys in the bucket, and publish them to the pub_subject
         match bucket.list_keys(Some(0u64)) {
             Ok(key_response) => {
                 for key in key_response.keys {
@@ -89,17 +147,29 @@ impl NatsKvDemoGuest for NatsKvDemo {
                     }) {
                         log(Level::Error, "kv-demo", "Failed to publish message");
                     }
-                    log(Level::Info, "kv-demo", format!("Listed key: {}", key).as_str());
+                    log(
+                        Level::Info,
+                        "kv-demo",
+                        format!("Listed key: {}", key).as_str(),
+                    );
                 }
-            },
+            }
             Err(_) => log(Level::Error, "kv-demo", "Failed to list keys"),
         }
 
-        // Delete the key
+        // 9) Delete the key
         if let Err(_) = bucket.delete(&key) {
-            log(Level::Error, "kv-demo", format!("Failed to delete key {}", key).as_str());
+            log(
+                Level::Error,
+                "kv-demo",
+                format!("Failed to delete key {}", key).as_str(),
+            );
         } else {
-            log(Level::Info, "kv-demo", format!("Deleted key {}", key).as_str());
+            log(
+                Level::Info,
+                "kv-demo",
+                format!("Deleted key {}", key).as_str(),
+            );
         }
 
         Ok(())
