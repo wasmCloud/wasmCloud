d2fe6aafd480e199825c06710156f238b4b0673d
Brooks Townsend <brooksmtownsend@gmail.com>
2024-06-14 11:28:05 -0400

    test(config)!: fetch secret in pingpong test
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    test(config): use secret format
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    config tests
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    fix(configtest): clippy warnings
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- tests/components/rust/ponger-config-component/src/lib.rs
+++ tests/components/rust/ponger-config-component/src/lib.rs
@@ -14,6 +14,16 @@ impl pingpong::Guest for Actor {
             .expect("Unable to fetch value")
             .unwrap_or_else(|| "config value not set".to_string())
     }
+
+    fn ping_secret() -> String {
+        let secret = wasmcloud::secrets::store::get("ponger").expect("Unable to fetch value");
+        match wasmcloud::secrets::reveal::reveal(&secret) {
+            wasmcloud::secrets::store::SecretValue::String(s) => s,
+            wasmcloud::secrets::store::SecretValue::Bytes(bytes) => {
+                String::from_utf8_lossy(&bytes).to_string()
+            }
+        }
+    }
 }
 
 export!(Actor);
