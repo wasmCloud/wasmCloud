d34699d922a50233bee9914b071b70679bd81e91
Victor Adossi <vadossi@cosmonic.com>
2024-06-21 02:37:13 +0900

    fix(tests): component builds
    The component builds broke on Rust 1.79, because the interface to
    `cargo` broke.
    
    This commit updates the changed pieces, and refactors the build script
    as well.
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- tests/components/build.rs
+++ tests/components/build.rs
@@ -3,14 +3,64 @@ use std::env;
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::process::{Output, Stdio};
+use std::sync::Arc;
 
-use anyhow::{bail, ensure, Context};
-use futures::try_join;
+use anyhow::{bail, ensure, Context, Result};
+use heck::ToKebabCase;
 use nkeys::KeyPair;
 use serde::Deserialize;
 use tokio::fs;
 use tokio::process::Command;
+use tokio::task::JoinSet;
 use wasmcloud_component_adapters::WASI_PREVIEW1_REACTOR_COMPONENT_ADAPTER;
+
+/// List of (manifest path, output artifact name) for all the packages used during test
+///
+/// Manifest paths should be relative to the directory containing this build.rs (i.e. tests/components)
+const WASI_WASM32_PACKAGES: [(&str, &str); 6] = [
+    ("./rust/Cargo.toml", "builtins-component-reactor"),
+    ("./rust/Cargo.toml", "interfaces-handler-reactor"),
+    ("./rust/Cargo.toml", "interfaces-reactor"),
+    ("./rust/Cargo.toml", "pinger-config-component"),
+    ("./rust/Cargo.toml", "ponger-config-component"),
+    (
+        "../../examples/rust/components/http-keyvalue-counter/Cargo.toml",
+        "http-keyvalue-counter",
+    ),
+];
+
+/// List of packages which should have output artifacts signed
+const WASI_WASM32_PACKAGES_SIGNED: [&str; 12] = [
+    "builtins-component-reactor",
+    "builtins-component-reactor-preview2",
+    "http-keyvalue-counter",
+    "http-keyvalue-counter-preview2",
+    "interfaces-handler-reactor",
+    "interfaces-handler-reactor-preview2",
+    "interfaces-reactor",
+    "interfaces-reactor-preview2",
+    "pinger-config-component",
+    "pinger-config-component-preview2",
+    "ponger-config-component",
+    "ponger-config-component-preview2",
+];
+
+/// Convert a hard-coded artifact name (see [`WASI_WASM32_PACKAGES`])
+///
+/// Pre-Rust 1.79, kebab cased project names would turn into kebab-cased artifacts
+#[rustversion::before(1.79)]
+fn generate_expected_artifact_name(project_name: &str) -> impl AsRef<str> {
+    project_name.to_kebab_case()
+}
+
+/// Convert a hard-coded artifact name (see [`WASI_WASM32_PACKAGES`])
+///
+/// Post-Rust 1.79, kebab cased project names turn into snake_case cased artifacts
+#[rustversion::since(1.79)]
+fn generate_expected_artifact_name(project_name: &str) -> impl AsRef<str> {
+    use heck::ToSnakeCase;
+    project_name.to_snake_case()
+}
 
 // Unfortunately, `cargo` exported structs and enums do not implement `Deserialize`, so
 // implement the relevant parts here
@@ -30,6 +80,8 @@ enum CrateType {
     Cdylib,
     #[serde(rename = "staticlib")]
     Staticlib,
+    #[serde(rename = "procmacro")]
+    ProcMacro,
     #[serde(other)]
     Other,
 }
@@ -57,7 +109,7 @@ enum BuildMessage {
 async fn build_artifacts(
     args: impl IntoIterator<Item = impl AsRef<OsStr>>,
     pred: impl Fn(&str, &[CrateType]) -> bool,
-) -> anyhow::Result<impl Iterator<Item = (String, Vec<PathBuf>)>> {
+) -> Result<impl Iterator<Item = (String, Vec<PathBuf>)>> {
     let Output {
         status,
         stdout,
@@ -74,6 +126,7 @@ async fn build_artifacts(
         .await
         .context("failed to call `cargo`")?;
     ensure!(status.success(), "`cargo` invocation failed");
+
     serde_json::Deserializer::from_reader(stdout.as_slice())
         .into_iter()
         .filter_map(|message| match message {
@@ -97,7 +150,8 @@ async fn build_artifacts(
         .map(IntoIterator::into_iter)
 }
 
-async fn copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<u64> {
+/// Copy a file from `src` to `dst`
+async fn copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> Result<u64> {
     let src = src.as_ref();
     let dst = dst.as_ref();
     fs::copy(&src, &dst)
@@ -105,121 +159,58 @@ async fn copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<u6
         .with_context(|| format!("failed to copy `{}` to `{}`", src.display(), dst.display()))
 }
 
-trait DerefArtifact {
-    fn deref_artifact(&self) -> Option<(&str, &[PathBuf])>;
-}
+async fn install_rust_wasm32_wasi_actors(out_dir: impl AsRef<Path>) -> Result<()> {
+    let out_dir = Arc::new(out_dir.as_ref().to_path_buf());
 
-impl DerefArtifact for Option<(String, Vec<PathBuf>)> {
-    fn deref_artifact(&self) -> Option<(&str, &[PathBuf])> {
-        self.as_ref()
-            .map(|(pkg, paths)| (pkg.as_str(), paths.as_slice()))
+    // Build the artifacts in parallel
+    let mut artifacts = JoinSet::new();
+    for (manifest_path, package) in WASI_WASM32_PACKAGES {
+        artifacts.spawn(build_artifacts(
+            [
+                format!(
+                    "--manifest-path={}/{manifest_path}",
+                    env!("CARGO_MANIFEST_DIR")
+                ),
+                "--target=wasm32-wasi".to_string(),
+                // We use dashes for packages, since they're still always
+                format!("-p={package}"),
+            ],
+            |name, kind| {
+                WASI_WASM32_PACKAGES
+                    .iter()
+                    .any(|(_, n)| generate_expected_artifact_name(n).as_ref() == name)
+                    && (kind.contains(&CrateType::Cdylib) || kind.contains(&CrateType::Bin))
+            },
+        ));
     }
-}
 
-async fn install_rust_wasm32_wasi_actors(out_dir: impl AsRef<Path>) -> anyhow::Result<()> {
-    let out_dir = out_dir.as_ref();
+    // Copy the artifacts to expected paths,
+    let mut copies = JoinSet::new();
+    while let Some(Ok(Ok(mut iter))) = artifacts.join_next().await {
+        let out_dir = out_dir.clone();
+        if let Some((artifact, paths)) = iter.next() {
+            copies.spawn(async move {
+                copy(
+                    paths
+                        .first()
+                        .context("failed to get back path from artifact build")?,
+                    // Code still expects kebab case (according to project names, not artifact names)
+                    // This is a no-op for pre-1.79, but is required 1.79 or after
+                    out_dir.join(format!("rust-{}.wasm", artifact.to_kebab_case())),
+                )
+                .await
+            });
+        }
+    }
 
-    try_join!(
-        async {
-            // NOTE: this list should be kept sorted
-            let packages = [
-                "builtins-component-reactor",
-                "interfaces-handler-reactor",
-                "interfaces-reactor",
-                "pinger-config-component",
-                "ponger-config-component",
-            ];
-
-            let mut artifacts = build_artifacts(
-                [
-                    vec![
-                        "--manifest-path=./rust/Cargo.toml".to_string(),
-                        "--target=wasm32-wasi".to_string(),
-                    ],
-                    packages
-                        .iter()
-                        .map(|n| format!("-p={n}"))
-                        .collect::<Vec<String>>(),
-                ]
-                .concat(),
-                |name, kind| {
-                    packages.contains(&name)
-                        && (kind.contains(&CrateType::Cdylib) || kind.contains(&CrateType::Bin))
-                },
-            )
-            .await
-            .with_context(|| format!("failed to build {packages:?}"))?;
-            match (
-                artifacts.next().deref_artifact(),
-                artifacts.next().deref_artifact(),
-                artifacts.next().deref_artifact(),
-                artifacts.next().deref_artifact(),
-                artifacts.next().deref_artifact(),
-                artifacts.next(),
-            ) {
-                (
-                    Some(("builtins-component-reactor", [builtins_component_reactor])),
-                    Some(("interfaces-handler-reactor", [interfaces_handler_reactor])),
-                    Some(("interfaces-reactor", [interfaces_reactor])),
-                    Some(("pinger-config-component", [pinger_config_component])),
-                    Some(("ponger-config-component", [ponger_config_component])),
-                    None,
-                ) => {
-                    try_join!(
-                        copy(
-                            builtins_component_reactor,
-                            out_dir.join("rust-builtins-component-reactor.wasm"),
-                        ),
-                        copy(
-                            interfaces_reactor,
-                            out_dir.join("rust-interfaces-reactor.wasm"),
-                        ),
-                        copy(
-                            interfaces_handler_reactor,
-                            out_dir.join("rust-interfaces-handler-reactor.wasm"),
-                        ),
-                        copy(
-                            pinger_config_component,
-                            out_dir.join("rust-pinger-config-component.wasm"),
-                        ),
-                        copy(
-                            ponger_config_component,
-                            out_dir.join("rust-ponger-config-component.wasm"),
-                        ),
-                    )
-                }
-                v => bail!("invalid {packages:?} build artifacts: {v:#?}"),
-            }
-        },
-        async {
-            let mut artifacts = build_artifacts(
-                [
-                    "--manifest-path=../../examples/rust/components/http-keyvalue-counter/Cargo.toml",
-                    "--target=wasm32-wasi",
-                ],
-                |name, kind| name == "http-keyvalue-counter" && kind.contains(&CrateType::Cdylib),
-            )
-            .await
-            .context("failed to build `http-keyvalue-counter`")?;
-            match (artifacts.next().deref_artifact(), artifacts.next()) {
-                (Some(("http-keyvalue-counter", [wasm])), None) => {
-                    copy(
-                        wasm,
-                        out_dir.join("rust-example-http-keyvalue-counter.wasm"),
-                    )
-                    .await
-                }
-                v => bail!("invalid `http-keyvalue-counter` build artifacts: {v:#?}"),
-            }
-        },
-    )
-    .context("failed to build `wasm32-wasi` components")?;
+    // Wait for all the copies
+    while (copies.join_next().await).is_some() {}
 
     Ok(())
 }
 
 #[cfg(not(feature = "docs"))]
-fn encode_component(module: impl AsRef<[u8]>, adapter: &[u8]) -> anyhow::Result<Vec<u8>> {
+fn encode_component(module: impl AsRef<[u8]>, adapter: &[u8]) -> Result<Vec<u8>> {
     wit_component::ComponentEncoder::default()
         .validate(true)
         .module(module.as_ref())
@@ -231,12 +222,12 @@ fn encode_component(module: impl AsRef<[u8]>, adapter: &[u8]) -> anyhow::Result<
 }
 
 #[cfg(feature = "docs")]
-fn encode_component(_: impl AsRef<[u8]>, _: &[u8]) -> anyhow::Result<Vec<u8>> {
+fn encode_component(_: impl AsRef<[u8]>, _: &[u8]) -> Result<Vec<u8>> {
     Ok(Vec::default())
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=../../crates/component");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=rust");
@@ -247,22 +238,14 @@ async fn main() -> anyhow::Result<()> {
     install_rust_wasm32_wasi_actors(&out_dir).await?;
 
     // Build WASI reactor components
-    for name in [
-        "builtins-component-reactor",
-        "example-http-keyvalue-counter",
-        "interfaces-handler-reactor",
-        "interfaces-reactor",
-        "pinger-config-component",
-        "ponger-config-component",
-    ] {
-        let path = out_dir.join(format!("rust-{name}.wasm"));
+    for (_manifest_path, package_name) in WASI_WASM32_PACKAGES {
+        let path = out_dir.join(format!("rust-{package_name}.wasm"));
         let module = fs::read(&path)
             .await
             .with_context(|| format!("failed to read `{}`", path.display()))?;
         let component = encode_component(module, WASI_PREVIEW1_REACTOR_COMPONENT_ADAPTER)
             .with_context(|| format!("failed to encode `{}`", path.display()))?;
-
-        let path = out_dir.join(format!("rust-{name}-preview2.wasm"));
+        let path = out_dir.join(format!("rust-{package_name}-preview2.wasm"));
         fs::write(&path, component)
             .await
             .with_context(|| format!("failed to write `{}`", path.display()))?;
@@ -275,24 +258,10 @@ async fn main() -> anyhow::Result<()> {
         issuer.seed().expect("failed to extract issuer seed")
     );
 
-    // Sign the built wasm modules with relevant claims
-    for name in [
-        "builtins-component-reactor",
-        "builtins-component-reactor-preview2",
-        "example-http-keyvalue-counter",
-        "example-http-keyvalue-counter-preview2",
-        "interfaces-handler-reactor",
-        "interfaces-handler-reactor-preview2",
-        "interfaces-reactor",
-        "interfaces-reactor-preview2",
-        "pinger-config-component",
-        "pinger-config-component-preview2",
-        "ponger-config-component",
-        "ponger-config-component-preview2",
-    ] {
-        let wasm = fs::read(out_dir.join(format!("rust-{name}.wasm")))
+    for package_name in WASI_WASM32_PACKAGES_SIGNED {
+        let wasm = fs::read(out_dir.join(format!("rust-{package_name}.wasm")))
             .await
-            .with_context(|| format!("failed to read `{name}` Wasm"))?;
+            .with_context(|| format!("failed to read `{package_name}` Wasm"))?;
         let wasm = if cfg!(feature = "docs") {
             wasm
         } else {
@@ -301,17 +270,20 @@ async fn main() -> anyhow::Result<()> {
                 .issuer(&issuer.public_key())
                 .subject(&module.public_key())
                 .with_metadata(wascap::jwt::Component {
-                    name: Some(name.into()),
-                    call_alias: Some(name.into()),
+                    name: Some(package_name.into()),
+                    call_alias: Some(package_name.into()),
                     ..Default::default()
                 })
                 .build();
             wascap::wasm::embed_claims(&wasm, &claims, &issuer)
                 .context("failed to embed component claims")?
         };
-        fs::write(out_dir.join(format!("rust-{name}.signed.wasm")), wasm)
-            .await
-            .context("failed to write Wasm")?;
+        fs::write(
+            out_dir.join(format!("rust-{package_name}.signed.wasm")),
+            wasm,
+        )
+        .await
+        .context("failed to write Wasm")?;
     }
 
     Ok(())
