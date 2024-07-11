f6925207280f85a5385082828e34e451d16a5e29
Roman Volosatovs <rvolosatovs@riseup.net>
2024-07-11 19:59:17 +0200

    chore(runtime): remove unused logging exports
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/runtime/src/component/logging.rs
+++ crates/runtime/src/component/logging.rs
@@ -27,24 +27,6 @@ pub trait Logging {
     ) -> anyhow::Result<()>;
 }
 
-//impl<H: Handler> Instance<H> {
-//    /// Instantiates and returns an [`InterfaceInstance<logging_bindings::Logging>`] if exported by the [`Instance`].
-//    ///
-//    /// # Errors
-//    ///
-//    /// Fails if logging bindings are not exported by the [`Instance`]
-//    pub async fn into_logging(
-//        mut self,
-//    ) -> anyhow::Result<InterfaceInstance<logging_bindings::Logging, H>> {
-//        let (bindings, _) =
-//            logging_bindings::Logging::instantiate_pre(&mut self.store, &self.instance_pre).await?;
-//        Ok(InterfaceInstance {
-//            store: Mutex::new(self.store),
-//            bindings,
-//        })
-//    }
-//}
-
 #[async_trait]
 impl<H: Handler> logging::Host for Ctx<H> {
     #[instrument(skip_all)]
@@ -57,31 +39,3 @@ impl<H: Handler> logging::Host for Ctx<H> {
         self.handler.log(level, context, message).await
     }
 }
-
-//#[async_trait]
-//impl<H: Handler> Logging for InterfaceInstance<logging_bindings::Logging, H> {
-//    #[instrument(skip(self))]
-//    async fn log(
-//        &self,
-//        level: logging::Level,
-//        context: String,
-//        message: String,
-//    ) -> anyhow::Result<()> {
-//        let mut store = self.store.lock().await;
-//        // NOTE: It appears that unifying the `Level` type is not possible currently
-//        use logging_bindings::exports::wasi::logging::logging::Level;
-//        let level = match level {
-//            logging::Level::Trace => Level::Trace,
-//            logging::Level::Debug => Level::Debug,
-//            logging::Level::Info => Level::Info,
-//            logging::Level::Warn => Level::Warn,
-//            logging::Level::Error => Level::Error,
-//            logging::Level::Critical => Level::Critical,
-//        };
-//        trace!("call `wasi:logging/logging.log`");
-//        self.bindings
-//            .wasi_logging_logging()
-//            .call_log(&mut *store, level, &context, &message)
-//            .await
-//    }
-//}
