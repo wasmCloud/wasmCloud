61890be16072e58268e91ac07a2ce73d5a948ec7
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-08-05 19:13:35 -0400

    fix(lint): update linting after tooling update
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- washboard-ui/packages/washboard-ui/src/components/toast/use-toast.ts
+++ washboard-ui/packages/washboard-ui/src/components/toast/use-toast.ts
@@ -60,7 +60,7 @@ const addToRemoveQueue = (toastId: string): void => {
   const timeout = setTimeout(() => {
     toastTimeouts.delete(toastId);
     dispatch({
-      type: 'REMOVE_TOAST',
+      type: actionTypes.REMOVE_TOAST,
       toastId: toastId,
     });
   }, TOAST_REMOVE_DELAY);
@@ -70,21 +70,21 @@ const addToRemoveQueue = (toastId: string): void => {
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case 'ADD_TOAST': {
+    case actionTypes.ADD_TOAST: {
       return {
         ...state,
         toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
       };
     }
 
-    case 'UPDATE_TOAST': {
+    case actionTypes.UPDATE_TOAST: {
       return {
         ...state,
         toasts: state.toasts.map((t) => (t.id === action.toast.id ? {...t, ...action.toast} : t)),
       };
     }
 
-    case 'DISMISS_TOAST': {
+    case actionTypes.DISMISS_TOAST: {
       const {toastId} = action;
 
       // ! Side effects ! - This could be extracted into a dismissToast() action,
@@ -109,7 +109,7 @@ export const reducer = (state: State, action: Action): State => {
         ),
       };
     }
-    case 'REMOVE_TOAST': {
+    case actionTypes.REMOVE_TOAST: {
       if (action.toastId === undefined) {
         return {
           ...state,
@@ -148,13 +148,13 @@ function toast({...props}: Toast): ToastReturn {
 
   const update = (props: ToasterToast): void =>
     dispatch({
-      type: 'UPDATE_TOAST',
+      type: actionTypes.UPDATE_TOAST,
       toast: {...props, id},
     });
-  const dismiss = (): void => dispatch({type: 'DISMISS_TOAST', toastId: id});
+  const dismiss = (): void => dispatch({type: actionTypes.DISMISS_TOAST, toastId: id});
 
   dispatch({
-    type: 'ADD_TOAST',
+    type: actionTypes.ADD_TOAST,
     toast: {
       ...props,
       id,
@@ -193,7 +193,7 @@ function useToast(): UseToastReturn {
   return {
     ...state,
     toast,
-    dismiss: (toastId?: string): void => dispatch({type: 'DISMISS_TOAST', toastId}),
+    dismiss: (toastId?: string): void => dispatch({type: actionTypes.DISMISS_TOAST, toastId}),
   };
 }
 
