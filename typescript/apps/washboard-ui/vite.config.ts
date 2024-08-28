35e47167189ffc5ddcd4a6c171e9cc072ae63b84
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-08-28 01:53:23 -0400

    chore(typescript): update to vite for build and clean up tasks/deps
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- typescript/apps/washboard-ui/vite.config.ts
+++ typescript/apps/washboard-ui/vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import sourceMaps from 'rollup-plugin-sourcemaps';
 import {defineConfig} from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,4 +7,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      plugins: [sourceMaps()],
+    },
+  },
+  server: {
+    sourcemapIgnoreList: false,
+  },
 });
---
+++ typescript/packages/lattice-client-core/vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import {defineConfig} from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), dts()],
+  resolve: {alias: {'@/': path.resolve('src/')}},
+  build: {
+    lib: {
+      entry: path.resolve(import.meta.dirname, 'src/index.ts'),
+      name: 'lattice-client-core',
+      formats: ['es', 'cjs'],
+    },
+    outDir: 'build',
+    rollupOptions: {
+      plugins: [sourceMaps()],
+    },
+    sourcemap: true,
+  },
+});
