81c10d6e4c207759ce89494cb22e8455b18d359d
Lucas Fontes <lucas@cosmonic.com>
2024-08-29 12:08:36 -0400

    chore: only vanity urls
    Signed-off-by: Lucas Fontes <lucas@cosmonic.com>

--- go/provider/examples/keyvalue-inmemory/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the Go binary and package it into a par file
-# This is designed to be run from the root of the project
-go generate ./...
-go build ./
-wash par create --vendor wasmcloud --name "KeyValue Go" --binary ./keyvalue-inmemory --compress
-rm keyvalue-inmemory
