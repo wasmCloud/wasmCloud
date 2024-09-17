8a87361f11e7d41e321519508937761463c1d37a
Victor Adossi <vadossi@cosmonic.com>
2024-09-17 23:12:14 +0900

    refactor(core): re-add missing cache code
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/core/src/oci.rs
+++ crates/core/src/oci.rs
@@ -4,10 +4,12 @@ use std::str::FromStr;
 
 use anyhow::{bail, Context as _};
 use oci_distribution::client::ClientProtocol;
+use oci_distribution::client::ImageData;
 use oci_distribution::Reference;
 use oci_wasm::WASM_LAYER_MEDIA_TYPE;
 use oci_wasm::WASM_MANIFEST_MEDIA_TYPE;
 use tokio::fs;
+use tokio::io::AsyncWriteExt;
 use wascap::jwt;
 
 use crate::tls;
@@ -17,13 +19,15 @@ const PROVIDER_ARCHIVE_MEDIA_TYPE: &str = "application/vnd.wasmcloud.provider.ar
 const WASM_MEDIA_TYPE: &str = "application/vnd.module.wasm.content.layer.v1+wasm";
 const OCI_MEDIA_TYPE: &str = "application/vnd.oci.image.layer.v1.tar";
 
-/// Default directory in which OCI artifacts are cached
-pub async fn oci_cache_dir() -> anyhow::Result<PathBuf> {
-    let path = temp_dir().join("wasmcloud_ocicache");
-    if !fs::try_exists(&path).await? {
-        fs::create_dir_all(&path).await?;
-    }
-    Ok(path)
+/// Whether to update an OCI artifact cache
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum OciArtifactCacheUpdate {
+    /// Do not update the OCI artifact cache
+    #[default]
+    Ignore,
+    /// Update the cache
+    Update,
 }
 
 /// OCI artifact fetcher
@@ -84,6 +88,37 @@ impl From<RegistryConfig> for OciFetcher {
     }
 }
 
+/// Default directory in which OCI artifacts are cached
+pub async fn oci_cache_dir() -> anyhow::Result<PathBuf> {
+    let path = temp_dir().join("wasmcloud_ocicache");
+    if !fs::try_exists(&path).await? {
+        fs::create_dir_all(&path).await?;
+    }
+    Ok(path)
+}
+
+#[allow(unused)]
+async fn cache_oci_image(
+    image: ImageData,
+    cache_filepath: impl AsRef<Path>,
+    digest_filepath: impl AsRef<Path>,
+) -> std::io::Result<()> {
+    let mut cache_file = fs::File::create(cache_filepath).await?;
+    let content = image
+        .layers
+        .into_iter()
+        .flat_map(|l| l.data)
+        .collect::<Vec<_>>();
+    cache_file.write_all(&content).await?;
+    cache_file.flush().await?;
+    if let Some(digest) = image.digest {
+        let mut digest_file = fs::File::create(digest_filepath).await?;
+        digest_file.write_all(digest.as_bytes()).await?;
+        digest_file.flush().await?;
+    }
+    Ok(())
+}
+
 fn prune_filepath(img: &str) -> String {
     let mut img = img.replace(':', "_");
     img = img.replace('/', "_");
@@ -98,6 +133,7 @@ impl OciFetcher {
         output_dir: impl AsRef<Path>,
         img: impl AsRef<str>,
         accepted_media_types: Vec<&str>,
+        cache: OciArtifactCacheUpdate,
     ) -> anyhow::Result<PathBuf> {
         let output_dir = output_dir.as_ref();
         let img = img.as_ref().to_lowercase(); // the OCI spec does not allow for capital letters in references
@@ -164,6 +200,13 @@ impl OciFetcher {
                 imgdata.layers.len()
             )
         }
+        // Update the OCI artifact cache if specified
+        if let OciArtifactCacheUpdate::Update = cache {
+            cache_oci_image(imgdata, &cache_file, digest_file)
+                .await
+                .context("failed to cache OCI bytes")?;
+        }
+
         Ok(cache_file)
     }
 
@@ -178,6 +221,7 @@ impl OciFetcher {
                 oci_cache_dir().await?,
                 oci_ref,
                 vec![WASM_MEDIA_TYPE, OCI_MEDIA_TYPE, WASM_LAYER_MEDIA_TYPE],
+                OciArtifactCacheUpdate::Update,
             )
             .await
             .context("failed to fetch OCI path")?;
@@ -201,6 +245,7 @@ impl OciFetcher {
                 oci_cache_dir().await?,
                 oci_ref.as_ref(),
                 vec![PROVIDER_ARCHIVE_MEDIA_TYPE, OCI_MEDIA_TYPE],
+                OciArtifactCacheUpdate::Update,
             )
             .await
             .context("failed to fetch OCI path")?;
