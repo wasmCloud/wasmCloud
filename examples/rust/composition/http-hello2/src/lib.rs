f0ab99fde5d016984680b97f61fd8258db561f09
Roman Volosatovs <rvolosatovs@riseup.net>
2024-09-25 10:28:58 +0200

    feat: update `wit-bindgen` to `0.32`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- examples/rust/composition/http-hello2/src/lib.rs
+++ examples/rust/composition/http-hello2/src/lib.rs
@@ -1,5 +1,4 @@
-#![allow(clippy::missing_safety_doc)]
-wit_bindgen::generate!();
+wit_bindgen::generate!({ generate_all });
 
 use exports::wasi::http::incoming_handler::Guest;
 use wasi::http::types::*;
