7756d06f4f90aab202223905f7c13de004dccaf9
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-09-25 16:16:47 -0400

    fix(lattice-client-react): add comments, rev version, remove unused argument
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- typescript/packages/lattice-client-react/src/context/use-lattice-client.tsx
+++ typescript/packages/lattice-client-react/src/context/use-lattice-client.tsx
@@ -2,10 +2,8 @@ import {type LatticeClient} from '@wasmcloud/lattice-client-core';
 import * as React from 'react';
 import {LatticeClientContext} from '@/context/lattice-client-context';
 
-export function useLatticeClient(LatticeClient?: LatticeClient): LatticeClient {
+export function useLatticeClient(): LatticeClient {
   const client = React.useContext(LatticeClientContext);
-
-  if (LatticeClient) return LatticeClient;
 
   if (!client)
     throw new Error('You must provide a LatticeClient instance through <LatticeClientProvider>');
