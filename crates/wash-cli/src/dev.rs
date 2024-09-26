d28ebe1783c7535ac30cccad1dacc70cb921f371
Victor Adossi <vadossi@cosmonic.com>
2024-09-27 02:15:16 +0900

    chore: update messaging-nats provider references
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/src/dev.rs
+++ crates/wash-cli/src/dev.rs
@@ -56,7 +56,7 @@ const DEFAULT_KEYVALUE_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/keyvalue-nats:0
 const DEFAULT_HTTP_CLIENT_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/http-client:0.12.1";
 const DEFAULT_HTTP_SERVER_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/http-server:0.23.1";
 const DEFAULT_BLOBSTORE_FS_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/blobstore-fs:0.8.0";
-const DEFAULT_MESSAGING_NATS_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/messaging-nats:0.22.0";
+const DEFAULT_MESSAGING_NATS_PROVIDER_IMAGE: &str = "ghcr.io/wasmcloud/messaging-nats:0.23.1";
 
 const DEFAULT_INCOMING_HANDLER_ADDRESS: &str = "127.0.0.1:8000";
 const DEFAULT_MESSAGING_HANDLER_SUBSCRIPTION: &str = "wasmcloud.dev";
