08e4f571edf5aeb7fe8de2cc174866d32220625a
ruichao.hu <ruichao.hu@daocloud.io>
2024-09-28 01:28:49 +0800

    fix(washboard-ui): reorganize components table view
    Signed-off-by: ruby <ruichao.hu@daocloud.io>
    Signed-off-by: ruichao.hu <ruichao.hu@daocloud.io>

--- typescript/apps/washboard-ui/src/app/helpers/count-instances.ts
+++
@@ -1,5 +0,0 @@
-import {WasmCloudComponent} from '@wasmcloud/lattice-client-react';
-
-export function countInstances(instances: WasmCloudComponent['instances']): number {
-  return Object.values(instances).reduce((accumulator, current) => accumulator + current.length, 0);
-}
