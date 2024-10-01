be905ac395fe42e3c4ac3796ee7acc4d8d76df53
Victor Adossi <vadossi@cosmonic.com>
2024-10-01 23:43:57 +0900

    feat(wash-cli): support loading existing manifests for `wash dev`
    This commit adds support for loading and using a manifest for use
    during development with `wash dev`.
    
    When manifest overrides are specified, the existing manifest will be
    used, and no new manifests will be generated.
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/tests/wash_dev.rs
+++ crates/wash-cli/tests/wash_dev.rs
@@ -1,9 +1,14 @@
 #![cfg(target_family = "unix")]
 
+use std::path::PathBuf;
 use std::sync::Arc;
 
-use anyhow::{Context, Result};
-use tokio::{process::Command, sync::RwLock, time::Duration};
+use anyhow::{anyhow, bail, Context, Result};
+use tokio::io::AsyncWriteExt;
+use tokio::process::Command;
+use tokio::sync::RwLock;
+use tokio::time::Duration;
+use wasmcloud_control_interface::ClientBuilder as CtlClientBuilder;
 
 mod common;
 use common::{
@@ -13,8 +18,6 @@ use common::{
 #[tokio::test]
 #[serial_test::serial]
 async fn integration_dev_hello_component_serial() -> Result<()> {
-    use anyhow::{anyhow, bail};
-
     wait_for_no_hosts()
         .await
         .context("unexpected wasmcloud instance(s) running")?;
@@ -75,6 +78,176 @@ async fn integration_dev_hello_component_serial() -> Result<()> {
     .await
     .context("timed out while waiting for file path to get created")?;
     assert!(signed_file_path.exists(), "signed component file was built",);
+
+    let process_pid = dev_cmd
+        .write()
+        .await
+        .id()
+        .context("failed to get child process pid")?;
+
+    // Send ctrl + c signal to stop the process
+    // send SIGINT to the child
+    nix::sys::signal::kill(
+        nix::unistd::Pid::from_raw(process_pid as i32),
+        nix::sys::signal::Signal::SIGINT,
+    )
+    .expect("cannot send ctrl-c");
+
+    // Wait until the process stops
+    let _ = tokio::time::timeout(Duration::from_secs(15), dev_cmd.write().await.wait())
+        .await
+        .context("dev command did not exit")?;
+
+    wait_for_no_hosts()
+        .await
+        .context("wasmcloud instance failed to exit cleanly (processes still left over)")?;
+
+    // Kill the nats instance
+    nats.kill().await.map_err(|e| anyhow!(e))?;
+
+    wait_for_no_nats()
+        .await
+        .context("nats instance failed to exit cleanly (processes still left over)")?;
+
+    Ok(())
+}
+
+/// Ensure that overriding manifest YAML works
+#[tokio::test]
+#[serial_test::serial]
+async fn integration_override_manifest_yaml_serial() -> Result<()> {
+    wait_for_no_hosts()
+        .await
+        .context("unexpected wasmcloud instance(s) running")?;
+
+    let test_setup = init("hello", "hello-world-rust").await?;
+    let project_dir = test_setup.project_dir;
+    let dir = test_dir_with_subfolder("dev_hello_component");
+
+    wait_for_no_hosts()
+        .await
+        .context("one or more unexpected wasmcloud instances running")?;
+
+    // Start NATS
+    let nats_port = find_open_port().await?;
+    let mut nats = start_nats(nats_port, &dir).await?;
+
+    // Write out the fixture configuration to disk
+    let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("./tests/fixtures/wadm/hello-world-rust-dev-override.yaml");
+    tokio::fs::write(
+        project_dir.join("test.wadm.yaml"),
+        tokio::fs::read(&fixture_path)
+            .await
+            .with_context(|| format!("failed to read fixture @ [{}]", fixture_path.display()))?,
+    )
+    .await
+    .context("failed to write out fixture file")?;
+
+    // Manipulate the wasmcloud.toml for the test project and override the manifest
+    let wasmcloud_toml_path = project_dir.join("wasmcloud.toml");
+    let mut wasmcloud_toml = tokio::fs::File::options()
+        .append(true)
+        .open(&wasmcloud_toml_path)
+        .await
+        .with_context(|| {
+            format!(
+                "failed to open wasmcloud toml file @ [{}]",
+                wasmcloud_toml_path.display()
+            )
+        })?;
+    wasmcloud_toml
+        .write_all(b"[dev]\nmanifest = \"test.wadm.yaml\"\n")
+        .await
+        .context("failed tow write dev configuration content to file")?;
+
+    // Run wash dev
+    let dev_cmd = Arc::new(RwLock::new(
+        Command::new(env!("CARGO_BIN_EXE_wash"))
+            .args([
+                "dev",
+                "--nats-port",
+                nats_port.to_string().as_ref(),
+                "--nats-connect-only",
+                "--ctl-port",
+                nats_port.to_string().as_ref(),
+            ])
+            .kill_on_drop(true)
+            .spawn()
+            .context("failed running cargo dev")?,
+    ));
+    let watch_dev_cmd = dev_cmd.clone();
+
+    // Create a ctl client to check the cluster
+    let ctl_client = CtlClientBuilder::new(
+        async_nats::connect(format!("127.0.0.1:{nats_port}"))
+            .await
+            .context("failed to create nats client")?,
+    )
+    .lattice("default")
+    .build();
+
+    // Get the host that was created
+    let host = tokio::time::timeout(Duration::from_secs(3), async {
+        loop {
+            if let Some(h) = ctl_client
+                .get_hosts()
+                .await
+                .map_err(|e| anyhow!("failed to get hosts: {e}"))
+                .context("get components")?
+                .into_iter()
+                .map(|v| v.into_data())
+                .collect::<Vec<_>>()
+                .first()
+            {
+                return Ok(h);
+            }
+            tokio::time::sleep(Duration::from_millis(250)).await;
+        }
+    })
+    .await
+    .context("timed out waiting for host to start up")?
+    .context("failed to get the host")?;
+    let host_id = host
+        .as_ref()
+        .context("host was missing from request")?
+        .id()
+        .to_string();
+
+    // Wait until the ferris-says component is present on the host
+    let _ = tokio::time::timeout(
+        Duration::from_secs(60),
+        tokio::spawn(async move {
+            loop {
+                // If the command failed (and exited early), bail
+                if let Ok(Some(exit_status)) = watch_dev_cmd.write().await.try_wait() {
+                    if !exit_status.success() {
+                        bail!("dev command failed");
+                    }
+                }
+                // If the file got built, we know dev succeeded
+                if ctl_client
+                    .get_host_inventory(&host_id)
+                    .await
+                    .map_err(|e| anyhow!(e))
+                    .map(|v| v.into_data())
+                    .context("failed to get host inventory")
+                    .is_ok_and(|inv| {
+                        inv.is_some_and(|cs| {
+                            cs.components()
+                                .iter()
+                                .any(|c| c.name() == Some("ferris-says"))
+                        })
+                    })
+                {
+                    break Ok(()) as anyhow::Result<()>;
+                }
+                tokio::time::sleep(Duration::from_secs(5)).await;
+            }
+        }),
+    )
+    .await
+    .context("timed out while waiting for file path to get created")?;
 
     let process_pid = dev_cmd
         .write()
