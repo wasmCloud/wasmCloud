28455680eff436e3b23abd75496357769c948631
Roman Volosatovs <rvolosatovs@riseup.net>
2024-10-22 22:17:34 +0200

    chore: patch image hashes
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nix/images/default.nix
+++ nix/images/default.nix
@@ -4,23 +4,23 @@
    debian-amd64.finalImageTag = "12-slim";
    debian-amd64.imageDigest = "sha256:36e591f228bb9b99348f584e83f16e012c33ba5cad44ef5981a1d7c0a93eca22";
    debian-amd64.imageName = "debian";
-   debian-amd64.sha256 = "sha256-MOYfHp7XaB3KZVqvT8kSCjmvDPYAaMIW9K74bmxPqCg=";
+   debian-amd64.sha256 = "sha256-ay7tfkWFe6mhW/ysqJKd9+u89g25qtZjCN5/EwaUktg=";
    debian-arm64.arch = "arm64";
    debian-arm64.finalImageName = "debian";
    debian-arm64.finalImageTag = "12-slim";
    debian-arm64.imageDigest = "sha256:36e591f228bb9b99348f584e83f16e012c33ba5cad44ef5981a1d7c0a93eca22";
    debian-arm64.imageName = "debian";
-   debian-arm64.sha256 = "sha256-Zy9awVhKdhYU2mTQMeQprDJffLZj5veOmr4Nkh3Jdc0=";
+   debian-arm64.sha256 = "sha256-dB2CTDtJAdlkBzUOUHxioXtkFNmlbT6IRreBUB5h+zk=";
    wolfi-amd64.arch = "amd64";
    wolfi-amd64.finalImageName = "cgr.dev/chainguard/wolfi-base";
    wolfi-amd64.finalImageTag = "latest";
    wolfi-amd64.imageDigest = "sha256:4857dbc65f7dbf22dd662370a6b211621eba5550d276a9b2ad2596b666cbbdfe";
    wolfi-amd64.imageName = "cgr.dev/chainguard/wolfi-base";
-   wolfi-amd64.sha256 = "sha256-yRSO+//09a00qd+cJJDBhckJPOsdw26tuwvHMEoZrnk=";
+   wolfi-amd64.sha256 = "sha256-OFpWnJj7J2qMEEubZ3dFHbqd08Pqmg4qajztkk9WRkE=";
    wolfi-arm64.arch = "arm64";
    wolfi-arm64.finalImageName = "cgr.dev/chainguard/wolfi-base";
    wolfi-arm64.finalImageTag = "latest";
    wolfi-arm64.imageDigest = "sha256:4857dbc65f7dbf22dd662370a6b211621eba5550d276a9b2ad2596b666cbbdfe";
    wolfi-arm64.imageName = "cgr.dev/chainguard/wolfi-base";
-   wolfi-arm64.sha256 = "sha256-kdsF5b7qKmVpRR2DNYXLD/PVmuAYq8TYLXLA1YyH3+0=";
+   wolfi-arm64.sha256 = "sha256-fIxLRqjZpo1An3nBWLZoaYzFJ6qCOereN93/rflEGl4=";
 }
