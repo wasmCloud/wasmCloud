59c40fd4ac606530c0429e6a699a7329e968523b
Joonas Bergius <joonas@cosmonic.com>
2024-10-24 18:53:26 -0500

    chore(examples): Modernize http-client-tinygo
    Signed-off-by: Joonas Bergius <joonas@cosmonic.com>

--- examples/golang/components/http-client-tinygo/go.mod
+++ examples/golang/components/http-client-tinygo/go.mod
@@ -1,3 +1,20 @@
 module github.com/wasmcloud/wasmcloud/examples/golang/components/http-client-tinygo
 
-go 1.21
+go 1.23.0
+
+toolchain go1.23.2
+
+require (
+	github.com/bytecodealliance/wasm-tools-go v0.2.1
+	go.wasmcloud.dev/component v0.0.3
+)
+
+require (
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/samber/slog-common v0.17.1 // indirect
+	github.com/urfave/cli/v3 v3.0.0-alpha9 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
--- examples/golang/components/http-client-tinygo/hello.go
+++ examples/golang/components/http-client-tinygo/hello.go
@@ -1,70 +1,45 @@
+//go:generate go run github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go generate --world hello --out gen ./wit
+
 package main
 
 import (
-	"log"
-	"math"
+	"fmt"
+	"io"
+	"net/http"
 
-	http "github.com/wasmcloud/wasmcloud/examples/golang/components/http-client-tinygo/gen"
+	"go.wasmcloud.dev/component/log/wasilog"
+	"go.wasmcloud.dev/component/net/wasihttp"
 )
 
-// Helper type aliases to make code more readable
-type HttpRequest = http.ExportsWasiHttp0_2_0_IncomingHandlerIncomingRequest
-type HttpResponseWriter = http.ExportsWasiHttp0_2_0_IncomingHandlerResponseOutparam
-type HttpOutgoingResponse = http.WasiHttp0_2_0_TypesOutgoingResponse
-type HttpError = http.WasiHttp0_2_0_TypesErrorCode
-
-type HttpServer struct{}
+var (
+	wasiTransport = &wasihttp.Transport{}
+	httpClient    = &http.Client{Transport: wasiTransport}
+)
 
 func init() {
-	httpserver := HttpServer{}
-	// Set the incoming handler struct to HttpServer
-	http.SetExportsWasiHttp0_2_0_IncomingHandler(httpserver)
+	wasihttp.HandleFunc(handler)
 }
 
-func (h HttpServer) Handle(request HttpRequest, responseWriter HttpResponseWriter) {
-	req := http.NewOutgoingRequest(http.NewFields())
-	req.SetScheme(http.Some[http.WasiHttp0_2_0_TypesScheme](http.WasiHttp0_2_0_TypesSchemeHttps()))
-	req.SetAuthority(http.Some("dog.ceo"))
-	req.SetPathWithQuery(http.Some("/api/breeds/image/random"))
-	result := http.WasiHttp0_2_0_OutgoingHandlerHandle(req, http.None[http.WasiHttp0_2_0_OutgoingHandlerRequestOptions]())
-	if result.IsOk() {
-		incomingResponse := result.Unwrap()
-		incomingResponse.Subscribe().Block()
-		resp := incomingResponse.Get().Unwrap().Unwrap().Unwrap()
-		if resp.Status() == 200 {
-			responseBody := resp.Consume().Unwrap()
-			headers := http.NewFields()
-			httpResponse := http.NewOutgoingResponse(headers)
-			httpResponse.SetStatusCode(200)
-			body := httpResponse.Body().Unwrap()
-			bodyWrite := body.Write().Unwrap()
-			instream := responseBody.Stream().Unwrap()
-			okResponse := http.Ok[HttpOutgoingResponse, HttpError](httpResponse)
-			http.StaticResponseOutparamSet(responseWriter, okResponse)
-			splice(instream, bodyWrite)
-			bodyWrite.Drop()
-			http.StaticOutgoingBodyFinish(body, http.None[http.WasiHttp0_2_0_TypesTrailers]())
-		}
+func handler(w http.ResponseWriter, r *http.Request) {
+	logger := wasilog.ContextLogger("handler")
 
-	} else {
-		errorCode := result.UnwrapErr()
-		log.Fatal("Error fetching response from http provider", errorCode)
+	url := "https://dog.ceo/api/breeds/image/random"
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		logger.Error("failed to create outbound request", "err", err)
+		http.Error(w, fmt.Sprintf("handler: failed to create outbound request: %s", err), http.StatusInternalServerError)
+		return
 	}
-}
 
-func splice(input http.WasiIo0_2_0_StreamsInputStream, output http.WasiIo0_2_0_StreamsOutputStream) {
-	for {
-		res := output.BlockingSplice(input, math.MaxUint64)
-		if res.IsErr() {
-			err := res.UnwrapErr()
-			if err.Kind() == http.WasiIo0_2_0_StreamsStreamErrorKindClosed {
-				break
-			} else if err.Kind() == http.WasiIo0_2_0_StreamsStreamErrorKindLastOperationFailed {
-				log.Fatal("last operation failed")
-			}
-		}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		logger.Error("failed to make outbound request", "err", err)
+		http.Error(w, fmt.Sprintf("handler: failed to make outbound request: %s", err), http.StatusInternalServerError)
+		return
 	}
+
+	w.WriteHeader(resp.StatusCode)
+	_, _ = io.Copy(w, resp.Body)
 }
 
-//go:generate wit-bindgen tiny-go wit --out-dir=gen --gofmt
 func main() {}
