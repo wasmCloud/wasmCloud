db1468e73f0557f0c3c5c6c9b240706660e49d84
Márk Kővári <kovarimarkofficial@gmail.com>
2024-10-28 16:28:59 +0100

    fix(washboard-ui): fast refresh for settings-context to resolve esling warning
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

--- typescript/apps/washboard-ui/src/app/index.tsx
+++ typescript/apps/washboard-ui/src/app/index.tsx
@@ -3,7 +3,7 @@ import {ReactElement} from 'react';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import {AppLatticeClientProvider} from '@/app/components/app-lattice-client-provider';
 import {AppProvider} from '@/app/components/app-provider';
-import {SettingsProvider} from '@/app/contexts/settings-context';
+import {SettingsProvider} from '@/app/contexts/settings-context-provider';
 import {routes} from '@/app/routes';
 
 export function App(): ReactElement {
