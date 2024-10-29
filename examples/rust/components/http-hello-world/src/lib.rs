d1ef2ab601d9dfb0727ad4eac99b33740a45e94a
Brooks Townsend <brooksmtownsend@gmail.com>
2024-10-29 17:42:56 -0400

    feat(examples): rust hello world use SDK
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/rust/components/http-hello-world/src/lib.rs
+++ examples/rust/components/http-hello-world/src/lib.rs
@@ -1,25 +1,13 @@
-wit_bindgen::generate!({
-    generate_all
-});
+use wasmcloud_component::http;
 
-use exports::wasi::http::incoming_handler::Guest;
-use wasi::http::types::*;
+struct Component;
 
-struct HttpServer;
+http::export!(Component);
 
-impl Guest for HttpServer {
-    fn handle(_request: IncomingRequest, response_out: ResponseOutparam) {
-        let response = OutgoingResponse::new(Fields::new());
-        response.set_status_code(200).unwrap();
-        let response_body = response.body().unwrap();
-        ResponseOutparam::set(response_out, Ok(response));
-        response_body
-            .write()
-            .unwrap()
-            .blocking_write_and_flush(b"Hello from Rust!\n")
-            .unwrap();
-        OutgoingBody::finish(response_body, None).expect("failed to finish response body");
+impl http::Server for Component {
+    fn handle(
+        _request: http::IncomingRequest,
+    ) -> http::Result<http::Response<impl http::OutgoingBody>> {
+        Ok(http::Response::new("Hello from Rust!\n"))
     }
 }
-
-export!(HttpServer);
