a179f974ca551c19f4627aa3cd6db4ae42491c50
Brooks Townsend <brooksmtownsend@gmail.com>
2024-11-01 12:01:15 -0400

    fix(examples): dog fetcher 0.2.2
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/rust/components/dog-fetcher/src/lib.rs
+++ examples/rust/components/dog-fetcher/src/lib.rs
@@ -3,20 +3,20 @@ mod bindings {
 
     wit_bindgen::generate!({
         with: {
-            "wasi:clocks/monotonic-clock@0.2.1": ::wasi::clocks::monotonic_clock,
-            "wasi:http/incoming-handler@0.2.1": generate,
-            "wasi:http/outgoing-handler@0.2.1": ::wasi::http::outgoing_handler,
-            "wasi:http/types@0.2.1": ::wasi::http::types,
-            "wasi:io/error@0.2.1": ::wasi::io::error,
-            "wasi:io/poll@0.2.1": ::wasi::io::poll,
-            "wasi:io/streams@0.2.1": ::wasi::io::streams,
+            "wasi:clocks/monotonic-clock@0.2.2": ::wasi::clocks::monotonic_clock,
+            "wasi:http/incoming-handler@0.2.2": generate,
+            "wasi:http/outgoing-handler@0.2.2": ::wasi::http::outgoing_handler,
+            "wasi:http/types@0.2.2": ::wasi::http::types,
+            "wasi:io/error@0.2.2": ::wasi::io::error,
+            "wasi:io/poll@0.2.2": ::wasi::io::poll,
+            "wasi:io/streams@0.2.2": ::wasi::io::streams,
         }
     });
 
     export!(DogFetcher);
 }
 
-use std::io::{Write as _, Read as _};
+use std::io::{Read as _, Write as _};
 
 use bindings::exports::wasi::http::incoming_handler::Guest;
 use wasi::http::types::*;
@@ -75,10 +75,10 @@ impl Guest for DogFetcher {
         // Build the HTTP response we'll send back to the user
         let response = OutgoingResponse::new(Fields::new());
         response.set_status_code(200).unwrap();
-        let response_body = response.body().unwrap();
-        let mut write_stream = response_body.write().unwrap();
 
         // Write the headers and then write the body
+        let response_body = response.body().unwrap();
+        let mut write_stream = response_body.write().unwrap();
         ResponseOutparam::set(response_out, Ok(response));
 
         // wasi:io/outgoing-stream.blocking_write_and_flush() is the simplest way to
