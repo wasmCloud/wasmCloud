db95cf10d9825bad84929193547c82bce210c0cb
Brooks Townsend <brooksmtownsend@gmail.com>
2024-11-08 15:17:15 -0500

    feat(examples): dog fetcher use blobstore
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/rust/components/dog-fetcher/src/lib.rs
+++ examples/rust/components/dog-fetcher/src/lib.rs
@@ -1,25 +1,11 @@
-mod bindings {
-    use crate::DogFetcher;
-
-    wit_bindgen::generate!({
-        with: {
-            "wasi:clocks/monotonic-clock@0.2.2": ::wasi::clocks::monotonic_clock,
-            "wasi:http/incoming-handler@0.2.2": generate,
-            "wasi:http/outgoing-handler@0.2.2": ::wasi::http::outgoing_handler,
-            "wasi:http/types@0.2.2": ::wasi::http::types,
-            "wasi:io/error@0.2.2": ::wasi::io::error,
-            "wasi:io/poll@0.2.2": ::wasi::io::poll,
-            "wasi:io/streams@0.2.2": ::wasi::io::streams,
-        }
-    });
-
-    export!(DogFetcher);
-}
-
-use std::io::{Read as _, Write as _};
-
-use bindings::exports::wasi::http::incoming_handler::Guest;
-use wasi::http::types::*;
+use url::Url;
+use wasmcloud_component::{
+    http::{ErrorCode, IncomingRequest, OutgoingBody, Response, Server},
+    wasi::{
+        self,
+        http::types::{Fields, Scheme},
+    },
+};
 
 #[derive(serde::Deserialize)]
 struct DogResponse {
@@ -28,70 +14,72 @@ struct DogResponse {
 
 struct DogFetcher;
 
-impl Guest for DogFetcher {
-    fn handle(_request: IncomingRequest, response_out: ResponseOutparam) {
-        // Build a request to dog.ceo which returns a URL at which we can find a doggo
-        let req = wasi::http::outgoing_handler::OutgoingRequest::new(Fields::new());
-        req.set_scheme(Some(&Scheme::Https)).unwrap();
-        req.set_authority(Some("dog.ceo")).unwrap();
-        req.set_path_with_query(Some("/api/breeds/image/random"))
-            .unwrap();
+impl Server for DogFetcher {
+    fn handle(_request: IncomingRequest) -> Result<Response<impl OutgoingBody>, ErrorCode> {
+        // Get dog picture URL
+        let dog_picture_url = make_outgoing_request("https://dog.ceo/api/breeds/image/random")?;
+        let dog_response: DogResponse =
+            serde_json::from_reader(dog_picture_url.stream().map_err(|_| {
+                ErrorCode::InternalError(Some("failed to stream dog API response".to_string()))
+            })?)
+            .map_err(|_| {
+                ErrorCode::InternalError(Some("failed to deserialize dog API response".to_string()))
+            })?;
 
-        // Perform the API call to dog.ceo, expecting a URL to come back as the response body
-        let dog_picture_url = match wasi::http::outgoing_handler::handle(req, None) {
-            Ok(resp) => {
-                resp.subscribe().block();
-                let response = resp
-                    .get()
-                    .expect("HTTP request response missing")
-                    .expect("HTTP request response requested more than once")
-                    .expect("HTTP request failed");
-                if response.status() == 200 {
-                    let response_body = response
-                        .consume()
-                        .expect("failed to get incoming request body");
-                    let body = {
-                        let mut buf = vec![];
-                        let mut stream = response_body
-                            .stream()
-                            .expect("failed to get HTTP request response stream");
-                        stream
-                            .read_to_end(&mut buf)
-                            .expect("failed to read value from HTTP request response stream");
-                        buf
-                    };
-                    let _trailers = wasi::http::types::IncomingBody::finish(response_body);
-                    let dog_response: DogResponse = serde_json::from_slice(&body).unwrap();
-                    dog_response.message
-                } else {
-                    format!("HTTP request failed with status code {}", response.status())
-                }
-            }
-            Err(e) => {
-                format!("Got error when trying to fetch dog: {}", e)
-            }
-        };
-
-        // Build the HTTP response we'll send back to the user
-        let response = OutgoingResponse::new(Fields::new());
-        response.set_status_code(200).unwrap();
-
-        // Write the headers and then write the body
-        let response_body = response.body().unwrap();
-        let mut write_stream = response_body.write().unwrap();
-        ResponseOutparam::set(response_out, Ok(response));
-
-        // wasi:io/outgoing-stream.blocking_write_and_flush() is the simplest way to
-        // write small payloads to an IO stream, but it is limited (up to 4096 bytes).
-        //
-        // Since it's likely that the URL we retrieved from the dog.ceo API is likely
-        // within that limit, we use blocking_write_and_flush() here.
-        //
-        // If we expected the body to possibly be longer, we'd need to loop and write chunks,
-        // paying attention to how to use the appropriate wasi:io APIs.
-        write_stream.write_all(dog_picture_url.as_bytes()).unwrap();
-        drop(write_stream);
-
-        OutgoingBody::finish(response_body, None).expect("failed to finish response body");
+        // Get dog picture
+        let dog_picture = make_outgoing_request(&dog_response.message)?;
+        // TODO: blobstore
+        Ok(Response::new(dog_picture))
     }
 }
+
+fn make_outgoing_request(url: &str) -> Result<wasi::http::types::IncomingBody, ErrorCode> {
+    let req = wasi::http::outgoing_handler::OutgoingRequest::new(Fields::new());
+    let url = Url::parse(url)
+        .map_err(|_| ErrorCode::InternalError(Some("failed to parse URL".to_string())))?;
+
+    if url.scheme() == "https" {
+        req.set_scheme(Some(&Scheme::Https)).map_err(|_| {
+            ErrorCode::InternalError(Some("failed to set HTTPS scheme".to_string()))
+        })?;
+    } else if url.scheme() == "http" {
+        req.set_scheme(Some(&Scheme::Http))
+            .map_err(|_| ErrorCode::InternalError(Some("failed to set HTTP scheme".to_string())))?;
+    } else {
+        req.set_scheme(Some(&Scheme::Other(url.scheme().to_string())))
+            .map_err(|_| {
+                ErrorCode::InternalError(Some("failed to set custom scheme".to_string()))
+            })?;
+    }
+
+    req.set_authority(Some(url.authority()))
+        .map_err(|_| ErrorCode::InternalError(Some("failed to set URL authority".to_string())))?;
+
+    req.set_path_with_query(Some(url.path()))
+        .map_err(|_| ErrorCode::InternalError(Some("failed to set URL path".to_string())))?;
+
+    match wasi::http::outgoing_handler::handle(req, None) {
+        Ok(resp) => {
+            resp.subscribe().block();
+            let response = resp
+                .get()
+                .ok_or(())
+                .map_err(|_| {
+                    ErrorCode::InternalError(Some("HTTP request response missing".to_string()))
+                })?
+                .map_err(|_| {
+                    ErrorCode::InternalError(Some(
+                        "HTTP request response requested more than once".to_string(),
+                    ))
+                })?
+                .map_err(|_| ErrorCode::InternalError(Some("HTTP request failed".to_string())))?;
+
+            response.consume().map_err(|_| {
+                ErrorCode::InternalError(Some("failed to consume response".to_string()))
+            })
+        }
+        Err(e) => Err(e),
+    }
+}
+
+wasmcloud_component::http::export!(DogFetcher);
