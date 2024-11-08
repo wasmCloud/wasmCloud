d5384bcfa3811233bb6646833701b2103f913729
Dan Norris <protochron@users.noreply.github.com>
2024-11-07 23:02:58 -0500

    feat: add TinyGo version of the http-password-checker
    Implement a Go version of the http-password checker. It has a slightly
    different output format than the Rust one because it uses a much less
    feature rich library for assessing the password complexity.
    
    Signed-off-by: Dan Norris <protochron@users.noreply.github.com>

---
+++ examples/golang/components/http-password-checker/main.go
@@ -0,0 +1,75 @@
+//go:generate go run github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go generate --world hello --out gen ./wit
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	gopasswordvalidator "github.com/wagslane/go-password-validator"
+	"go.wasmcloud.dev/component/net/wasihttp"
+)
+
+type CheckRequest struct {
+	Value string `json:"value"`
+}
+
+type CheckResponse struct {
+	Valid   bool   `json:"valid"`
+	Length  int    `json:"length,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func init() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/check", handleRequest)
+	wasihttp.Handle(mux)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errResponseJSON(w, http.StatusMethodNotAllowed, "Method not allowed")
+		return
+	}
+
+	var req CheckRequest
+	defer r.Body.Close()
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		errResponseJSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := json.Unmarshal(b, &req); err != nil {
+		errResponseJSON(w, http.StatusBadRequest, fmt.Sprintf("error with json input: %s", err.Error()))
+		return
+	}
+
+	err = gopasswordvalidator.Validate(req.Value, 60)
+	if err != nil {
+		errResponseJSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	resp := CheckResponse{Valid: true, Length: len(req.Value)}
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		errResponseJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respJSON)
+}
+
+func errResponseJSON(w http.ResponseWriter, code int, message string) {
+	msg, _ := json.Marshal(CheckResponse{Valid: false, Message: message})
+	http.Error(w, string(msg), code)
+	w.Header().Set("Content-Type", "application/json")
+}
+
+// Since we don't run this program like a CLI, the `main` function is empty. Instead,
+// we call the `handleRequest` function when an HTTP request is received.
+func main() {}
