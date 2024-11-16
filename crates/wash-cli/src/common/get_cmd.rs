02b001c5b221c9748f9c969911a5ec16d47c2ca7
Gohlub <62673775+Gohlub@users.noreply.github.com>
2024-11-16 18:12:52 -0500

    fix(wash-cli): spinner rendering error
    Fixed the spinner rendering inconsistencies by separating out the logic to the match statement
    
    Reviewed-by: Zahir S
    Refs: #3584
    Signed-off-by: Gohlub  <62673775+Gohlub@users.noreply.github.com>
    Signed-off-by: Gohlub <62673775+Gohlub@users.noreply.github.com>
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/src/common/get_cmd.rs
+++ crates/wash-cli/src/common/get_cmd.rs
@@ -19,22 +19,24 @@ use crate::ctl::{
 };
 
 pub async fn handle_command(command: GetCommand, output_kind: OutputKind) -> Result<CommandOutput> {
-    let sp: Spinner = Spinner::new(&output_kind)?;
     let out: CommandOutput = match command {
         GetCommand::Links(GetLinksCommand { opts }) => {
             handle_link_command(LinkCommand::Query(LinkQueryCommand { opts }), output_kind).await?
         }
         GetCommand::Claims(cmd) => {
+            let sp: Spinner = Spinner::new(&output_kind)?;
             sp.update_spinner_message("Retrieving claims ... ".to_string());
             let claims = get_claims(cmd).await?;
             get_claims_output(claims)
         }
         GetCommand::Hosts(cmd) => {
+            let sp: Spinner = Spinner::new(&output_kind)?;
             sp.update_spinner_message(" Retrieving Hosts ...".to_string());
             let hosts = get_hosts(cmd).await?;
             get_hosts_output(hosts)
         }
         GetCommand::HostInventories(cmd) => {
+            let sp: Spinner = Spinner::new(&output_kind)?;
             if let Some(id) = cmd.host_id.as_ref() {
                 sp.update_spinner_message(format!(" Retrieving inventory for host {} ...", id));
             } else {
