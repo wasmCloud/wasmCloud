cd825e563e080c9f92044c6c13daf20940cef284
ruichao.hu <ruichao.hu@daocloud.io>
2024-11-20 10:38:04 +0800

    refactor(lattice-client-react): split lattice config context into separate file
    Signed-off-by: ruichao.hu <ruichao.hu@daocloud.io>

--- typescript/packages/lattice-client-react/src/use-lattice-config.ts
+++ typescript/packages/lattice-client-react/src/use-lattice-config.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {
   LatticeConfigContext,
   LatticeConfigContextType,
-} from './context/lattice-config-provider';
+} from './context/lattice-config-context';
 
 /**
  * get the current lattice config and a function to update it
