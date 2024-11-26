a25c71f9a091fbeb423a7e07bfe4887214823a4b
Victor Adossi <vadossi@cosmonic.com>
2024-11-26 23:11:13 +0900

    feat(wash-cli): add support for deleting all links
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/src/cmd/link/del.rs
+++ crates/wash-cli/src/cmd/link/del.rs
@@ -2,11 +2,13 @@
 
 use std::collections::HashMap;
 
-use anyhow::{anyhow, bail, ensure, Result};
+use anyhow::{anyhow, bail, ensure, Context as _, Result};
 use serde_json::json;
 use wash_lib::cli::link::{delete_link, get_links, LinkDelCommand};
 use wash_lib::cli::{CommandOutput, OutputKind};
 use wash_lib::config::WashConnectionOptions;
+use wash_lib::generate::interactive::prompt_for_choice;
+use wash_lib::generate::project_variables::StringEntry;
 
 use crate::appearance::spinner::Spinner;
 
@@ -18,11 +20,86 @@ pub async fn invoke(
         wit_namespace: namespace,
         wit_package: package,
         opts,
+        all,
+        force,
     }: LinkDelCommand,
     output_kind: OutputKind,
 ) -> Result<CommandOutput> {
-    let sp: Spinner = Spinner::new(&output_kind)?;
     let wco: WashConnectionOptions = opts.try_into()?;
+
+    // If the user has chosen to delete all links, but *did not* force, then prompt
+    if all {
+        let delete_links = |wco: WashConnectionOptions| async move {
+            let sp: Spinner = Spinner::new(&output_kind)?;
+            let links = get_links(wco.clone())
+                .await
+                .context("failed to retrieve links")?;
+            let mut deleted_links = Vec::with_capacity(links.len());
+            for link in &links {
+                sp.update_spinner_message(format!(
+                    "Deleting link for {} on {}:{} ({}) ... ",
+                    link.source_id(),
+                    link.wit_namespace(),
+                    link.wit_package(),
+                    link.name(),
+                ));
+                if let Err(e) = delete_link(
+                    wco.clone(),
+                    link.source_id(),
+                    link.name(),
+                    link.wit_namespace(),
+                    link.wit_package(),
+                )
+                .await
+                .context("failed to delete link, aborting delete all operation")
+                {
+                    return Ok::<CommandOutput, anyhow::Error>(CommandOutput::new(
+                        format!("Deleted some {} links successfully", links.len(),),
+                        HashMap::from([
+                            ("error".into(), json!(e.to_string())),
+                            (
+                                "deleted".into(),
+                                json!(deleted_links.into_iter().collect::<Vec<_>>()),
+                            ),
+                        ]),
+                    ));
+                }
+                deleted_links.push(link);
+            }
+            sp.finish_and_clear();
+            Ok::<CommandOutput, anyhow::Error>(CommandOutput::new(
+                format!("Deleted all links ({}) successfully", links.len(),),
+                HashMap::from([(
+                    "deleted".into(),
+                    json!(deleted_links.into_iter().collect::<Vec<_>>()),
+                )]),
+            ))
+        };
+
+        if force {
+            return delete_links(wco.clone())
+                .await
+                .context("failed to delete all links");
+        } else {
+            match prompt_for_choice(
+                &StringEntry {
+                    default: Some("cancel".to_string()),
+                    choices: Some(vec!["Delete all links".to_string(), "Cancel".to_string()]),
+                    regex: None,
+                },
+                "Are you sure you want to delete all links in the cluster? (this action cannot be reversed)",
+            ) {
+                Ok(0) => return delete_links(wco.clone()).await.context("failed to delete all links"),
+                Ok(1) => bail!("Link deletion cancelled"),
+                _ => unreachable!("unexpected choice received"),
+            }
+        }
+    }
+
+    let sp: Spinner = Spinner::new(&output_kind)?;
+    let package = package.context("missing required argument package")?;
+    let source_id = source_id.context("missing required argument source_id")?;
+    let namespace = namespace.context("missing required argument namespace")?;
 
     // If the link name is not specified, and multiple links are similar in other ways
     // make deleting the link an error, as the user should likely be explicitly choosing
@@ -48,7 +125,6 @@ pub async fn invoke(
     };
 
     let link_name = link_name.clone().unwrap_or_else(|| "default".to_string());
-
     sp.update_spinner_message(format!(
         "Deleting link for {source_id} on {namespace}:{package} ({link_name}) ... ",
     ));
