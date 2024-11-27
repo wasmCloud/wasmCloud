7d2cefc355d15bfcf92d9e60afa64b3047c94db7
Roman Volosatovs <rvolosatovs@riseup.net>
2024-11-27 10:34:05 +0100

    test: do not test builtin providers
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- tests/interfaces.rs
+++ tests/interfaces.rs
@@ -98,27 +98,28 @@ async fn interfaces() -> anyhow::Result<()> {
         rust_blobstore_fs,
         rust_blobstore_s3,
         rust_http_client,
+        rust_http_server,
         rust_keyvalue_redis,
         rust_keyvalue_vault,
+        rust_messaging_nats,
     ) = join!(
         providers::rust_blobstore_fs(),
         providers::rust_blobstore_s3(),
         providers::rust_http_client(),
+        providers::rust_http_server(),
         providers::rust_keyvalue_redis(),
         providers::rust_keyvalue_vault(),
+        providers::rust_messaging_nats(),
     );
-
-    let builtin_http_server_id = "http-server";
-    let builtin_messaging_nats_id = "messaging-nats";
 
     let rust_blobstore_fs_id = rust_blobstore_fs.subject.public_key();
     let rust_blobstore_s3_id = rust_blobstore_s3.subject.public_key();
     let rust_http_client_id = rust_http_client.subject.public_key();
+    let rust_http_server_id = rust_http_server.subject.public_key();
     let rust_keyvalue_redis_id = rust_keyvalue_redis.subject.public_key();
     let rust_keyvalue_vault_id = rust_keyvalue_vault.subject.public_key();
+    let rust_messaging_nats_id = rust_messaging_nats.subject.public_key();
 
-    let host_key = host.host_key();
-    let host_id = host_key.public_key();
     try_join!(
         async {
             let minio_config_json = format!(
@@ -145,7 +146,7 @@ async fn interfaces() -> anyhow::Result<()> {
                     &ctl_client,
                     &http_server_config_name,
                     [(
-                        "address".to_string(),
+                        "ADDRESS".to_string(),
                         format!("{}:{http_port}", Ipv4Addr::LOCALHOST),
                     )],
                 ),
@@ -186,13 +187,15 @@ async fn interfaces() -> anyhow::Result<()> {
             .context("failed to put configuration")
         },
         async {
-            let builtin_http_server_url = providers::builtin_http_server();
-            let builtin_messaging_nats_url = providers::builtin_messaging_nats();
+            let host_key = host.host_key();
             let rust_blobstore_fs_url = rust_blobstore_fs.url();
             let rust_blobstore_s3_url = rust_blobstore_s3.url();
             let rust_http_client_url = rust_http_client.url();
+            let rust_http_server_url = rust_http_server.url();
             let rust_keyvalue_redis_url = rust_keyvalue_redis.url();
             let rust_keyvalue_vault_url = rust_keyvalue_vault.url();
+            let rust_messaging_nats_url = rust_messaging_nats.url();
+            let host_id = host_key.public_key();
             try_join!(
                 assert_start_provider(StartProviderArgs {
                     client: &ctl_client,
@@ -218,8 +221,8 @@ async fn interfaces() -> anyhow::Result<()> {
                 assert_start_provider(StartProviderArgs {
                     client: &ctl_client,
                     host_id: &host_id,
-                    provider_id: builtin_http_server_id,
-                    provider_ref: builtin_http_server_url.as_str(),
+                    provider_id: &rust_http_server_id,
+                    provider_ref: rust_http_server_url.as_str(),
                     config: vec![],
                 }),
                 assert_start_provider(StartProviderArgs {
@@ -239,8 +242,8 @@ async fn interfaces() -> anyhow::Result<()> {
                 assert_start_provider(StartProviderArgs {
                     client: &ctl_client,
                     host_id: &host_id,
-                    provider_id: builtin_messaging_nats_id,
-                    provider_ref: builtin_messaging_nats_url.as_str(),
+                    provider_id: &rust_messaging_nats_id,
+                    provider_ref: rust_messaging_nats_url.as_str(),
                     config: vec![],
                 }),
             )
@@ -283,7 +286,7 @@ async fn interfaces() -> anyhow::Result<()> {
 
     assert_advertise_link(
         &ctl_client,
-        builtin_http_server_id,
+        &rust_http_server_id,
         INTERFACES_REACTOR_ID,
         "default",
         "wasi",
@@ -386,7 +389,7 @@ async fn interfaces() -> anyhow::Result<()> {
     assert_advertise_link(
         &ctl_client,
         INTERFACES_REACTOR_ID,
-        builtin_messaging_nats_id,
+        &rust_messaging_nats_id,
         "default",
         "wasmcloud",
         "messaging",
@@ -403,7 +406,7 @@ async fn interfaces() -> anyhow::Result<()> {
     assert_advertise_link(
         &ctl_client,
         INTERFACES_HANDLER_REACTOR_ID,
-        builtin_messaging_nats_id,
+        &rust_messaging_nats_id,
         "default",
         "wasmcloud",
         "messaging",
