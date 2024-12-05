b4070e957990742b93fe9741fc14bc6c5ad2e05b
Brooks Townsend <brooksmtownsend@gmail.com>
2024-12-05 11:29:59 -0500

    refactor(host): consistently name features
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/test-util/src/host.rs
+++ crates/test-util/src/host.rs
@@ -102,8 +102,8 @@ impl WasmCloudTestHost {
             allow_file_load: true,
             secrets_topic_prefix,
             experimental_features: Features::new()
-                .enable_builtin_http()
-                .enable_builtin_messaging(),
+                .enable_builtin_http_server()
+                .enable_builtin_messaging_nats(),
             ..Default::default()
         };
         if let Some(psc) = policy_service_config {
