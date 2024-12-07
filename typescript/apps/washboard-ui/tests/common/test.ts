1bb785df1c9930ddda143f66dad4046c46d8a72e
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-12-07 00:30:11 -0500

    test(washboard): make wasmcloud fixture automatic and configurable
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- typescript/apps/washboard-ui/tests/common/test.ts
+++ typescript/apps/washboard-ui/tests/common/test.ts
@@ -1,28 +1,45 @@
-/* eslint-disable react-hooks/rules-of-hooks -- Disabling rules-of-hooks since these are Playwright extensions */
-
 import {test as base} from '@playwright/test';
 
 import {logger} from './logger';
 import {WasmCloudInstance, WasmCloudInstanceOptions} from './wasmcloud-instance';
 
-export type TestMeta = {instance: WasmCloudInstance};
+export type WasmCloudInstanceFixture = {
+  wasmCloud: WasmCloudInstance;
+  wasmCloudOptions: WasmCloudInstanceOptions;
+};
 
 // Extend basic test by providing a wasmCloud instance that starts before
 // the tests run and enables programmatic manipulation of a wasmCloud instance via `wash`
-export const test = base.extend<TestMeta>({
-  instance: async ({page}, use) => {
-    const instance = new WasmCloudInstance({
-      ...WasmCloudInstanceOptions.default(),
-      washUI: {
-        // NOTE: this port is set to the deafult for the `webServer` configured via playwright config
-        port: 5173,
-      },
-    });
-    await instance.start();
-    const uiBaseURL = instance.uiBaseURL();
-    logger.debug(`UI available at @ ${uiBaseURL}]`);
-    await page.goto(uiBaseURL);
-    await use(instance);
-    await instance.stop();
-  },
+export const test = base.extend<WasmCloudInstanceFixture>({
+  wasmCloudOptions: {},
+  wasmCloud: [
+    async ({wasmCloudOptions}, use, testInfo) => {
+      const instance = new WasmCloudInstance({
+        ...WasmCloudInstanceOptions.default(),
+        ...wasmCloudOptions,
+      });
+      await test.step(`Start (${instance.uuid()})`, async () => {
+        if (wasmCloudOptions.startWashUI) {
+          await instance.start();
+          const uiBaseURL = instance.uiBaseURL();
+          logger.debug(`UI available at @ ${uiBaseURL}]`);
+          test.use({baseURL: uiBaseURL});
+        }
+      });
+
+      await use(instance);
+
+      await test.step(`Stop (${instance.uuid()})`, async () => {
+        await instance.stop();
+
+        // Attach logs if the test failed
+        if (testInfo.status !== testInfo.expectedStatus) {
+          for (const [name, logFile] of Object.entries(instance.logPaths)) {
+            testInfo.attachments.push({name, contentType: 'text/plain', path: logFile});
+          }
+        }
+      });
+    },
+    {title: 'wasmCloud', auto: true},
+  ],
 });
--- typescript/apps/washboard-ui/tests/e2e/home.spec.ts
+++ typescript/apps/washboard-ui/tests/e2e/home.spec.ts
@@ -2,11 +2,13 @@ import {expect} from '@playwright/test';
 
 import {test} from '../common';
 
-/// ////////
-// Tests //
-/// ////////
-
-test('home page has the the proper title', async ({page, instance}) => {
-  await page.goto(instance.uiBaseURL());
+test('home page has the the proper title', async ({page}) => {
+  await page.goto('/');
   await expect(page).toHaveTitle('wasmCloud UI | Washboard 🏄');
+});
+
+test('page shows the application frame', async ({page}) => {
+  await page.goto('/');
+  const rootElement = await page.$('#root');
+  await expect(await rootElement.innerHTML()).toBeTruthy();
 });
