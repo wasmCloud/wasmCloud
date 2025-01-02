350fa8f6b796ba6ba302ce9bd2094798ef165bf2
Brooks Townsend <brooksmtownsend@gmail.com>
2025-01-02 12:03:26 -0500

    fix(host): output host_id on provider_start_failed
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/host/src/wasmbus/mod.rs
+++ crates/host/src/wasmbus/mod.rs
@@ -2222,7 +2222,7 @@ impl Host {
                 if let Err(err) = self
                     .publish_event(
                         "provider_start_failed",
-                        event::provider_start_failed(provider_ref, provider_id, &err),
+                        event::provider_start_failed(provider_ref, provider_id, host_id, &err),
                     )
                     .await
                 {
