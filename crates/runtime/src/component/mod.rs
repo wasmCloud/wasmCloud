84402a1703a38718ac72dcddfed99f61ab750e09
Aditya Salunkhe <aditya.salunkh919@gmail.com>
2025-02-06 00:30:03 +0530

    feat(host): Add runtime support for cronjob provider
    Signed-off-by: Aditya Salunkhe <aditya.salunkh919@gmail.com>

--- crates/runtime/src/component/mod.rs
+++ crates/runtime/src/component/mod.rs
@@ -46,6 +46,7 @@ pub(crate) mod blobstore;
 mod bus;
 mod bus1_0_0;
 mod config;
+mod cron;
 mod http;
 mod identity;
 mod keyvalue;
@@ -251,6 +252,13 @@ pub enum WrpcServeEvent<C> {
     },
     /// dynamic export return event
     DynamicExportReturned {
+        /// Invocation context
+        context: C,
+        /// Whether the invocation was successfully handled
+        success: bool,
+    },
+    /// `wasmcloud:cron/scheduler.timed-invoke` return event
+    CronInvocationReturned {
         /// Invocation context
         context: C,
         /// Whether the invocation was successfully handled
@@ -618,6 +626,17 @@ where
                     .await
                     .context("failed to serve `wrpc:http/incoming-handler`")?;
                     invocations.push(handle);
+                }
+                (
+                    "wasmcloud:cron/scheduler@0.1.0-draft",
+                    types::ComponentItem::ComponentInstance(..),
+                ) => {
+                    let instance = instance.clone();
+                    let [(_, _, timed_invoke)] =
+                        wrpc::exports::wasmcloud::cron::scheduler::serve_interface(srv, instance)
+                            .await
+                            .context("failed to serve `wrpc:keyvalue/watcher`")?;
+                    invocations.push(timed_invoke);
                 }
                 (
                     "wasmcloud:messaging/handler@0.2.0"
