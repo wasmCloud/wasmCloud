d54eb0f035a4269ea163dbb5c3282a613f8e78e4
Joonas Bergius <joonas@cosmonic.com>
2025-02-13 18:42:25 -0600

    chore: Match method and variable naming from suggestions
    Signed-off-by: Joonas Bergius <joonas@cosmonic.com>

--- crates/host/src/workload_identity.rs
+++ crates/host/src/workload_identity.rs
@@ -16,21 +16,23 @@ impl WorkloadIdentityConfig {
     /// Fetch workload identity configuration from environment variables
     pub fn from_env() -> Result<Self> {
         // TODO(joonas): Should we fall back to a default value here instead of requiring the endpoint to be explicitly set?
-        let endpoint = get_default_socket_path()
+        let spiffe_endpoint = get_default_socket_path()
             .context("failed to load SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET")?;
 
-        validate_socket_path(&spiffe_endpoint).context("failed to validate SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET")?;
+        validate_socket_path(&spiffe_endpoint)
+            .context("failed to validate SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET")?;
 
-        let endpoint_uri = url::Url::parse(&endpoint)?;
+        let endpoint_uri = url::Url::parse(&spiffe_endpoint)?;
 
         // If we're given a path to socket, check that the socket exists.
         if endpoint_uri.scheme() == UNIX_SCHEME && !Path::new(endpoint_uri.path()).exists() {
-            bail!("provided SPIFFE workload endpoint does not exist: {endpoint}");
+            bail!("provided SPIFFE workload endpoint does not exist: {spiffe_endpoint}");
         }
 
         // TODO(joonas): figure out better naming here. maybe this should be interpolated from a trust domain?
         // This needs to follow format like: "spiffe://{spiffe_trust_domain}/{nats_auth_callout_service}"
-        let auth_service_audience = std::env::var(AUTH_SERVICE_AUDIENCE_ENV).context("workload identity auth callout audience environment variable is missing")?;
+        let auth_service_audience = std::env::var(AUTH_SERVICE_AUDIENCE_ENV)
+            .context("workload identity auth callout audience environment variable is missing")?;
 
         Ok(Self {
             spiffe_endpoint,
