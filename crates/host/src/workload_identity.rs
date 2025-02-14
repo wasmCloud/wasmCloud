e5cfea8345d3d7b316223404b056184791d4dddf
Joonas Bergius <joonas@users.noreply.github.com>
2025-02-13 18:37:15 -0600

    Apply suggestions from code review
    Co-authored-by: Brooks Townsend <brooksmtownsend@gmail.com>
    Signed-off-by: Joonas Bergius <joonas@users.noreply.github.com>

--- crates/host/src/workload_identity.rs
+++ crates/host/src/workload_identity.rs
@@ -13,39 +13,27 @@ pub struct WorkloadIdentityConfig {
 }
 
 impl WorkloadIdentityConfig {
-    pub fn new() -> Result<Self> {
+    /// Fetch workload identity configuration from environment variables
+    pub fn from_env() -> Result<Self> {
         // TODO(joonas): Should we fall back to a default value here instead of requiring the endpoint to be explicitly set?
         let endpoint = get_default_socket_path()
             .context("failed to load SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET")?;
 
-        if let Err(err) = validate_socket_path(&endpoint) {
-            bail!(
-                "failed to validate SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET: {:?}",
-                err
-            );
-        }
+        validate_socket_path(&spiffe_endpoint).context("failed to validate SPIFFE workload endpoint from SPIFFE_ENDPOINT_SOCKET")?;
 
         let endpoint_uri = url::Url::parse(&endpoint)?;
 
         // If we're given a path to socket, check that the socket exists.
         if endpoint_uri.scheme() == UNIX_SCHEME && !Path::new(endpoint_uri.path()).exists() {
-            bail!(
-                "provided SPIFFE workload endpoint does not exist: {}",
-                endpoint,
-            );
+            bail!("provided SPIFFE workload endpoint does not exist: {endpoint}");
         }
 
         // TODO(joonas): figure out better naming here. maybe this should be interpolated from a trust domain?
         // This needs to follow format like: "spiffe://{spiffe_trust_domain}/{nats_auth_callout_service}"
-        let auth_service_audience = match std::env::var(AUTH_SERVICE_AUDIENCE_ENV) {
-            Ok(value) => value,
-            Err(_) => {
-                bail!("workload identity auth callout audience environment variable is missing")
-            }
-        };
+        let auth_service_audience = std::env::var(AUTH_SERVICE_AUDIENCE_ENV).context("workload identity auth callout audience environment variable is missing")?;
 
         Ok(Self {
-            spiffe_endpoint: endpoint,
+            spiffe_endpoint,
             auth_service_audience,
         })
     }
