4f18b83ad795d092508fb47291eb278f6d1ce5be
Aditya Salunkhe <aditya.salunkh919@gmail.com>
2025-02-23 23:08:54 +0530

    feat(providers): Implement wasmcloud:cron and it's scheduler interface
    Signed-off-by: Aditya Salunkhe <aditya.salunkh919@gmail.com>

--- crates/provider-cron-scheduler/src/lib.rs
+++ crates/provider-cron-scheduler/src/lib.rs
@@ -1,10 +1,16 @@
 use std::collections::HashMap;
-
+mod bindings {
+    wit_bindgen_wrpc::generate!({
+        with: {
+            "wasmcloud:cron/scheduler@0.1.0-draft":generate,
+        }
+    });
+}
 struct CronProvider {
     jobs: HashMap<String, String>,
 }
 
-pub async fn run()  -> anyhow::Result<()> {
+pub async fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
