bd01caf3a5b78249cf3ea8b54bd8811ad64a6b2c
Brooks Townsend <brooksmtownsend@gmail.com>
2025-04-30 15:02:34 -0400

    fix(test-util): wait for host_started event to ensure ready
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/test-util/src/host.rs
+++ crates/test-util/src/host.rs
@@ -9,6 +9,7 @@ use anyhow::{anyhow, Context as _, Result};
 use async_nats::{Client as NatsClient, ServerAddr};
 use nkeys::KeyPair;
 use tokio::task::JoinSet;
+use tokio_stream::StreamExt as _;
 use url::Url;
 
 use wasmcloud_control_interface::{Client as WasmcloudCtlClient, ClientBuilder};
@@ -127,8 +128,8 @@ impl WasmCloudTestHost {
             None,
             BTreeMap::new(),
             false,
-            false,
-            false,
+            true,
+            true,
         )
         .await?
         .with_event_publisher(host_key.public_key());
@@ -154,12 +155,39 @@ impl WasmCloudTestHost {
         };
 
         let (host_builder, ctl_server) = nats_builder.build(host_config).await?;
+
+        let mut host_started_sub = nats_client
+            .subscribe(format!("wasmbus.evt.{lattice_name}.host_started"))
+            .await
+            .context("failed to subscribe for host started event")?;
         let (host, shutdown_hook) = host_builder
             .build()
             .await
             .context("failed to initialize host")?;
 
         let ctl_server_handle = ctl_server.start(host.clone()).await?;
+
+        let host_public_key = host_key.public_key();
+        tokio::time::timeout(Duration::from_secs(30), async move {
+            while let Some(msg) = host_started_sub.next().await {
+                let evt_value: serde_json::Value = serde_json::from_slice(&msg.payload)
+                    .context("failed to deserialize host started event")?;
+                // TODO(#4408): Use strongly typed host_started event here
+                if let Some(target) = evt_value.as_object() {
+                    if let Some(data) = target.get("data") {
+                        if let Some(host_id) = data.get("id") {
+                            if *host_id == *host_public_key {
+                                return Ok(());
+                            }
+                        }
+                    }
+                }
+            }
+
+            Err(anyhow!("failed to receive host started event"))
+        })
+        .await
+        .context("failed to wait for host to start")?;
 
         Ok(Self {
             cluster_key,
