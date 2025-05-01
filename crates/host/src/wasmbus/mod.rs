d046a604761ff63c5569fda203bc42cec1b4f7a0
Brooks Townsend <brooksmtownsend@gmail.com>
2025-05-01 09:11:06 -0400

    fix(host): use provided defaults for registry pulls
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- crates/host/src/wasmbus/mod.rs
+++ crates/host/src/wasmbus/mod.rs
@@ -1490,7 +1490,7 @@ impl Host {
         fetch_component(
             component_ref,
             self.host_config.allow_file_load,
-            &self.host_config.oci_opts.additional_ca_paths,
+            &self.host_config.oci_opts,
             &registry_config,
         )
         .await
@@ -1908,7 +1908,7 @@ impl Host {
                     &provider_ref,
                     host_id,
                     self.host_config.allow_file_load,
-                    &self.host_config.oci_opts.additional_ca_paths,
+                    &self.host_config.oci_opts,
                     &registry_config,
                 )
                 .await
