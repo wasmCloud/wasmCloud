9527afb9980cb641a82da822f3682aeb54b402ab
Bailey Hayes <bailey@cosmonic.com>
2026-04-30 16:45:20 -0400

    chore(go): lint for pulling in controller-runtime
    - Drop import of sigs.k8s.io/controller-runtime/pkg/scheme
    - Inline a small local Builder type with the same
      Register(...) + AddToScheme(...) shape, built on
      runtime.SchemeBuilder from apimachinery
    - Existing SchemeBuilder.Register(&T{}, &TList{}) calls in
      *_types.go files keep working unchanged
    
    Signed-off-by: Bailey Hayes <bailey@cosmonic.com>

--- runtime-operator/api/runtime/v1alpha1/groupversion_info.go
+++ runtime-operator/api/runtime/v1alpha1/groupversion_info.go
@@ -4,8 +4,11 @@
 package v1alpha1
 
 import (
+	"reflect"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -13,8 +16,34 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "runtime.wasmcloud.dev", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Builder is a local replacement for sigs.k8s.io/controller-runtime/pkg/scheme.Builder,
+// kept here so this api package depends only on k8s.io/apimachinery.
+// +kubebuilder:object:generate=false
+type Builder struct {
+	GroupVersion schema.GroupVersion
+	runtime.SchemeBuilder
+}
+
+// Register adds objects to the GroupVersionKind scheme.
+func (b *Builder) Register(objects ...runtime.Object) *Builder {
+	b.SchemeBuilder.Register(func(scheme *runtime.Scheme) error {
+		for _, obj := range objects {
+			gvk := b.GroupVersion.WithKind(reflect.TypeOf(obj).Elem().Name())
+			scheme.AddKnownTypeWithName(gvk, obj)
+		}
+		metav1.AddToGroupVersion(scheme, b.GroupVersion)
+		return nil
+	})
+	return b
+}
+
+// AddToScheme adds the registered types to the given scheme.
+func (b *Builder) AddToScheme(s *runtime.Scheme) error {
+	return b.SchemeBuilder.AddToScheme(s)
+}
